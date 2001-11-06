725a7177b2cc67ba6952a1f305cdab954722659e
fabled <fabled@c6295689-39f2-0310-b995-f0e70906c6a9>
2001-11-06 18:43:36 +0000

    Updated license to LGPL. Added short description.
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@37 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/pkcs11/verify.c
+++ src/pkcs11/verify.c
@@ -1,19 +1,21 @@
-/* Copyright (C) 2001  Timo Teräs <timo.teras@iki.fi>
- * All rights reserved.
+/*
+ * verify.c: PKCS#11 functions for signature verification
  *
- * This program is free software; you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ * Copyright (C) 2001  Timo Teräs <timo.teras@iki.fi>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include "sc-pkcs11.h"
