cf795aa7b8debf7355e890ab6f5324dc26ccc5c6
okir <okir@c6295689-39f2-0310-b995-f0e70906c6a9>
2002-03-11 14:12:46 +0000

    - exporting sc_update_dir to applications
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@292 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/opensc.h
+++ src/libopensc/opensc.h
@@ -720,6 +720,7 @@ int sc_get_cache_dir(struct sc_context *ctx, char *buf, size_t bufsize);
 int sc_enum_apps(struct sc_card *card);
 const struct sc_app_info * sc_find_app_by_aid(struct sc_card *card,
 					      const u8 *aid, size_t aid_len);
+int sc_update_dir(struct sc_card *card, struct sc_app_info *app);
 
 struct sc_card_error {
 	int SWs;
