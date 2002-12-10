9b6074b95118e8124a7ef7b9ce22eeab0b02f31e
jey <jey@c6295689-39f2-0310-b995-f0e70906c6a9>
2002-12-10 13:43:23 +0000

    - Added openscd and Assuan
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@763 c6295689-39f2-0310-b995-f0e70906c6a9

---
+++ src/openscd/mkdtemp.c
@@ -0,0 +1,102 @@
+/* mkdtemp.c - libc replacement function
+ * Copyright (C) 2001 Free Software Foundation, Inc.
+ *
+ * This file is part of GnuPG.
+ *
+ * GnuPG is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GnuPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+/* This is a replacement function for mkdtemp in case the platform
+   we're building on (like mine!) doesn't have it. */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <assert.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "../scrandom/scrandom.h"
+
+#ifdef MKDIR_TAKES_ONE_ARG
+# undef mkdir
+# define mkdir(a,b) mkdir(a)
+#endif
+
+char *mkdtemp(char *template)
+{
+	int attempts, idx, count = 0;
+	unsigned char *ch;
+
+	idx = strlen(template);
+
+	/* Walk backwards to count all the Xes */
+	while (idx > 0 && template[idx - 1] == 'X') {
+		count++;
+		idx--;
+	}
+
+	if (count == 0) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	ch = &template[idx];
+
+	/* Try 4 times to make the temp directory */
+	for (attempts = 0; attempts < 4; attempts++) {
+		int remaining = count;
+		char *marker = ch;
+		unsigned char *randombits;
+
+		idx = 0;
+
+		/* Using really random bits is probably overkill here.  The
+		   worst thing that can happen with a directory name collision
+		   is that the function will return an error. */
+
+		randombits = malloc(4 * remaining);
+		assert(randombits != NULL);
+		assert(scrandom_get_data(randombits, 4 * remaining) ==
+		       4 * remaining);
+
+		while (remaining > 1) {
+			sprintf(marker, "%02X", randombits[idx++]);
+			marker += 2;
+			remaining -= 2;
+		}
+
+		/* Any leftover Xes?  get_random_bits rounds up to full bytes,
+		   so this is safe. */
+		if (remaining > 0)
+			sprintf(marker, "%X", randombits[idx] & 0xF);
+
+		free(randombits);
+
+		if (mkdir(template, 0700) == 0)
+			break;
+	}
+
+	if (attempts == 4)
+		return NULL;	/* keeps the errno from mkdir, whatever it is */
+
+	return template;
+}
---
+++ src/openscd/openscd.h
@@ -0,0 +1,42 @@
+#ifndef OPENSCD_H
+
+#include "../libopensc/opensc.h"
+#include <pthread.h>
+
+#define MAX_CARDS 4
+
+struct openscd_context {
+	struct sc_context *ctx;
+	char *socket_name;
+	int socket_fd;
+
+        int cmd_stuff_ok;
+	struct openscd_card {
+		sc_card_t *card;
+		sc_pkcs15_card_t *p15card;
+
+		struct sc_reader *reader;
+		int slot_id;
+		int card_id;
+                pthread_mutex_t mutex;
+	} *cards;
+	int card_count;
+        int card_id_number;
+        pthread_mutex_t card_mutex;
+
+	pthread_t *threads;
+        int thread_count;
+};
+
+struct openscd_thread_arg {
+	struct openscd_context *dctx;
+	struct sc_reader *reader;
+};
+
+char *mkdtemp(char *template);
+void die(int return_code, const char *errmsg, ...);
+void command_handler(struct openscd_context *dctx);
+void init_cmd_stuff(struct openscd_context *dctx);
+void cleanup_cmd_stuff(struct openscd_context *dctx);
+
+#endif
