5ef34fc7bc5635d5033cb73c74bfe3db77f6007c
okir <okir@c6295689-39f2-0310-b995-f0e70906c6a9>
2002-12-17 20:15:32 +0000

    - Added support for SC_ALGORITHM_RSA_HASH_RIPEMD160 in sc_pkcs15_compute_signature - rewrote add_padding
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@781 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/pkcs15-sec.c
+++ src/libopensc/pkcs15-sec.c
@@ -29,6 +29,34 @@
 #include <unistd.h>
 #endif
 
+/*
+ * Prefixes for pkcs-v1 signatures
+ */
+static const u8 hdr_md5[] = {
+	0x30, 0x20, 0x30, 0x0c, 0x06, 0x08, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x02, 0x05, 0x05, 0x00, 0x04, 0x10
+};
+static const u8 hdr_sha1[] = {
+	0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02, 0x1a, 0x05, 0x00, 0x04, 0x14
+};
+static const u8 hdr_ripemd160[] = {
+	0x30, 0x1c, 0x30, 0x06, 0x01, 0x03, 0x24, 0x03, 0x02, 0x01
+};
+
+#define DIGEST_INFO_COUNT 5
+static struct digest_info_prefix {
+	unsigned int	algorithm;
+	const u8 *	hdr;
+	size_t		hdr_len;
+	int		hash_len;
+} digest_info_prefix[DIGEST_INFO_COUNT] = {
+      {	SC_ALGORITHM_RSA_HASH_MD5,	hdr_md5,	sizeof(hdr_md5),	16	},
+      { SC_ALGORITHM_RSA_HASH_SHA1,	hdr_sha1,	sizeof(hdr_sha1),	20	},
+      { SC_ALGORITHM_RSA_HASH_RIPEMD160,hdr_ripemd160,	sizeof(hdr_ripemd160),	20	},
+      { SC_ALGORITHM_RSA_HASH_MD5_SHA1,	NULL,		0,			36	},
+      {	0,				NULL,		0,			-1	}
+};
+
+
 static int pkcs1_strip_padding(u8 *data, size_t len)
 {
 	unsigned int	n = 0;
@@ -141,75 +169,86 @@ int sc_pkcs15_decipher(struct sc_pkcs15_card *p15card,
 	return r;
 }
 
-static int pkcs1_add_padding(const u8 *in, size_t inlen, u8 *out, size_t outlen)
+/*
+ * No padding required - card will add the padding itself
+ */
+static int add_no_padding(struct digest_info_prefix *pfx,
+		          const u8 *in, size_t inlen,
+			  u8 *out, size_t *outlen,
+			  size_t mod_length)
 {
+	size_t msglen = pfx->hdr_len + inlen;
+
+	if (msglen > mod_length)
+		return SC_ERROR_INVALID_ARGUMENTS;
+	if (msglen > *outlen)
+		return SC_ERROR_BUFFER_TOO_SMALL;
+	memcpy(out, pfx->hdr, pfx->hdr_len);
+	memcpy(out + pfx->hdr_len, in, inlen);
+
+	*outlen = msglen;
+	return 0;
+}
+
+/*
+ * Add pkcs1 padding
+ */
+static int add_pkcs1_padding(struct digest_info_prefix *pfx,
+			     const u8 *in, size_t inlen,
+			     u8 *out, size_t *outlen,
+			     size_t mod_length)
+{
+	size_t msglen = pfx->hdr_len + inlen;
 	int i;
-	
-	if (inlen + 11 > outlen)
+
+	if (*outlen < mod_length)
+		return SC_ERROR_BUFFER_TOO_SMALL;
+	if (msglen + 11 > mod_length)
 		return SC_ERROR_INVALID_ARGUMENTS;
 	*out++ = 0x00;
 	*out++ = 0x01;
 	
-	i = outlen - 3 - inlen;
+	i = mod_length - 3 - msglen;
 	memset(out, 0xFF, i);
 	out += i;
 	*out++ = 0x00;
-	memcpy(out, in, inlen);
+	memcpy(out, pfx->hdr, pfx->hdr_len);
+	memcpy(out + pfx->hdr_len, in, inlen);
 
+	*outlen = mod_length;
 	return 0;
 }
 
 static int add_padding(struct sc_context *ctx, const u8 *in, size_t inlen, u8 *out,
 		       size_t *outlen, unsigned long flags, unsigned int mod_length)
 {
-	u8 buf[64];
-        size_t buflen;
-	const u8 *hdr_md5 =  (const u8 *) "\x30\x20\x30\x0c\x06\x08\x2a\x86\x48\x86\xf7\x0d\x02\x05\x05\x00\x04\x10";
-        size_t hdr_md5_len = 18;
-	const u8 *hdr_sha1 = (const u8 *) "\x30\x21\x30\x09\x06\x05\x2b\x0e\x03\x02\x1a\x05\x00\x04\x14";
-        size_t hdr_sha1_len = 15;
+	struct digest_info_prefix *pfx;
+	int j, hash_algo, pad_algo;
 
-	if (flags & SC_ALGORITHM_RSA_HASH_SHA1) {
-		if (inlen != 20)
-                        return SC_ERROR_WRONG_LENGTH;
-		memcpy(buf, hdr_sha1, hdr_sha1_len);
-		memcpy(buf + hdr_sha1_len, in, 20);
-                buflen = 20 + hdr_sha1_len;
-	} else if (flags & SC_ALGORITHM_RSA_HASH_MD5) {
-		if (inlen != 16)
-                        return SC_ERROR_WRONG_LENGTH;
-		memcpy(buf, hdr_md5, hdr_md5_len);
-		memcpy(buf + hdr_md5_len, in, 16);
-                buflen = 16 + hdr_md5_len;
-	} else if (flags & SC_ALGORITHM_RSA_HASH_MD5_SHA1) {
-		if (inlen != 36)
-                        return SC_ERROR_WRONG_LENGTH;
-		memcpy(buf, in, 36);
-                buflen = 36;
-	} else
-                buflen = 0;
-	if (buflen) {
-		in = buf;
-                inlen = buflen;
-	}
-	if (flags & SC_ALGORITHM_RSA_PAD_PKCS1) {
-		int r;
+	hash_algo = flags & SC_ALGORITHM_RSA_HASHES;
+	pad_algo  = flags & SC_ALGORITHM_RSA_PADS;
 
-		if (*outlen < mod_length)
-                        return SC_ERROR_BUFFER_TOO_SMALL;
-		r = pkcs1_add_padding(in, inlen, out, mod_length);
-		if (r)
-			return r;
-                *outlen = mod_length;
-	} else {
-		if (*outlen < inlen)
-			return SC_ERROR_BUFFER_TOO_SMALL;
-		memcpy(out, in, inlen);
-                *outlen = inlen;
+	for (j = DIGEST_INFO_COUNT, pfx = digest_info_prefix; j--; pfx++) {
+		if (pfx->algorithm == hash_algo)
+			break;
 	}
-	if (buflen)
-                memset(buf, 0, buflen);
-        return 0;
+	if (j <= 0) {
+		error(ctx, "Unsupported digest algorithm 0x%x\n", hash_algo);
+		return SC_ERROR_NOT_SUPPORTED;
+	}
+
+	if (pfx->hash_len > 0 && inlen != pfx->hash_len)
+		return SC_ERROR_WRONG_LENGTH;
+
+	switch (pad_algo) {
+	case 0: /* padding done by card */
+		return add_no_padding(pfx, in, inlen, out, outlen, mod_length);
+	case SC_ALGORITHM_RSA_PAD_PKCS1:
+		return add_pkcs1_padding(pfx, in, inlen, out, outlen, mod_length);
+	default:
+		error(ctx, "Unsupported padding algorithm 0x%x\n", pad_algo);
+		return SC_ERROR_NOT_SUPPORTED;
+	}
 }
 
 int sc_pkcs15_compute_signature(struct sc_pkcs15_card *p15card,
@@ -258,6 +297,13 @@ int sc_pkcs15_compute_signature(struct sc_pkcs15_card *p15card,
 			pad_flags |= SC_ALGORITHM_RSA_HASH_MD5;
 		else
                         senv.algorithm_flags |= SC_ALGORITHM_RSA_HASH_MD5;
+	} else if (flags & SC_ALGORITHM_RSA_HASH_RIPEMD160) {
+		if (inlen != 20)
+			SC_FUNC_RETURN(ctx, 0, SC_ERROR_WRONG_LENGTH);
+		if (!(alg_info->flags & SC_ALGORITHM_RSA_HASH_RIPEMD160))
+			pad_flags |= SC_ALGORITHM_RSA_HASH_RIPEMD160;
+		else
+                        senv.algorithm_flags |= SC_ALGORITHM_RSA_HASH_RIPEMD160;
 	} else if (flags & SC_ALGORITHM_RSA_HASH_MD5_SHA1) {
 		if (inlen != 36)
 			SC_FUNC_RETURN(ctx, 0, SC_ERROR_WRONG_LENGTH);
