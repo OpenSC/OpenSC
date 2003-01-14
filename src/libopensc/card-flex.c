f2f6c247163e2536b8a3478985b988d9f9b03c81
okir <okir@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-01-14 16:44:34 +0000

    - added ATR for Cryptoflex 32k v4
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@861 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/card-flex.c
+++ src/libopensc/card-flex.c
@@ -44,6 +44,8 @@ static struct {
 	TYPE_CRYPTOFLEX },
       {	"3B:95:94:40:FF:63:01:01:02:01",       /* 16k */
 	TYPE_CRYPTOFLEX },
+      { "3B:95:18:40:FF:64:02:01:01:02",       /* 32K v4 */
+	TYPE_CRYPTOFLEX },
       {	"3B:95:18:40:FF:62:04:01:01:05",       /* 32K e-gate v4 */
 	TYPE_CRYPTOFLEX },
       {	"3B:E2:00:00:40:20:49:06",
