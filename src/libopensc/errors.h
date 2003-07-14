29add3c3c9ff852f97500e40e6f4ddcd97d9590e
okir <okir@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-07-14 16:56:38 +0000

    - added SC_ERROR_WRONG_PADDING
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1260 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/errors.h
+++ src/libopensc/errors.h
@@ -84,6 +84,7 @@ extern "C" {
 #define SC_ERROR_PASSPHRASE_REQUIRED		-1409
 #define SC_ERROR_EXTRACTABLE_KEY		-1410
 #define SC_ERROR_DECRYPT_FAILED			-1411
+#define SC_ERROR_WRONG_PADDING			-1412
 
 /* Relating to PKCS #15 init stuff */
 #define SC_ERROR_PKCS15INIT			-1500
