9dd994fb2c0320f00571b14377983d768315f7a1
sth <sth@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-07-17 15:09:01 +0000

    Little fix for Windows
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1275 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/include/winconfig.h
+++ src/include/winconfig.h
@@ -45,7 +45,7 @@
  */
 #define OPENSC_CONF_PATH "%windir%\\opensc.conf"
 
-#define SC_PKCS15_PROFILE_DIRECTORY "%windir%\\"
+#define SC_PKCS15_PROFILE_DIRECTORY "%windir%"
 
 #define PATH_MAX _MAX_PATH
 
