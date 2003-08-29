bd54ea7a62c6dd4495c3c2d37fdfc64889d85ad1
aet <aet@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-08-29 16:29:52 +0000

    - Move all dynamic loading related code to src/common/scdl.c,   probably needs some more work.
    As a side bonus, we now have a working CT-API support for MacOS X.
    
    
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1402 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/module.c
+++ src/libopensc/module.c
@@ -23,81 +23,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#ifdef HAVE_DLFCN_H
-#include <dlfcn.h>
-
-int sc_module_open(struct sc_context *ctx, void **mod_handle, const char *filename)
-{
-	const char *error;
-	void *handle;
-
-	assert(ctx != NULL);
-
-	if (!filename)
-		return SC_ERROR_UNKNOWN;
-
-	handle = dlopen(filename, RTLD_LAZY);
-
-	if ((error = dlerror()) != NULL) {
-		if (ctx->debug)
-			sc_debug(ctx, "sc_module_open: %s\n", error);
-		return SC_ERROR_UNKNOWN;
-	}
-	*mod_handle = handle;
-	return SC_SUCCESS;
-}
-
-int sc_module_close(struct sc_context *ctx, void *mod_handle)
-{
-	const char *error;
-
-	assert(ctx != NULL);
-
-	if (!mod_handle)
-		return SC_ERROR_UNKNOWN;
-
-	dlclose(mod_handle);
-
-	if ((error = dlerror()) != NULL) {
-		if (ctx->debug)
-			sc_debug(ctx, "sc_module_close: %s\n", error);
-		return SC_ERROR_UNKNOWN;
-	}
-	return SC_SUCCESS;
-}
-
-int sc_module_get_address(struct sc_context *ctx, void *mod_handle, void **sym_address, const char *sym_name)
-{
-	const char *error;
-	char name[256];
-	void *address;
-
-	assert(ctx != NULL);
-
-	if (!mod_handle || !sym_name)
-		return SC_ERROR_UNKNOWN;
-
-	/* Some platforms might need a leading underscore for the symbol */
-	name[0] = '_';
-	strncpy(&name[1], sym_name, sizeof(name) - 1);
-
-	address = dlsym(mod_handle, name);
-
-	/* Failed? Try again without the leading underscore */
-	if (address == NULL)
-		address = dlsym(mod_handle, sym_name);
-
-	if ((error = dlerror()) != NULL) {
-		if (ctx->debug)
-			sc_debug(ctx, "sc_module_get_address: %s\n", error);
-		return SC_ERROR_UNKNOWN;
-	}
-	*sym_address = address;
-	return SC_SUCCESS;
-}
-
-#elif defined(_WIN32)
-#include <windows.h>
+#include "../common/scdl.c"
 
 int sc_module_open(struct sc_context *ctx, void **mod_handle, const char *filename)
 {
@@ -108,11 +34,11 @@ int sc_module_open(struct sc_context *ctx, void **mod_handle, const char *filena
 	if (!filename)
 		return SC_ERROR_UNKNOWN;
 
-	handle = LoadLibrary(filename);
+	handle = scdl_open(filename);
 
 	if (handle == NULL) {
 		if (ctx->debug)
-			/* TODO: GetLastError */
+			/* TODO: scdl_error */
 			sc_debug(ctx, "sc_module_open: unknown error");
 		return SC_ERROR_UNKNOWN;
 	}
@@ -127,9 +53,7 @@ int sc_module_close(struct sc_context *ctx, void *mod_handle)
 	if (!mod_handle)
 		return SC_ERROR_UNKNOWN;
 
-	FreeLibrary(mod_handle);
-	/* TODO: GetLastError */
-
+	scdl_close(mod_handle);
 	return SC_SUCCESS;
 }
 
@@ -142,34 +66,14 @@ int sc_module_get_address(struct sc_context *ctx, void *mod_handle, void **sym_a
 	if (!mod_handle || !sym_name)
 		return SC_ERROR_UNKNOWN;
 
-
-	address = GetProcAddress(mod_handle, sym_name);
+	address = scdl_get_address(mod_handle, sym_name);
 
 	if (address == NULL) {
 		if (ctx->debug)
-			/* TODO: GetLastError */
+			/* TODO: scdl_error */
 			sc_debug(ctx, "sc_module_get_address: unknown error");
 		return SC_ERROR_UNKNOWN;
 	}
 	*sym_address = address;
 	return SC_SUCCESS;
 }
-
-#else
-
-int sc_module_open(struct sc_context *ctx, void **mod_handle, const char *filename)
-{
-	return SC_ERROR_UNKNOWN;
-}
-
-int sc_module_close(struct sc_context *ctx, void *mod_handle)
-{
-	return SC_ERROR_UNKNOWN;
-}
-
-int sc_module_get_address(struct sc_context *ctx, void *mod_handle, void **sym_address, const char *sym_name)
-{
-	return SC_ERROR_UNKNOWN;
-}
-
-#endif
--- src/pkcs11/libpkcs11.c
+++ src/pkcs11/libpkcs11.c
@@ -12,15 +12,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#ifdef __APPLE__
-#include <Carbon/Carbon.h>
-#endif
+#include "../common/scdl.c"
 
 #define MAGIC			0xd00bed00
-
-#define DL_TYPE_DLFCN		0x01
-#define DL_TYPE_WIN32		0x02
-#define DL_TYPE_MAC		0x03
 
 #if defined(_WIN32)
 #define DEFAULT_MODULE_NAME	"opensc-pkcs11";
@@ -34,16 +28,8 @@
 
 struct sc_pkcs11_module {
 	unsigned int _magic;
-	void *_dl_handle;
-	unsigned int _type;
-#if defined(__APPLE__)
-	CFBundleRef bundleRef;
-#endif
+	scdl_context_t *handle;
 };
-
-static int	sys_dlopen(sc_pkcs11_module_t *, const char *);
-static int	sys_dlclose(sc_pkcs11_module_t *);
-static void *	sys_dlsym(sc_pkcs11_module_t *, const char *);
 
 /*
  * Load a module - this will load the shared object, call
@@ -58,12 +44,16 @@ C_LoadModule(const char *mspec, CK_FUNCTION_LIST_PTR_PTR funcs)
 
 	mod = (sc_pkcs11_module_t *) calloc(1, sizeof(*mod));
 	mod->_magic = MAGIC;
-	if (sys_dlopen(mod, mspec) < 0)
+
+	if (mspec == NULL)
+		mspec = DEFAULT_MODULE_NAME;
+	mod->handle = scdl_open(mspec);
+	if (mod->handle == NULL)
 		goto failed;
 
 	/* Get the list of function pointers */
 	c_get_function_list = (CK_RV (*)(CK_FUNCTION_LIST_PTR_PTR))
-				sys_dlsym(mod, "C_GetFunctionList");
+				scdl_get_address(mod->handle, "C_GetFunctionList");
 	if (!c_get_function_list)
 		goto failed;
 	rv = c_get_function_list(funcs);
@@ -85,250 +75,10 @@ C_UnloadModule(sc_pkcs11_module_t *mod)
 	if (!mod || mod->_magic != MAGIC)
 		return CKR_ARGUMENTS_BAD;
 
-	if (sys_dlclose(mod) < 0)
+	if (scdl_close(mod->handle) < 0)
 		return CKR_FUNCTION_FAILED;
 
 	memset(mod, 0, sizeof(*mod));
 	free(mod);
 	return CKR_OK;
-}
-
-#if defined(HAVE_DLFCN_H)
-#include <dlfcn.h>
-
-/*
- * Module loader for platforms that have dlopen
- *
- * This is intentionally primitive; we may want a more
- * elaborate loader in libopensc one day
- */
-int
-dl_dlopen(struct sc_pkcs11_module *mod, const char *name)
-{
-	const char	**dir, *ldlist[64];
-	char		pathbuf[4096], *ldenv;
-	unsigned int	n = 0;
-
-	if ((ldenv = getenv("LD_LIBRARY_PATH"))
-	 && (ldenv = strdup(ldenv))) {
-		ldlist[n] = strtok(ldenv, ":");
-		while (ldlist[n] != NULL && ++n < 63)
-			ldlist[n] = strtok(NULL, ":");
-	}
-	ldlist[n] = NULL;
-
-	for (dir = ldlist; *dir; dir++) {
-		snprintf(pathbuf, sizeof(pathbuf), "%s/%s", *dir, name);
-		mod->_dl_handle = dlopen(pathbuf, RTLD_NOW);
-		if (mod->_dl_handle != NULL)
-			break;
-	}
-
-	if (mod->_dl_handle == NULL)
-		mod->_dl_handle = dlopen(name, RTLD_NOW);
-
-	if (ldenv)
-		free(ldenv);
-
-	mod->_type = DL_TYPE_DLFCN;
-	return (mod->_dl_handle? 0 : -1);
-}
-
-int
-dl_dlclose(struct sc_pkcs11_module *mod)
-{
-	if (mod->_dl_handle)
-		dlclose(mod->_dl_handle);
-	mod->_dl_handle = NULL;
-	return 0;
-}
-
-void *
-dl_dlsym(sc_pkcs11_module_t *mod, const char *name)
-{
-	char sym_name[256];
-	void *address;
-
-	if (!mod->_dl_handle)
-		return NULL;
-
-	/* Some platforms might need a leading underscore for the symbol */
-	snprintf(sym_name, sizeof(sym_name), "_%s", name);
-	address = dlsym(mod->_dl_handle, sym_name);
-
-	/* Failed? Try again without the leading underscore */
-	if (address == NULL)
-		address = dlsym(mod->_dl_handle, name);
-
-	return address;
-}
-
-#endif
-#ifdef _WIN32
-#include <windows.h>
-
-/*
- * Module loader for the Windows platform.
- */
-int
-win32_dlopen(struct sc_pkcs11_module *mod, const char *name)
-{
-	mod->_dl_handle = LoadLibrary(name);
-	mod->_type = DL_TYPE_WIN32;
-
-	return (mod->_dl_handle? 0 : GetLastError());
-}
-
-int
-win32_dlclose(struct sc_pkcs11_module *mod)
-{
-	if (mod->_dl_handle) {
-		if (FreeLibrary(mod->_dl_handle)) {
-			mod->_dl_handle = NULL;
-			return 0;
-		}
-		else
-			return -1;
-	}
-
-	return 0;
-}
-
-void *
-win32_dlsym(sc_pkcs11_module_t *mod, const char *name)
-{
-	if (!mod->_dl_handle)
-		return NULL;
-	return GetProcAddress(mod->_dl_handle, name);
-}
-
-#endif
-#if defined(__APPLE__)
-#include <mach-o/dyld.h>
-
-/*
- * Module loader for MacOS X
- */
-int
-mac_dlopen(struct sc_pkcs11_module *mod, const char *name)
-{
-	if (strstr(name, ".bundle")) {
-		CFStringRef text = CFStringCreateWithFormat(
-			NULL, NULL, CFSTR("%s"), name);
-		CFURLRef urlRef = CFURLCreateWithFileSystemPath(
-			kCFAllocatorDefault, text, kCFURLPOSIXPathStyle, 1);
-		mod->bundleRef = CFBundleCreate(kCFAllocatorDefault, urlRef);
-		CFRelease(urlRef);
-		CFRelease(text);
-		mod->_dl_handle = NULL;
-	} else {
-		mod->_dl_handle = (struct mach_header *) NSAddImage(name,
-			NSADDIMAGE_OPTION_WITH_SEARCHING);
-		mod->bundleRef = NULL;
-	}
-	mod->_type = DL_TYPE_MAC;
-
-	return (mod->_dl_handle == NULL && mod->bundleRef == NULL ? -1 : 0);
-}
-
-int
-mac_dlclose(struct sc_pkcs11_module *mod)
-{
-	if (mod->bundleRef != NULL) {
-		CFBundleUnloadExecutable(mod->bundleRef);
-		CFRelease(mod->bundleRef);
-	}
-
-	return 0;
-}
-
-void *
-mac_dlsym(sc_pkcs11_module_t *mod, const char *name)
-{
-	NSSymbol symbol = NULL;
-
-	if (mod->bundleRef != NULL) {
-		CFStringRef text = CFStringCreateWithFormat(
-			NULL, NULL, CFSTR("%s"), name);
-		symbol = CFBundleGetFunctionPointerForName(
-			mod->bundleRef, text);
-		CFRelease(text);
-		return symbol;
-	} else {
-		char sym_name[4096];
-
-		snprintf(sym_name, sizeof(sym_name), "_%s", name);
-		symbol = NSLookupSymbolInImage((const struct mach_header *)
-			mod->_dl_handle, sym_name,
-			NSLOOKUPSYMBOLINIMAGE_OPTION_BIND_NOW);
-		if (symbol == NULL)
-			return NULL;
-		return NSAddressOfSymbol(symbol);
-	}
-}
-#endif
-
-int
-sys_dlopen(struct sc_pkcs11_module *mod, const char *name)
-{
-	if (name == NULL)
-		name = DEFAULT_MODULE_NAME;
-#if defined(_WIN32)
-	return win32_dlopen(mod, name);
-#elif defined(HAVE_DLFCN_H) && defined(__APPLE__)
-	if (strstr(name, ".bundle") || strstr(name, ".dylib")) {
-		return mac_dlopen(mod, name);
-	} else {
-		return dl_dlopen(mod, name);
-	}
-#elif defined(__APPLE__)
-	return mac_dlopen(mod, name);
-#elif defined(HAVE_DLFCN_H)
-	return dl_dlopen(mod, name);
-#endif
-	return -1;
-}
-
-int
-sys_dlclose(struct sc_pkcs11_module *mod)
-{
-#if defined(_WIN32)
-	return win32_dlclose(mod);
-#elif defined(HAVE_DLFCN_H) && defined(__APPLE__)
-	switch(mod->_type) {
-	case DL_TYPE_MAC:
-		return mac_dlclose(mod);
-		break;
-	case DL_TYPE_DLFCN:
-		return dl_dlclose(mod);
-		break;
-	}
-#elif defined(__APPLE__)
-	return mac_dlclose(mod);
-#elif defined(HAVE_DLFCN_H)
-	return dl_dlclose(mod);
-#endif
-	return 0;
-}
-
-void *
-sys_dlsym(sc_pkcs11_module_t *mod, const char *name)
-{
-#if defined(_WIN32)
-	return win32_dlsym(mod, name);
-#elif defined(HAVE_DLFCN_H) && defined(__APPLE__)
-	switch(mod->_type) {
-	case DL_TYPE_MAC:
-		return mac_dlsym(mod, name);
-		break;
-	case DL_TYPE_DLFCN:
-		return dl_dlsym(mod, name);
-		break;
-	}
-#elif defined(__APPLE__)
-	return mac_dlsym(mod, name);
-#elif defined(HAVE_DLFCN_H)
-	return dl_dlsym(mod, name);
-#endif
-	return NULL;
 }
