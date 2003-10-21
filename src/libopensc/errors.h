72788e32ec20f70e286fa2a691ab531201503698
okir <okir@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-10-21 11:11:19 +0000

    - added SC_ERROR_CANNOT_LOAD_MODULE
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1550 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/errors.h
+++ src/libopensc/errors.h
@@ -86,6 +86,7 @@ extern "C" {
 #define SC_ERROR_DECRYPT_FAILED			-1411
 #define SC_ERROR_WRONG_PADDING			-1412
 #define SC_ERROR_WRONG_CARD			-1413
+#define SC_ERROR_CANNOT_LOAD_MODULE		-1414
 
 /* Relating to PKCS #15 init stuff */
 #define SC_ERROR_PKCS15INIT			-1500
