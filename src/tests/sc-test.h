fd1272c7677a066346c3df73fd61df55dfdc608d
okir <okir@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-10-31 12:28:15 +0000

    - moved p15 print_card to tests/print.c
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1585 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/tests/sc-test.h
+++ src/tests/sc-test.h
@@ -7,10 +7,12 @@ extern "C" {
 
 extern struct sc_context *ctx;
 extern struct sc_card *card;
+struct sc_pkcs15_card;
 struct sc_pkcs15_object;
 
 int sc_test_init(int *argc, char *argv[]);
 void sc_test_cleanup();
+void sc_test_print_card(const struct sc_pkcs15_card *);
 void sc_test_print_object(const struct sc_pkcs15_object *);
 
 #ifdef  __cplusplus
