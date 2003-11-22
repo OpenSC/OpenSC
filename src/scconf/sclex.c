1435c8fccec59bc8f418045035231c2f430c2804
aet <aet@c6295689-39f2-0310-b995-f0e70906c6a9>
2003-11-22 18:50:04 +0000

    - Back out Olaf's change, as it seems to break existing   behaviour while parsing pkcs15 profile files. Although   officially any list value with an equal sign or braces   should be enclosured with quotation marks, but anyway.
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1633 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/scconf/sclex.c
+++ src/scconf/sclex.c
@@ -155,7 +155,7 @@ static int scconf_lex_engine(scconf_parser * parser, BUFHAN * bp)
 		case EOF:
 			break;
 		default:
-			buf_eat_till(bp, (char) this_char, ";,{}= \t\r\n");
+			buf_eat_till(bp, (char) this_char, ";, \t\r\n");
 			scconf_parse_token(parser, TOKEN_TYPE_STRING, bp->buf);
 			buf_zero(bp);
 			continue;
