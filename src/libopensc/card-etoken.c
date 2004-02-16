9333f53ffcdfce7374fb4703763fee8a47a462a1
aj <aj@c6295689-39f2-0310-b995-f0e70906c6a9>
2004-02-16 12:29:54 +0000

    remove CARDOS_TYPE_ETOKEN_PRO, the name does not fit. annotate atr with version number 4.0 / 4.01 / 4.01a.
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1753 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/card-etoken.c
+++ src/libopensc/card-etoken.c
@@ -33,7 +33,6 @@
 
 /* Different eToken types */
 #define CARDOS_TYPE_ANY		1
-#define CARDOS_TYPE_ETOKEN_PRO	2
 
 static const struct sc_card_operations *iso_ops = NULL;
 
@@ -48,9 +47,9 @@ const struct {
 	const char *	atr;
 	int		type;
 } etoken_atrs[] = {
-    { "3b:e2:00:ff:c1:10:31:fe:55:c8:02:9c",	CARDOS_TYPE_ETOKEN_PRO },
-    { "3b:f2:98:00:ff:c1:10:31:fe:55:c8:03:15",	CARDOS_TYPE_ETOKEN_PRO },
-    { "3b:f2:98:00:ff:c1:10:31:fe:55:c8:04:12",	CARDOS_TYPE_ETOKEN_PRO },
+    { "3b:e2:00:ff:c1:10:31:fe:55:c8:02:9c",	CARDOS_TYPE_ANY }, /* 4.0 */
+    { "3b:f2:98:00:ff:c1:10:31:fe:55:c8:03:15",	CARDOS_TYPE_ANY }, /* 4.01 */
+    { "3b:f2:98:00:ff:c1:10:31:fe:55:c8:04:12",	CARDOS_TYPE_ANY }, /* 4.01a */
     /* Italian eID card: */
     { "3b:e9:00:ff:c1:10:31:fe:55:00:64:05:00:c8:02:31:80:00:47", CARDOS_TYPE_ANY },
 
