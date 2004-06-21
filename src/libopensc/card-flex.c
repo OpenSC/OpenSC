a9b00f005f06be05bbc3051654279ff656bcde28
nils <nils@c6295689-39f2-0310-b995-f0e70906c6a9>
2004-06-21 21:20:29 +0000

    use the padding character from the pkcs15 objects
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@1799 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/card-flex.c
+++ src/libopensc/card-flex.c
@@ -1262,7 +1262,6 @@ static void flex_init_pin_info(struct sc_pin_cmd_pin *pin, unsigned int num)
 	pin->encoding   = SC_PIN_ENCODING_ASCII;
 	pin->max_length = 8;
 	pin->pad_length = 8;
-	pin->pad_char   = 0;
 	pin->offset     = 5 + num * 8;
 }
 
