a0aa5dd05e565644c9c11c72168f9e925c71da57
nils <nils@c6295689-39f2-0310-b995-f0e70906c6a9>
2005-02-02 22:18:50 +0000

    make win compilers happy (include config.h)
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@2125 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/pkcs15-starcert.c
+++ src/libopensc/pkcs15-starcert.c
@@ -18,6 +18,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <opensc/pkcs15.h>
 #include <opensc/cardctl.h>
 #include <stdlib.h>
