42d3d503c72555ecd8a29088dd54e2dd6eb08913
aet <aet@c6295689-39f2-0310-b995-f0e70906c6a9>
2005-02-04 11:43:51 +0000

    - Add internal.h, that's where the config.h and other general   stuff comes from.
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@2128 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/pkcs15-starcert.c
+++ src/libopensc/pkcs15-starcert.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "internal.h"
 #include <opensc/pkcs15.h>
 #include <opensc/cardctl.h>
 #include <stdlib.h>
