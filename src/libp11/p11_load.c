b99c52c6f5428f900ca1e668ebebb4d17891e524
nils <nils@c6295689-39f2-0310-b995-f0e70906c6a9>
2005-07-19 11:57:31 +0000

    free error strings on when destroying context
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@2440 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libp11/p11_load.c
+++ src/libp11/p11_load.c
@@ -137,6 +137,8 @@ void PKCS11_CTX_unload(PKCS11_CTX * ctx)
  */
 void PKCS11_CTX_free(PKCS11_CTX * ctx)
 {
+	ERR_free_strings();
+	ERR_remove_state(0);
 	OPENSSL_free(ctx->manufacturer);
 	OPENSSL_free(ctx->description);
 	OPENSSL_free(ctx->_private);
