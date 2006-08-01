81fd37e61750eeb086125ab7e5794f3ce714d09f
nils <nils@c6295689-39f2-0310-b995-f0e70906c6a9>
2006-08-01 18:49:08 +0000

    fix sc_compare_path_prefix(); patch supplied by Henryk Plötz <henryk@ploetzli.ch>
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@3002 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/sc.c
+++ src/libopensc/sc.c
@@ -331,8 +331,8 @@ int sc_compare_path_prefix(const sc_path_t *prefix, const sc_path_t *path)
 	if (prefix->len > path->len)
 		return 0;
 
-	tpath      = *path;
-	tpath.len -= prefix->len;
+	tpath     = *path;
+	tpath.len = prefix->len;
 
 	return sc_compare_path(&tpath, prefix);
 }
