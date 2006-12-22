d08c8344ecf5fd3d2b0fb1bf712946970a62cd20
nils <nils@c6295689-39f2-0310-b995-f0e70906c6a9>
2006-12-22 12:43:00 +0000

    encode max pin length as well
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@3092 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/pkcs15-pin.c
+++ src/libopensc/pkcs15-pin.c
@@ -141,6 +141,8 @@ int sc_pkcs15_encode_aodf_entry(sc_context_t *ctx,
 	sc_format_asn1_entry(asn1_pin_attr + 1, &pin->type, NULL, 1);
 	sc_format_asn1_entry(asn1_pin_attr + 2, &pin->min_length, NULL, 1);
 	sc_format_asn1_entry(asn1_pin_attr + 3, &pin->stored_length, NULL, 1);
+	if (pin->max_length > 0)
+		sc_format_asn1_entry(asn1_pin_attr + 4, &pin->max_length, NULL, 1);
 	if (pin->reference >= 0)
 		sc_format_asn1_entry(asn1_pin_attr + 5, &pin->reference, NULL, 1);
 	/* FIXME: check if pad_char present */
