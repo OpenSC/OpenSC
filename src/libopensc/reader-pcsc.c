71bef700bfcf025ab8a9c40f0a6b507dcbccbb9c
ludovic.rousseau <ludovic.rousseau@c6295689-39f2-0310-b995-f0e70906c6a9>
2007-02-06 14:17:02 +0000

    use SCARD_SCOPE_USER instead of the pcsc-lite specific SCARD_SCOPE_GLOBAL
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@3116 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/reader-pcsc.c
+++ src/libopensc/reader-pcsc.c
@@ -60,7 +60,6 @@
 #undef SCARD_PROTOCOL_ANY
 #define SCARD_PROTOCOL_ANY (SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
 #ifdef _WIN32
-#define SCARD_SCOPE_GLOBAL SCARD_SCOPE_USER
 
 /* Error printing */
 #define PCSC_ERROR(ctx, desc, rv) sc_error(ctx, desc ": %lx\n", rv);
@@ -716,7 +715,7 @@ static int pcsc_init(sc_context_t *ctx, void **reader_data)
 	struct pcsc_global_private_data *gpriv;
 	scconf_block *conf_block;
 
-	rv = SCardEstablishContext(SCARD_SCOPE_GLOBAL,
+	rv = SCardEstablishContext(SCARD_SCOPE_USER,
                               NULL, NULL, &pcsc_ctx);
 	if (rv != SCARD_S_SUCCESS)
 		return pcsc_ret_to_error(rv);
