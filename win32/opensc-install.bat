d1f6df897e3a190f21b95b50845b02c8f93272d8
alonbl <alonbl@c6295689-39f2-0310-b995-f0e70906c6a9>
2008-04-13 17:51:20 +0000

    Add simple Windows installer script for OpenSC
    As nobody want to maintain UI installer, at least
    provide an installation script.
    
    The opensc-install.bat should be run from the
    installed location.
    
    
    
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@3473 c6295689-39f2-0310-b995-f0e70906c6a9

---
+++ win32/opensc-install.bat
@@ -0,0 +1,40 @@
+@echo off
+
+rem This script installs OpenSC
+rem Parameters:
+rem     user - Install for this user only.
+
+setlocal
+
+set MODE=%1
+
+set KEY=HKEY_LOCAL_MACHINE
+if "%MODE%" == "user" set KEY=HKEY_CURRENT_USER
+
+cd %0\..\..
+
+if not exist bin\opensc-tool.exe goto error
+
+for /f %%f in (".") do set OPENSC_HOME=%%~ff
+
+set OPENSC_HOME_ESCAPED=%OPENSC_HOME:\=\\%
+set REG_FILE=%TEMP%\opensc-install.reg
+
+echo Windows Registry Editor Version 5.00 > %REG_FILE%
+echo [%KEY%\SOFTWARE\OpenSC] >> %REG_FILE%
+echo "ConfigFile"="%OPENSC_HOME_ESCAPED%\\etc\\opensc.conf" >> %REG_FILE%
+
+regedit /s %REG_FILE%
+del /q %REG_FILE%
+
+"%OPENSC_HOME%\bin\opensc-tool" -S "app:default:profile_dir:%OPENSC_HOME%\share\opensc"
+
+goto end
+
+:error
+echo Invalid installation
+goto end
+
+:end
+
+endlocal
