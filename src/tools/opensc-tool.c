b2c5ab32092b6b04a6418d863881f7b787a5f9ad
aj <aj@c6295689-39f2-0310-b995-f0e70906c6a9>
2009-12-03 07:03:53 +0000

    Thomas Uhle: Add information about sun compiler to opensc-tool.
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@3875 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/tools/opensc-tool.c
+++ src/tools/opensc-tool.c
@@ -104,6 +104,15 @@ static int opensc_info(void)
 #endif
 		__VERSION__
 	);
+#elif defined(__SUNPRO_C)
+	printf (
+		"[Sun C %x.%x]\n",
+#if __SUNPRO_C > 0x590
+		(__SUNPRO_C >> 12), (__SUNPRO_C >> 8) & 0xFF
+#else
+		(__SUNPRO_C >>  8), (__SUNPRO_C >> 4) & 0xF
+#endif
+	);
 #elif defined(_MSC_VER)
 	printf ("[Microsoft %d]\n", _MSC_VER);
 #else
