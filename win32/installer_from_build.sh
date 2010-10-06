dcd077243fbd164531f84fdf4e78d5dc272a1eb5
martin <martin@c6295689-39f2-0310-b995-f0e70906c6a9>
2010-10-06 07:10:50 +0000

    WindowsInstaller: simplify the installer creation process
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@4794 c6295689-39f2-0310-b995-f0e70906c6a9

--- win32/installer_from_build.sh
+++ win32/installer_from_build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set -ex
-build_dir=$1
+build_dir=${1:-win32/build}
+
+if ! [ -d ${build_dir} ]; then
+	svn co http://www.opensc-project.org/svn/build/trunk ${build_dir}
+	mkdir -p ${build_dir}/sources
+fi
+
+# build a fresh targzip
+make dist
+rm -f ${build_dir}/sources/opensc*
+cp opensc-0.12.0-svn.tar.gz ${build_dir}/sources/
+
 # use mingw to generate binaries
 (cd ${build_dir}; CHOST=i586-mingw32msvc CBUILD=i686-pc-linux-gnu ./build)
+
 # Copy files
 cp ${build_dir}/image/opensc/etc/opensc.conf win32
 
 mkdir -p win32/opensc
 cp ${build_dir}/image/opensc/bin/*.dll ${build_dir}/image/opensc/bin/*.exe win32/opensc
 cp ${build_dir}/image/opensc/share/opensc/*.profile win32/opensc
+
 mkdir -p win32/engine_pkcs11
 cp ${build_dir}/image/engine_pkcs11/bin/libp11-1.dll ${build_dir}/image/engine_pkcs11/lib/engines/engine_pkcs11.dll win32/engine_pkcs11
 
