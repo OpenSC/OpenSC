d3a5a1389f5e429b57530ef2db5b43664e0cfcc4
ludovic.rousseau <ludovic.rousseau@c6295689-39f2-0310-b995-f0e70906c6a9>
2011-02-05 20:48:56 +0000

    Remove dead code
    pkcs15-muscle.c:101:3: warning: Value stored to 'type' is never read
                    type = SC_PKCS15INIT_USER_PIN;
                    ^      ~~~~~~~~~~~~~~~~~~~~~~
    pkcs15-muscle.c:99:3: warning: Value stored to 'type' is never read
                    type = SC_PKCS15INIT_SO_PIN;
                    ^      ~~~~~~~~~~~~~~~~~~~~
    
    
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@5147 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/pkcs15init/pkcs15-muscle.c
+++ src/pkcs15init/pkcs15-muscle.c
@@ -94,12 +94,6 @@ muscle_create_pin(sc_profile_t *profile, sc_pkcs15_card_t *p15card,
 	sc_file_t *file;
 	sc_pkcs15_pin_info_t *pin_info = (sc_pkcs15_pin_info_t *) pin_obj->data;
 	int r;
-	int type;
-	if (pin_info->flags & SC_PKCS15_PIN_FLAG_SO_PIN) {
-		type = SC_PKCS15INIT_SO_PIN;
-	} else {
-		type = SC_PKCS15INIT_USER_PIN;
-	}
 	if ((r = sc_select_file(p15card->card, &df->path, &file)) < 0)
 		return r;
 	if ((r = sc_pkcs15init_authenticate(profile, p15card, file, SC_AC_OP_WRITE)) < 0)
