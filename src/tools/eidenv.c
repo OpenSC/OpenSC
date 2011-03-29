4393f7231ea0230624885d879e2f6680d687bd52
martin <martin@c6295689-39f2-0310-b995-f0e70906c6a9>
2011-03-29 11:36:39 +0000

    eidenv: update copyright and correct used license to match the rest of OpenSC.
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@5272 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/tools/eidenv.c
+++ src/tools/eidenv.c
@@ -83,8 +83,8 @@ static void show_version(void)
 	fprintf(stderr,
 		"eidenv - EstEID utility version " PACKAGE_VERSION "\n"
 		"\n"
-		"Copyright (c) 2004 Martin Paljak <martin@paljak.pri.ee>\n"
-		"Licensed under GPL v2\n");
+		"Copyright (c) 2004 Martin Paljak <martin@martinpaljak.net>\n"
+		"Licensed under LGPL v2\n");
 }
 
 static void show_help(void)
