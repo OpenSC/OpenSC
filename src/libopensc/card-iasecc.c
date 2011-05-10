cbecf23395e19acd42076f752d4b2362e47a8616
vtarasov <vtarasov@c6295689-39f2-0310-b995-f0e70906c6a9>
2011-05-10 08:24:27 +0000

    iasecc: when getting data for qualified signature, take the last SHAxx block from the input data ...
    ... and not from the SHAxx-CTX, to avoid big/little endian uncertainty
    
    
    git-svn-id: https://www.opensc-project.org/svnp/opensc/trunk@5435 c6295689-39f2-0310-b995-f0e70906c6a9

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -2745,10 +2745,12 @@ iasecc_qsign_data_sha1(struct sc_context *ctx, const unsigned char *in, size_t i
 	int hh_size = sizeof(SHA_LONG), hh_num = SHA_DIGEST_LENGTH / sizeof(SHA_LONG);
 
 	LOG_FUNC_CALLED(ctx);
-	sc_log(ctx, "sc_pkcs15_get_qsign_data() input data length %i", in_len);
 
 	if (!in || !in_len || !out)
 		LOG_FUNC_RETURN(ctx, SC_ERROR_INVALID_ARGUMENTS);
+
+	sc_log(ctx, "sc_pkcs15_get_qsign_data() input data length %i", in_len);
+	memset(out, 0, sizeof(struct iasecc_qsign_data));
 
 	SHA1_Init(&sha);
 	SHA1_Update(&sha, in, in_len);
@@ -2768,10 +2770,11 @@ iasecc_qsign_data_sha1(struct sc_context *ctx, const unsigned char *in, size_t i
 		out->counter_long = out->counter_long*0x100 + out->counter[ii];
 	sc_log(ctx, "Pre counter(%li):%s", out->counter_long, sc_dump_hex(out->counter, sizeof(out->counter)));
 
-	for (ii=0;ii<sha.num; ii++)
-		out->last_block[ii] = (sha.data[ii/hh_size] >> 8*(ii%hh_size)) &0xFF;
-	out->last_block_size = sha.num;
-	sc_log(ctx, "Last block(%i):%s", out->last_block_size, sc_dump_hex(out->last_block, out->last_block_size));
+	if (sha.num)   {
+		memcpy(out->last_block, in + in_len - sha.num, sha.num);
+		out->last_block_size = sha.num;
+		sc_log(ctx, "Last block(%i):%s", out->last_block_size, sc_dump_hex(out->last_block, out->last_block_size));
+	}
 
 	SHA1_Final(out->hash, &sha);
 	out->hash_size = SHA_DIGEST_LENGTH;
@@ -2795,6 +2798,7 @@ iasecc_qsign_data_sha256(struct sc_context *ctx, const unsigned char *in, size_t
 		LOG_FUNC_RETURN(ctx, SC_ERROR_INVALID_ARGUMENTS);
 	
 	sc_log(ctx, "sc_pkcs15_get_qsign_data() input data length %i", in_len);
+	memset(out, 0, sizeof(struct iasecc_qsign_data));
 
 	SHA256_Init(&sha256);
 	SHA256_Update(&sha256, in, in_len);
@@ -2803,24 +2807,22 @@ iasecc_qsign_data_sha256(struct sc_context *ctx, const unsigned char *in, size_t
 		for(ii=0; ii<hh_size; ii++) 
 			out->pre_hash[jj*hh_size + ii] = ((sha256.h[jj] >> 8*(hh_size-1-ii)) & 0xFF);
 	out->pre_hash_size = SHA256_DIGEST_LENGTH;
+	sc_log(ctx, "Pre hash:%s", sc_dump_hex(out->pre_hash, out->pre_hash_size));
 
 	pre_hash_Nl = sha256.Nl - (sha256.Nl % (sizeof(sha256.data) * 8));
 	for (ii=0; ii<hh_size; ii++)   {
 		out->counter[ii] = (sha256.Nh >> 8*(hh_size-1-ii)) &0xFF;
 		out->counter[hh_size+ii] = (pre_hash_Nl >> 8*(hh_size-1-ii)) &0xFF;
 	}
-
 	for (ii=0, out->counter_long=0; ii<sizeof(out->counter); ii++)
 		out->counter_long = out->counter_long*0x100 + out->counter[ii];
-
-	sc_log(ctx, "Pre hash:%s", sc_dump_hex(out->pre_hash, out->pre_hash_size));
 	sc_log(ctx, "Pre counter(%li):%s", out->counter_long, sc_dump_hex(out->counter, sizeof(out->counter)));
 
-	for (ii=0;ii<sha256.num; ii++) 
-		out->last_block[ii] = (sha256.data[ii/hh_size] >> 8*(ii%hh_size)) &0xFF;
-
-	out->last_block_size = sha256.num;
-	sc_log(ctx, "Last block(%i):%s", out->last_block_size, sc_dump_hex(out->last_block, out->last_block_size));
+	if (sha256.num)   {
+		memcpy(out->last_block, in + in_len - sha256.num, sha256.num);
+		out->last_block_size = sha256.num;
+		sc_log(ctx, "Last block(%i):%s", out->last_block_size, sc_dump_hex(out->last_block, out->last_block_size));
+	}
 
 	SHA256_Final(out->hash, &sha256);
 	out->hash_size = SHA256_DIGEST_LENGTH;
@@ -2828,6 +2830,7 @@ iasecc_qsign_data_sha256(struct sc_context *ctx, const unsigned char *in, size_t
 
 	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 }
+
 
 static int 
 iasecc_compute_signature_dst(struct sc_card *card, 
