d43d0c35d90abf1ceee5b3dd1a104b66f68b6fc1
Viktor Tarasov <oltavic@gmail.com>
2012-12-07 18:01:08 +0100

    return to normal

--- src/common/compat_getopt.c
+++ src/common/compat_getopt.c
@@ -48,9 +48,10 @@ int my_getopt(int argc, char * argv[], const char *opts)
   char mode, colon_mode;
   int off = 0, opt = -1;
 
+#if 0
   colon_mode = 0;
   int fails_on_windows = 0;
-
+#endif
 
   if(getenv("POSIXLY_CORRECT")) colon_mode = mode = '+';
   else {
