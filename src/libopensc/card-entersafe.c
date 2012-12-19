1aefadf2adc50a88e901bc6ae66df92e08fc5232
entersafe <dev@entersafe.com>
2012-12-19 18:47:46 +0800

    check factor is padded with zero or not

--- src/libopensc/card-entersafe.c
+++ src/libopensc/card-entersafe.c
@@ -1147,15 +1147,40 @@ static int entersafe_write_rsa_key_factor(sc_card_t *card,
 	}
 
 	{/* Write 'x'; */
-		u8 sbuff[SC_MAX_APDU_BUFFER_SIZE];
-
+		 u8 sbuff[SC_MAX_APDU_BUFFER_SIZE] = { 0 , };
 		 sc_format_apdu(card,&apdu,SC_APDU_CASE_3_SHORT,0x46,factor,0x00);
-
 		 memcpy(sbuff,data.data,data.len);
 		 entersafe_reverse_buffer(sbuff,data.len);
+/*  
+ *  PK01C and PK13C smart card only support 1024 or 2048bit key .
+ *  Size of exponent1 exponent2 coefficient of RSA private key keep the same as size of prime1 
+ *  So check factor is padded with zero or not
+ */		 
+		 switch(factor){
+			 case 0x3:
+			 case 0x4:
+			 case 0x5:
+				 {
+					 if( data.len > 32 && data.len < 64 )
+					 {
+						 for(r = data.len ; r < 64 ; r ++)
+							 sbuff[r] = 0;
+						 data.len = 64;
+					 }	 
+					 else if( data.len > 64 && data.len < 128 )
+					 {
+						 for(r = data.len ; r < 128 ; r ++)
+							 sbuff[r] = 0;
+						 data.len = 128;
+					 }
+				 }
+				 break;
+			 default:
+				 break;
+		 }
+
 		 apdu.data=sbuff;
 		 apdu.lc=apdu.datalen=data.len;
-
 		 r = entersafe_transmit_apdu(card,&apdu,0,0,0,0);
 		 SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
 		 SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, sc_check_sw(card, apdu.sw1, apdu.sw2),"Write prkey factor failed");
@@ -1165,8 +1190,7 @@ static int entersafe_write_rsa_key_factor(sc_card_t *card,
 
 static int entersafe_write_large_rsa_key(sc_card_t *card,u8 key_id,struct sc_pkcs15_prkey_rsa *rsa)
 {
-	 int r;
-
+	int r;
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
 
 	 {/* write prkey */
