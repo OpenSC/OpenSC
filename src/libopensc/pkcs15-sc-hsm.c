99af6cd8ee78776f50bc016fc230541072c60afb
Andreas Schwier <andreas.schwier@cardcontact.de>
2013-01-24 14:09:02 +0100

    sc-hsm: Fixed a bug that prevents a newly generated 2048 key to show up at the PKCS#11 interface

--- src/libopensc/pkcs15-sc-hsm.c
+++ src/libopensc/pkcs15-sc-hsm.c
@@ -268,7 +268,7 @@ static int sc_pkcs15emu_sc_hsm_add_pubkey(sc_pkcs15_card_t *p15card, sc_pkcs15_p
 	sc_pkcs15_pubkey_info_t pubkey_info;
 	sc_pkcs15_object_t pubkey_obj;
 	struct sc_pkcs15_pubkey pubkey;
-	u8 efbin[512];
+	u8 efbin[1024];
 	sc_cvc_t cvc;
 	u8 *cvcpo;
 	size_t cvclen;
