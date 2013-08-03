daa79054c02bb0f3ce061b5bfc327ad2f8bf431d
Viktor Tarasov <viktor.tarasov@gmail.com>
2013-08-03 21:46:44 +0200

    CardOS: more debug messages in 'pin-cmd' handler

--- src/libopensc/card-cardos.c
+++ src/libopensc/card-cardos.c
@@ -1163,21 +1163,35 @@ cardos_card_ctl(sc_card_t *card, unsigned long cmd, void *ptr)
  * Unfortunately, it doesn't seem to work without this flag :-/
  */
 static int
-cardos_pin_cmd(sc_card_t *card, struct sc_pin_cmd_data *data,
+cardos_pin_cmd(struct sc_card *card, struct sc_pin_cmd_data *data,
 		 int *tries_left)
 {
+	struct sc_context *ctx = card->ctx;
+	int rv;
+
+	LOG_FUNC_CALLED(card->ctx);
+
 	data->flags |= SC_PIN_CMD_NEED_PADDING;
 	data->pin_reference |= 0x80;
+
+	sc_log(ctx, "PIN_CMD(cmd:%i, ref:%i)", data->cmd, data->pin_reference);
+	sc_log(ctx, "PIN1(max:%i, min:%i)", data->pin1.max_length, data->pin1.min_length);
+	sc_log(ctx, "PIN2(max:%i, min:%i)", data->pin2.max_length, data->pin2.min_length);
+
 	/* FIXME: the following values depend on what pin length was
 	 * used when creating the BS objects */
 	if (data->pin1.max_length == 0)
 		data->pin1.max_length = 8;
 	if (data->pin2.max_length == 0)
 		data->pin2.max_length = 8;
-	return iso_ops->pin_cmd(card, data, tries_left);
+
+	rv = iso_ops->pin_cmd(card, data, tries_left);
+	LOG_FUNC_RETURN(ctx, rv);
 }
 
-static int cardos_logout(sc_card_t *card)
+
+static int
+cardos_logout(sc_card_t *card)
 {
 	if (card->type == SC_CARD_TYPE_CARDOS_M4_01 ||
 	    card->type == SC_CARD_TYPE_CARDOS_M4_2) {
