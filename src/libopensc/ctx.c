c386bbdfdbca329bc67de2be4b426b3635874fba
Martin Paljak <martin@martinpaljak.net>
2014-03-16 16:39:26 +0000

    Remove the bogus javacard "driver".
    I had some ideas on how to improve the experience for JavaCards
    but that path did not get implemented further and my approach now
    is very different.
    
    While it would be nice if OpenSC (command line) detected
    somehow blank JavaCard-s and informed the user that steps a-b-c
    should be taken to make use of it, it doesn't really fit in.

--- src/libopensc/ctx.c
+++ src/libopensc/ctx.c
@@ -108,8 +108,6 @@ static const struct _sc_driver_entry internal_card_drivers[] = {
 	{ "atrust-acos",(void *(*)(void)) sc_get_atrust_acos_driver },
 	{ "PIV-II",	(void *(*)(void)) sc_get_piv_driver },
 	{ "itacns",	(void *(*)(void)) sc_get_itacns_driver },
-	/* javacard without supported applet - last before default */
-	{ "javacard",	(void *(*)(void)) sc_get_javacard_driver },
 	/* The default driver should be last, as it handles all the
 	 * unrecognized cards. */
 	{ "default",	(void *(*)(void)) sc_get_default_driver },
