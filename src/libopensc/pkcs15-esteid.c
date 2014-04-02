d0677eee933c6c912665f465949873df9af94ded
Raul Metsma <raul@metsma.ee>
2014-04-02 09:48:14 +0300

    Fix memory leak, sc_pkcs15_cert_t *cert is never freed

--- src/libopensc/pkcs15-esteid.c
+++ src/libopensc/pkcs15-esteid.c
@@ -118,10 +118,13 @@ sc_pkcs15emu_esteid_init (sc_pkcs15_card_t * p15card)
 			r = sc_pkcs15_read_certificate(p15card, &cert_info, &cert);
 			if (r == SC_SUCCESS) {
 				mem = BIO_new_mem_buf(cert->data.value, cert->data.len);
-				if (!mem)
+				if (!mem) {
+					sc_pkcs15_free_certificate(cert);
 					return SC_ERROR_INTERNAL;
+				}
 				x509 = d2i_X509_bio(mem, NULL);
 				BIO_free(mem);
+				sc_pkcs15_free_certificate(cert);
 				if (!x509)
 					return SC_ERROR_INTERNAL;
 				r = X509_NAME_get_index_by_NID(X509_get_subject_name(x509), NID_commonName, -1);
