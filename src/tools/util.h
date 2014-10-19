9096a9c216d19f470d500e79ace88db939583252
Sumedha Widyadharma <sumedha@widyadharma.de>
2014-10-19 10:09:06 +0200

    util: refer to util_getpass, behaviour should be documented there

--- src/tools/util.h
+++ src/tools/util.h
@@ -39,7 +39,7 @@ int util_getpass (char **lineptr, size_t *n, FILE *stream);
 
 /* Get a PIN (technically just a string). The source depends on the value of *input:
  * env:<var> - get from the environment variable <var>
- * -         - read until a linebreak or a maximum of 128 (MAX_PASS_SIZE) characters from stdin
+ * -         - read from stdin (uses util_getpass)
  * otherwise - copy *input
  *
  * All pins are freeable
