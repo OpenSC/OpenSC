c3a0bbc71583e80f9e794aca0896040c693db53e
Mathias Brossard <mathias@brossard.org>
2015-01-20 23:14:36 -0800

    Update email and year

--- src/pkcs11/pkcs11-spy.c
+++ src/pkcs11/pkcs11-spy.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003 Mathias Brossard <mathias.brossard@idealx.com>
+ * Copyright (C) 2015 Mathias Brossard <mathias@brossard.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
