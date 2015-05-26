76517b7d433f6d9e4a16a2217974804c339ef692
German Blanco <german.blanco@gmail.com>
2015-05-26 21:44:13 +0200

    Fixing part of the memory allocation problems in DNIe module.

--- src/libopensc/cwa14890.c
+++ src/libopensc/cwa14890.c
@@ -1397,6 +1397,10 @@ int cwa_create_secure_channel(sc_card_t * card,
 	/* arriving here means ok: cleanup */
 	res = SC_SUCCESS;
  csc_end:
+	if (icc_cert)
+		X509_free(icc_cert);
+	if (ca_cert)
+		X509_free(ca_cert);
 	if (icc_pubkey)
 		EVP_PKEY_free(icc_pubkey);
 	if (ifd_privkey)
--- src/libopensc/pkcs15-dnie.c
+++ src/libopensc/pkcs15-dnie.c
@@ -42,13 +42,19 @@ int dump_ef(sc_card_t * card, const char *path, u8 * buf, size_t * buf_len)
 {
 	int rv;
 	sc_file_t *file = sc_file_new();
-	sc_format_path(path, &file->path);
-	rv = sc_select_file(card, &file->path, &file);
-	if (rv < 0)
+	sc_path_t scpath;
+	sc_format_path(path, &scpath);
+	rv = sc_select_file(card, &scpath, &file);
+	if (rv < 0) {
+		sc_file_free(file);
 		return rv;
-	if (file->size > *buf_len)
+	}
+	if (file->size > *buf_len) {
+		sc_file_free(file);
 		return SC_ERROR_BUFFER_TOO_SMALL;
+	}
 	rv = sc_read_binary(card, 0, buf, file->size, 0);
+	sc_file_free(file);
 	if (rv < 0)
 		return rv;
 	*buf_len = rv;
