fb65e047178cd918fc43f53eafc6289b4cdd980f
vletoux <vincent.letoux@gmail.com>
2015-10-31 17:51:51 +0100

    win32: fix compilation error

--- src/common/compat_report_rangecheckfailure.c
+++ src/common/compat_report_rangecheckfailure.c
@@ -6,15 +6,15 @@
 
 // do not fail when linked with /GS dll and when /GS is not available
 #ifdef _WIN32
+#if _MSC_VER < 1700 // only for vs 2012 or later
 
 #include <Windows.h>
-
-#if _MSC_VER < 1700 // only for vs 2012 or later
 
 __declspec(noreturn) void __cdecl __report_rangecheckfailure()
 {
 	ExitProcess(1);
 }
 
+#endif
 #endif
 #endif
