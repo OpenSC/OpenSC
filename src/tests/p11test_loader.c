28d65a2c0c12998735d9bc72f848c3a87dbd2a1e
Jakub Jelen <jjelen@redhat.com>
2016-05-16 14:49:19 +0200

    License update based on the original project

--- src/tests/p11test_loader.c
+++ src/tests/p11test_loader.c
@@ -1,3 +1,25 @@
+/*
+ * p11test_loader.c: Library loader for PKCS#11 test suite
+ *
+ * Copyright (C) 2016 Martin Strhársky <strharsky.martin@gmail.com>
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Author: Jakub Jelen <jjelen@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "p11test_loader.h"
 
 static void *pkcs11_so;
