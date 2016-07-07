8654f3c528d4fcf503b98195d74d64c5522921e1
Jakub Jelen <jjelen@redhat.com>
2016-07-07 16:36:29 +0200

    Possibility to validate the testsuite agains software tokens

---
+++ src/tests/p11test/runtest.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# runtest.sh: Run test on existing card with possible initialization
+#
+# Copyright (C) 2016 Red Hat, Inc.
+#
+# Author: Jakub Jelen <jjelen@redhat.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#set -x
+SOPIN="12345678"
+PIN="123456"
+export GNUTLS_PIN=$PIN 
+GENERATE_KEYS=1
+
+function generate_cert() {
+	TYPE="$1"
+	ID="$2"
+	LABEL="$3"
+
+	# Generate key pair
+	p11tool --generate-$TYPE --login --provider="$P11LIB" --label="$LABEL"
+	if [[ "$?" -ne "0" ]]; then
+		echo "Couldn't generate $TYPE key pair"
+		return 1
+	fi
+	# Generate certificate
+	certtool --generate-self-signed --outfile="$TYPE.cert" --template=cert.cfg \
+		--provider="$P11LIB" --load-privkey "pkcs11:object=$LABEL;type=private"
+	# Write certificate
+	p11tool --login --write --load-certificate="$TYPE.cert" --label="$LABEL" \
+		--provider="$P11LIB"
+	rm "$TYPE.cert"
+	# Fix ID
+	p11tool --login --set-id=$ID --provider="$P11LIB" "pkcs11:object=$LABEL;type=cert"
+	p11tool --login --set-id=$ID --provider="$P11LIB" "pkcs11:object=$LABEL;type=private"
+	p11tool --login --set-id=$ID --provider="$P11LIB" "pkcs11:object=$LABEL;type=public"
+	# p11tool --login --provider="$P11LIB" --list-all
+}
+
+function card_setup() {
+		case $1 in
+			"softhsm")
+				P11LIB="/usr/lib64/pkcs11/libsofthsm2.so"
+				echo "directories.tokendir = .tokens/" > .softhsm2.conf
+				mkdir ".tokens"
+				export SOFTHSM2_CONF=".softhsm2.conf"
+				# Init token
+				softhsm2-util --init-token --slot 0 --label "SC test" --so-pin="$SOPIN" --pin="$PIN"
+				;;
+			"opencryptoki")
+				P11LIB="/usr/lib64/pkcs11/libopencryptoki.so"
+				SO_PIN=87654321
+				SLOT_ID=3 # swtok slot
+				systemctl is-active pkcsslotd
+				if [[ "$?" -ne "0" ]]; then
+					echo "Opencryptoki needs pkcsslotd running"
+					exit 1
+				fi
+				echo "test_swtok" | /usr/sbin/pkcsconf -I -c $SLOT_ID -S $SO_PIN
+				/usr/sbin/pkcsconf -u -c $SLOT_ID -S $SO_PIN -n $PIN
+				;;
+			"readonly")
+				GENERATE_KEYS=0
+				if [[ ! -z "$2" && -f "$2" ]]; then
+					P11LIB="$2"
+				else
+					P11LIB="/usr/lib64/pkcs11/opensc-pkcs11.so"
+					P11LIB="../pkcs11/.libs/opensc-pkcs11.so"
+				fi
+				;;
+			*)
+				echo "Error: Missing argument."
+				echo "    Usage:"
+				echo "        runtest.sh [softhsm|opencryptoki|readonly [pkcs-library.so]]"
+				exit 1;
+				;;
+		esac
+
+		if [[ $GENERATE_KEYS -eq 1 ]]; then
+			# Generate RSA Key pair
+			generate_cert "rsa" "02" "RSA_auth"
+			# Generate ECC Key pair
+			generate_cert "ecc" "03" "ECC_auth"
+		fi
+}
+
+function card_cleanup() {
+		case $1 in
+			"softhsm")
+				rm .softhsm2.conf
+				rm -rf ".tokens"
+				;;
+		esac
+}
+
+card_setup "$@"
+
+make p11test
+./p11test -m "$P11LIB"
+
+card_cleanup "$@"
