f64c71da28d1c1401445f6e8fa45bc079d5b6cd9
Ian Young <ian@iay.org.uk>
2016-08-27 22:14:04 +0100

    Don't free file if pointer is null (#858)
    Protect against segmentation
    
    closes #854.

--- src/libopensc/card-entersafe.c
+++ src/libopensc/card-entersafe.c
@@ -493,7 +493,7 @@ static int entersafe_select_fid(sc_card_t *card,
 	path.len=2;
 
 	r = iso_ops->select_file(card,&path,&file);
-	if(r) sc_file_free(file);
+	if(r && file) sc_file_free(file);
 	SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
 
 	/* update cache */
