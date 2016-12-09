aecc485669c51f49411b38fa59cd0f3f642e0605
Frank Morgner <frankmorgner@gmail.com>
2016-12-09 12:54:06 +0100

    config: added  disable_popups  for internal UI

--- src/libopensc/card-dnie.c
+++ src/libopensc/card-dnie.c
@@ -222,9 +222,10 @@ int dnie_ask_user_consent(struct sc_card * card, const char *title, const char *
 	if ((title==NULL) || (message==NULL))
 		LOG_FUNC_RETURN(card->ctx, SC_ERROR_INVALID_ARGUMENTS);
 
-	if (GET_DNIE_UI_CTX(card).user_consent_enabled == 0) {
+	if (GET_DNIE_UI_CTX(card).user_consent_enabled == 0
+			|| card->ctx & SC_CTX_FLAG_ENABLE_POPUPS) {
 		sc_log(card->ctx,
-		       "User Consent is disabled in configuration file");
+		       "User Consent or popups are disabled in configuration file");
 		LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 	}
 #ifdef _WIN32
