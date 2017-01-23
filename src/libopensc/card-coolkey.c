6aa52ce7530717107b3a617cfb9a0890d12488d2
Jakub Jelen <jjelen@redhat.com>
2017-01-23 09:14:22 +0100

    coolkey: Properly initialize serial length (partially resolves #943)

--- src/libopensc/card-coolkey.c
+++ src/libopensc/card-coolkey.c
@@ -1360,6 +1360,7 @@ static int coolkey_get_serial_nr_from_CUID(sc_card_t* card, sc_serial_number_t* 
 
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_NORMAL);
 	memcpy(serial->value, &priv->cuid, sizeof(priv->cuid));
+	serial->len = sizeof(priv->cuid);
 	SC_FUNC_RETURN(card->ctx, SC_LOG_DEBUG_NORMAL, SC_SUCCESS);
 }
 
