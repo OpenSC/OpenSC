68bb3683e652de1161b9b4502973cac447c52ee8
Arnaud Fontaine <arnaud.fontaine@ssi.gouv.fr>
2017-08-11 11:56:14 +0200

    Unify iasecc_init for AMOS and SAGEM cards

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -514,7 +514,7 @@ iasecc_init_oberthur(struct sc_card *card)
 
 
 static int
-iasecc_init_sagem(struct sc_card *card)
+iasecc_init_amos_or_sagem(struct sc_card *card)
 {
 	struct sc_context *ctx = card->ctx;
 	unsigned int flags;
@@ -543,36 +543,6 @@ iasecc_init_sagem(struct sc_card *card)
 	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 }
 
-
-static int
-iasecc_init_amos(struct sc_card *card)
-{
-	struct sc_context *ctx = card->ctx;
-	unsigned int flags;
-	int rv = 0;
-
-	LOG_FUNC_CALLED(ctx);
-
-	flags = IASECC_CARD_DEFAULT_FLAGS;
-
-	_sc_card_add_rsa_alg(card, 1024, flags, 0x10001);
-	_sc_card_add_rsa_alg(card, 2048, flags, 0x10001);
-
-	card->caps = SC_CARD_CAP_RNG;
-	card->caps |= SC_CARD_CAP_APDU_EXT;
-	card->caps |= SC_CARD_CAP_USE_FCI_AC;
-
-	rv = iasecc_parse_ef_atr(card);
-	if (rv == SC_ERROR_FILE_NOT_FOUND)   {
-		rv = iasecc_select_mf(card, NULL);
-		LOG_TEST_RET(ctx, rv, "MF selection error");
-
-		rv = iasecc_parse_ef_atr(card);
-	}
-
-	LOG_TEST_RET(ctx, rv, "IASECC: ATR parse failed");
-	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
-}
 
 static int
 iasecc_mi_match(struct sc_card *card)
@@ -619,7 +589,7 @@ iasecc_init_mi(struct sc_card *card)
 
 	if (rv) {
 		card->type = SC_CARD_TYPE_IASECC_MI2;
-		rv = iasecc_init_sagem(card);
+		rv = iasecc_init_amos_or_sagem(card);
 	}
 
 	LOG_TEST_RET(ctx, rv, "Could not select MI's AID");
@@ -647,9 +617,9 @@ iasecc_init(struct sc_card *card)
 	else if (card->type == SC_CARD_TYPE_IASECC_OBERTHUR)
 		rv = iasecc_init_oberthur(card);
 	else if (card->type == SC_CARD_TYPE_IASECC_SAGEM)
-		rv = iasecc_init_sagem(card);
+		rv = iasecc_init_amos_or_sagem(card);
 	else if (card->type == SC_CARD_TYPE_IASECC_AMOS)
-		rv = iasecc_init_amos(card);
+		rv = iasecc_init_amos_or_sagem(card);
 	else if (card->type == SC_CARD_TYPE_IASECC_MI)
 		rv = iasecc_init_mi(card);
 	else
