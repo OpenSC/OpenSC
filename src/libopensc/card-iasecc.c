47159afdeea497f815ca09b06c6b3311e59f72c1
Arnaud Fontaine <arnaud.fontaine@ssi.gouv.fr>
2017-08-11 12:34:28 +0200

    Unify iasecc_init for AMOS/SAGEM and MI cards

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -514,37 +514,6 @@ iasecc_init_oberthur(struct sc_card *card)
 
 
 static int
-iasecc_init_amos_or_sagem(struct sc_card *card)
-{
-	struct sc_context *ctx = card->ctx;
-	unsigned int flags;
-	int rv = 0;
-
-	LOG_FUNC_CALLED(ctx);
-
-	flags = IASECC_CARD_DEFAULT_FLAGS;
-
-	_sc_card_add_rsa_alg(card, 1024, flags, 0x10001);
-	_sc_card_add_rsa_alg(card, 2048, flags, 0x10001);
-
-	card->caps = SC_CARD_CAP_RNG;
-	card->caps |= SC_CARD_CAP_APDU_EXT;
-	card->caps |= SC_CARD_CAP_USE_FCI_AC;
-
-	rv = iasecc_parse_ef_atr(card);
-	if (rv == SC_ERROR_FILE_NOT_FOUND)   {
-		rv = iasecc_select_mf(card, NULL);
-		LOG_TEST_RET(ctx, rv, "MF selection error");
-
-		rv = iasecc_parse_ef_atr(card);
-	}
-	LOG_TEST_RET(ctx, rv, "IASECC: ATR parse failed");
-
-	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
-}
-
-
-static int
 iasecc_mi_match(struct sc_card *card)
 {
 	struct sc_context *ctx = card->ctx;
@@ -562,8 +531,9 @@ iasecc_mi_match(struct sc_card *card)
 	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 }
 
+
 static int
-iasecc_init_mi(struct sc_card *card)
+iasecc_init_amos_or_sagem(struct sc_card *card)
 {
 	struct sc_context *ctx = card->ctx;
 	unsigned int flags;
@@ -582,20 +552,29 @@ iasecc_init_mi(struct sc_card *card)
 	card->caps |= SC_CARD_CAP_APDU_EXT;
 	card->caps |= SC_CARD_CAP_USE_FCI_AC;
 
-	resp_len = sizeof(resp);
-	rv = iasecc_select_aid(card, &MIIASECC_AID, resp, &resp_len);
+	if (card->type == SC_CARD_TYPE_IASECC_MI)   {
+		resp_len = sizeof(resp);
+		rv = iasecc_select_aid(card, &MIIASECC_AID, resp, &resp_len);
 
-	rv = iasecc_mi_match(card);
-
-	if (rv) {
-		card->type = SC_CARD_TYPE_IASECC_MI2;
-		rv = iasecc_init_amos_or_sagem(card);
+		rv = iasecc_mi_match(card);
+		if (rv)
+			card->type = SC_CARD_TYPE_IASECC_MI2;
+		else
+			LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 	}
 
-	LOG_TEST_RET(ctx, rv, "Could not select MI's AID");
+	rv = iasecc_parse_ef_atr(card);
+	if (rv == SC_ERROR_FILE_NOT_FOUND)   {
+		rv = iasecc_select_mf(card, NULL);
+		LOG_TEST_RET(ctx, rv, "MF selection error");
+
+		rv = iasecc_parse_ef_atr(card);
+	}
+	LOG_TEST_RET(ctx, rv, "IASECC: ATR parse failed");
 
 	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 }
+
 
 static int
 iasecc_init(struct sc_card *card)
@@ -621,7 +600,7 @@ iasecc_init(struct sc_card *card)
 	else if (card->type == SC_CARD_TYPE_IASECC_AMOS)
 		rv = iasecc_init_amos_or_sagem(card);
 	else if (card->type == SC_CARD_TYPE_IASECC_MI)
-		rv = iasecc_init_mi(card);
+		rv = iasecc_init_amos_or_sagem(card);
 	else
 		LOG_FUNC_RETURN(ctx, SC_ERROR_NO_CARD_SUPPORT);
 
