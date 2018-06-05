a5c6e53122da7f3394dacb3e27f2758fbbaae70d
Peter Marschall <peter@adpm.de>
2018-06-05 10:28:27 +0200

    add parentheses to keep travis happy

--- src/libopensc/sc.c
+++ src/libopensc/sc.c
@@ -956,8 +956,8 @@ const u8 *sc_compacttlv_find_tag(const u8 *buf, size_t len, u8 tag, size_t *outl
 		size_t expected_len = tag & 0x0F;
 
 	        for (idx = 0; idx < len; idx++) {
-			if (buf[idx] & 0xF0 == plain_tag && idx + expected_len < len &&
-			    (expected_len == 0 || expected_len == buf[idx] & 0x0F)) {
+			if ((buf[idx] & 0xF0) == plain_tag && idx + expected_len < len &&
+			    (expected_len == 0 || expected_len == (buf[idx] & 0x0F))) {
 				if (outlen != NULL)
 					*outlen = buf[idx] & 0x0F;
 				return buf + (idx + 1);
