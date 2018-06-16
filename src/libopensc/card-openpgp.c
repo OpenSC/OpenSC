d03227bf9967e96661fc1c08c8046db881f36fe5
Peter Marschall <peter@adpm.de>
2018-06-16 19:34:39 +0200

    OpenPGP: improve get_full_pgp_aid()'s parameter checking

--- src/libopensc/card-openpgp.c
+++ src/libopensc/card-openpgp.c
@@ -365,10 +365,13 @@ struct pgp_priv_data {
 static int
 get_full_pgp_aid(sc_card_t *card, sc_file_t *file)
 {
-	int r = 0;
-	/* explicitly get the full aid */
-	r = sc_get_data(card, 0x004F, file->name, sizeof file->name);
-	file->namelen = MAX(r, 0);
+	int r = SC_ERROR_INVALID_ARGUMENTS;
+
+	if (file != NULL) {
+		/* explicitly get the full aid */
+		r = sc_get_data(card, 0x004F, file->name, sizeof file->name);
+		file->namelen = MAX(r, 0);
+	}
 
 	return r;
 }
