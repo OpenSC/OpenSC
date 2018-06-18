8ebb43d440858237d5f69c2d174d97b9009ce214
Hannu Honkanen <hannu.honkanen@aventra.fi>
2018-06-18 09:31:12 +0300

    Removed #ifdef USE_PKCS15_INIT around __pkcs15_create_secret_key_object. This function is now used also when reading and parsing a card, not only when creating new objects.

--- src/pkcs11/framework-pkcs15.c
+++ src/pkcs11/framework-pkcs15.c
@@ -794,7 +794,6 @@ __pkcs15_create_data_object(struct pkcs15_fw_data *fw_data,
 }
 
 
-#ifdef USE_PKCS15_INIT
 static int
 __pkcs15_create_secret_key_object(struct pkcs15_fw_data *fw_data,
 		struct sc_pkcs15_object *object, struct pkcs15_any_object **skey_object)
@@ -812,7 +811,6 @@ __pkcs15_create_secret_key_object(struct pkcs15_fw_data *fw_data,
 
 	return 0;
 }
-#endif
 
 
 static int
