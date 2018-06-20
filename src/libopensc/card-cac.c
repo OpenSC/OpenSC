54cb7c6e6f019b7bacb4c972f860f372a06d277d
Jakub Jelen <jjelen@redhat.com>
2018-06-20 12:52:17 +0200

    cac: Avoid segfaults from get_challenge()

--- src/libopensc/card-cac.c
+++ src/libopensc/card-cac.c
@@ -951,12 +951,13 @@ static int cac_get_challenge(sc_card_t *card, u8 *rnd, size_t len)
 {
 	/* CAC requires 8 byte response */
 	u8 rbuf[8];
+	u8 *rbufp = &rbuf[0];
 	size_t out_len = sizeof rbuf;
 	int r;
 
 	LOG_FUNC_CALLED(card->ctx);
 
-	r = cac_apdu_io(card, 0x84, 0x00, 0x00, NULL, 0, (u8 **) &rbuf, &out_len);
+	r = cac_apdu_io(card, 0x84, 0x00, 0x00, NULL, 0, &rbufp, &out_len);
 	LOG_TEST_RET(card->ctx, r, "Could not get challenge");
 
 	if (len < out_len) {
