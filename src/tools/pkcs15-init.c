8ba72be2d374331d6b6af5f5ab08b9413c9b401f
Priit Laes <plaes@plaes.org>
2018-08-22 22:00:45 +0300

    openssl: Drop all compatibility checks for <=openssl-1.0.2

--- src/tools/pkcs15-init.c
+++ src/tools/pkcs15-init.c
@@ -1344,20 +1344,8 @@ do_read_check_certificate(sc_pkcs15_cert_t *sc_oldcert,
 	r = SC_ERROR_INVALID_ARGUMENTS;
 	if (oldpk_type == newpk_type)
 	{
-#if  OPENSSL_VERSION_NUMBER >= 0x10002000L
 		if (EVP_PKEY_cmp(oldpk, newpk) == 1)
 			r = 0;
-#else
-		if ((oldpk_type == EVP_PKEY_DSA) &&
-			!BN_cmp(EVP_PKEY_get0_DSA(oldpk)->p, EVP_PKEY_get0_DSA(newpk)->p) &&
-			!BN_cmp(EVP_PKEY_get0_DSA(oldpk)->q, EVP_PKEY_get0_DSA(newpk)->q) &&
-			!BN_cmp(EVP_PKEY_get0_DSA(oldpk)->g, EVP_PKEY_get0_DSA(newpk)->g))
-				r = 0;
-		else if ((oldpk_type == EVP_PKEY_RSA) &&
-			!BN_cmp(EVP_PKEY_get0_RSA(oldpk)->n, EVP_PKEY_get0_RSA(newpk)->n) &&
-			!BN_cmp(EVP_PKEY_get0_RSA(oldpk)->e, EVP_PKEY_get0_RSA(newpk)->e))
-				r = 0;
-#endif
 	}
 
 	EVP_PKEY_free(newpk);
