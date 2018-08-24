OpenSC/OpenSC

commit 50b000047c44b1f2c6f0134094c19519d512e5b6

ias/ecc: disable iccsn parsing
if someone wants to implement this with memory bounds checking, please
raise your hands

src/libopensc/card-iasecc.c
@@ -2504,6 +2504,12 @@ iasecc_pin_cmd(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_le
 static int
 iasecc_get_serialnr(struct sc_card *card, struct sc_serial_number *serial)
 {
+#if 1
+	/* the current implementation doesn't perform any bounds check when parsing
+	 * the serial number. Hence, we disable this code until someone has time to
+	 * fix this. */
+	LOG_FUNC_RETURN(card->ctx, SC_ERROR_NOT_SUPPORTED);
+#else
 	struct sc_context *ctx = card->ctx;
 	struct sc_iin *iin = &card->serialnr.iin;
 	struct sc_apdu apdu;
@@ -2573,6 +2579,7 @@ end:
 		memcpy(serial, &card->serialnr, sizeof(*serial));
 
 	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
+#endif
 }
 
 

commit 5daec17e32df9f9d284c8792bdd67b70af9589c0

ias/ecc: ignore missing serial on card initialization
fixes problem in card detection introduced in
50b000047c44b1f2c6f0134094c19519d512e5b6

src/libopensc/card-iasecc.c
@@ -615,7 +615,7 @@ iasecc_init(struct sc_card *card)
 			LOG_TEST_RET(ctx, rv, "Select EF.ATR AID failed");
 		}
 
-		rv = iasecc_get_serialnr(card, NULL);
+		iasecc_get_serialnr(card, NULL);
 	}
 
 #ifdef ENABLE_SM