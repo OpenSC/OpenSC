6ba176fc791ee3c7ab97729bf27b74908a1ec903
Shahin Hajikhorasani <khorasani.shahin@yahoo.com>
2018-11-21 16:58:13 +0330

    Update reader-pcsc.c
    Error handling extended in case of changing the card reader

--- src/libopensc/reader-pcsc.c
+++ src/libopensc/reader-pcsc.c
@@ -339,6 +339,16 @@ static int refresh_attributes(sc_reader_t *reader)
 			reader->flags &= ~SC_READER_CARD_CHANGED;
 			LOG_FUNC_RETURN(reader->ctx, SC_SUCCESS);
 		}
+		
+		/* the system could not dectect any reader. It means, the prevoiusly attached reader is disconnected. */		
+		if (rv == SCARD_E_NO_READERS_AVAILABLE || rv == SCARD_E_SERVICE_STOPPED) {
+ 			if (old_flags & SC_READER_CARD_PRESENT) {
+ 				reader->flags |= SC_READER_CARD_CHANGED;
+ 			}
+			
+ 			SC_FUNC_RETURN(reader->ctx, SC_LOG_DEBUG_VERBOSE, SC_SUCCESS);
+ 		}
+
 		PCSC_TRACE(reader, "SCardGetStatusChange failed", rv);
 		return pcsc_to_opensc_error(rv);
 	}
