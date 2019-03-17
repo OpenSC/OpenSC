690cd81dd439f2bceda6c20adec214daeaa0f804
Raimonds Cicans <ray@apollo.lv>
2019-03-17 17:04:09 +0200

    Removed redundant checks from function iasecc_oberthur_match
    Motivation:
    - those checks relay solely on ATR value.
    - ATR is already validated in iasecc_match_card (complete match)
    - such checks must be maid once, by hands, when developer add support for new card
    - removed code prohibit easy adding 100% compatible Oberthur cards with different ATR format
      (example: LATVIA-eID - Oberthur's card for Latvian government)
    
    Developers should consider to inline function iasecc_oberthur_match into function iasecc_init_oberthur:
    - now function have nothing to do with "matching"
    - function is small (50% of code - function's generic header & footer)
    - function is used once

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -444,17 +444,8 @@ static int
 iasecc_oberthur_match(struct sc_card *card)
 {
 	struct sc_context *ctx = card->ctx;
-	unsigned char *hist = card->reader->atr_info.hist_bytes;
 
 	LOG_FUNC_CALLED(ctx);
-
-	if (*hist != 0x80 || ((*(hist+1)&0xF0) != 0xF0))
-		LOG_FUNC_RETURN(ctx, SC_ERROR_OBJECT_NOT_FOUND);
-
-	sc_log_hex(ctx, "AID in historical_bytes", hist + 2, *(hist+1) & 0x0F);
-
-	if (memcmp(hist + 2, OberthurIASECC_AID.value, *(hist+1) & 0x0F))
-		LOG_FUNC_RETURN(ctx, SC_ERROR_RECORD_NOT_FOUND);
 
 	if (!card->ef_atr)
 		card->ef_atr = calloc(1, sizeof(struct sc_ef_atr));
