f61d9b3b53aa97c3dad6f8d065c989786662878a
Jakub Jelen <jjelen@redhat.com>
2019-08-12 15:26:18 +0200

    Implement new Gemalto IDPrime driver
    The card is largely ISO 7816 compliant, but does not provide any
    simple way of listing the content which is supported by current
    PKCS#15 implementation therefore the PKCS#15 emulator had to be
    used.
    
    The certificates are compressed in a similar way as in DNIE
    cards which complicates reading from the card and which I think
    could be moved to the shared ISO (or some other file since I saw
    that code already many times).
    
    The card supports wide range of algorithms including
    RSA-PSS and RSA-OAEP padding schemes in-card. On the other hand,
    it does not allow raw RSA and SHA1 hashes on card anymore.
    
    The card is manufactured by Gemalto so it has strict ATR which
    can be used for detection.

--- src/libopensc/ctx.c
+++ src/libopensc/ctx.c
@@ -128,6 +128,7 @@ static const struct _sc_driver_entry internal_card_drivers[] = {
 	{ "atrust-acos",(void *(*)(void)) sc_get_atrust_acos_driver },
 	{ "westcos",	(void *(*)(void)) sc_get_westcos_driver },
 	{ "esteid2018",	(void *(*)(void)) sc_get_esteid2018_driver },
+	{ "idprime",	(void *(*)(void)) sc_get_idprime_driver },
 
 /* Here should be placed drivers that need some APDU transactions in the
  * driver's `match_card()` function. */
