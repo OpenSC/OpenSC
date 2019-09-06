ee1c8073c2ecd0d7644d64ef3f2a2be4b5752c6e
Frank Morgner <frankmorgner@gmail.com>
2019-09-06 16:02:43 +0200

    pkcs11-tool: allow tests with keys that don't require PIN
    ... such as the 9e key of a PIV card.

--- src/tools/pkcs11-tool.c
+++ src/tools/pkcs11-tool.c
@@ -775,7 +775,7 @@ int main(int argc, char * argv[])
 			opt_sig_format = optarg;
 			break;
 		case 't':
-			need_session |= NEED_SESSION_RO;
+			need_session |= NEED_SESSION_RW;
 			do_test = 1;
 			action_count++;
 			break;
