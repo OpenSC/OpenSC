611d1cea4c139c8f14088d3ac092c7d3ac648bca
Jakub Jelen <jjelen@redhat.com>
2019-10-24 11:46:53 +0200

    unittests: Verify BIT STRING parsing as integers

--- src/tests/unittests/asn1.c
+++ src/tests/unittests/asn1.c
@@ -234,6 +234,45 @@ static void torture_negative_int(void **state)
 	assert_int_equal(value, -224);
 }
 
+/*
+ * Test undefined behavior of too-large bit field (BIT STRING as an integer)
+ * https://oss-fuzz.com/testcase-detail/5764460018401280
+ *
+ * In this example, invalid "unused bytes" value was used
+ */
+static void torture_bit_field(void **state)
+{
+	/* Without the Tag and Length {0x03, 0x02} */
+	u8 data0[] = {0x07, 0x80};
+	/* Without the Tag and Length {0x03, 0x05} */
+	/* u8 data1[] = {0x20, 0x20, 0xff, 0xff, 0xff}; original data */
+	u8 data1[] = {0x20, 0xff, 0xff, 0xff, 0xff};
+	u8 data2[] = {0x00, 0xff, 0xff, 0xff, 0xff};
+	/* Without the Tag and Length {0x03, 0x06} */
+	u8 data3[] = {0x00, 0xff, 0xff, 0xff, 0xff, 0xff};
+	unsigned int value;
+	size_t value_len = sizeof(value);
+	int rv = 0;
+
+	/* Simple value 1 */
+	rv = decode_bit_field(data0, sizeof(data0), (unsigned int *)&value, value_len);
+	assert_int_equal(rv, SC_SUCCESS);
+	assert_int_equal(value, 1);
+
+	/* Too large unused bytes field */
+	rv = decode_bit_field(data1, sizeof(data1), (unsigned int *)&value, value_len);
+	assert_int_equal(rv, SC_ERROR_INVALID_ASN1_OBJECT);
+
+	/* This is the last value that can be represented in the unsigned int */
+	rv = decode_bit_field(data2, sizeof(data2), (unsigned int *)&value, value_len);
+	assert_int_equal(rv, SC_SUCCESS);
+	assert_int_equal(value, UINT_MAX);
+
+	/* Too large to represent in the unsigned int type */
+	rv = decode_bit_field(data3, sizeof(data3), (unsigned int *)&value, value_len);
+	assert_int_equal(rv, SC_ERROR_BUFFER_TOO_SMALL);
+
+}
 int main(void)
 {
 	int rc;
@@ -242,6 +281,7 @@ int main(void)
 		cmocka_unit_test(torture_integer),
 		cmocka_unit_test(torture_negative_int),
 		cmocka_unit_test(torture_oid),
+		cmocka_unit_test(torture_bit_field),
 	};
 
 	rc = cmocka_run_group_tests(tests, NULL, NULL);
