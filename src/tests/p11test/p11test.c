bfa8415ea3f5a0b055ef383863e2711b95b04ec8
pkubaj <pkubaj@users.noreply.github.com>
2019-10-30 15:34:58 +0000

    Fix build on ARM, PPC
    Those architectures use unsigned char by default, and command variable is later compared to -1.

--- src/tests/p11test/p11test.c
+++ src/tests/p11test/p11test.c
@@ -49,7 +49,7 @@ void display_usage() {
 }
 
 int main(int argc, char** argv) {
-	char command;
+	signed char command;
 	const struct CMUnitTest readonly_tests_without_initialization[] = {
 		/* Test card events on slot */
 		cmocka_unit_test_setup_teardown(wait_test,
