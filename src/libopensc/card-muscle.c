7360c4bf0c32002dc4d7c42d83e15889ddd83111
Jakub Jelen <jjelen@redhat.com>
2019-11-14 11:18:11 +0100

    muscle: Avoid access uninitialized memory

--- src/libopensc/card-muscle.c
+++ src/libopensc/card-muscle.c
@@ -93,7 +93,7 @@ static int muscle_match_card(sc_card_t *card)
 		apdu.resplen = 64;
 		apdu.resp = response;
 		r = sc_transmit_apdu(card, &apdu);
-		if (r == SC_SUCCESS && response[0] == 0x01) {
+		if (r == SC_SUCCESS && apdu.resplen > 1 && response[0] == 0x01) {
 			card->type = SC_CARD_TYPE_MUSCLE_V1;
 		} else {
 			card->type = SC_CARD_TYPE_MUSCLE_GENERIC;
