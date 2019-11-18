2d02de4233a70fdad4cbf0862c83f03d9ba85d78
Jakub Jelen <jjelen@redhat.com>
2019-11-18 12:59:03 +0100

    coolkey: Do not return uninitialized data if card does not return CPLP data
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=18990

--- src/libopensc/card-coolkey.c
+++ src/libopensc/card-coolkey.c
@@ -1096,8 +1096,16 @@ coolkey_get_cplc_data(sc_card_t *card, global_platform_cplc_data_t *cplc_data)
 {
 	size_t len = sizeof(global_platform_cplc_data_t);
 	u8 *receive_buf = (u8 *)cplc_data;
-	return coolkey_apdu_io(card, GLOBAL_PLATFORM_CLASS, ISO7816_INS_GET_DATA, 0x9f, 0x7f,
+	int rc;
+
+	rc = coolkey_apdu_io(card, GLOBAL_PLATFORM_CLASS, ISO7816_INS_GET_DATA, 0x9f, 0x7f,
 			NULL, 0, &receive_buf, &len,  NULL, 0);
+	/* We expect this will fill the whole structure in the argument.
+	 * If we got something else, report error */
+	if ((size_t)rc < sizeof(cplc_data)) {
+		LOG_FUNC_RETURN(card->ctx, SC_ERROR_CORRUPTED_DATA);
+	}
+	LOG_FUNC_RETURN(card->ctx, rc);
 }
 
 /* select the coolkey applet */
