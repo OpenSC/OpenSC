9dd337067306deda379d9f63e11565634f25b91f
Jakub Jelen <jjelen@redhat.com>
2020-06-04 16:32:40 +0200

    oberthur: Fix operator precedence
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=22665

--- src/libopensc/card-oberthur.c
+++ src/libopensc/card-oberthur.c
@@ -2173,7 +2173,7 @@ auth_read_binary(struct sc_card *card, unsigned int offset,
 				rv = SC_ERROR_UNKNOWN_DATA_RECEIVED;
 				goto err;
 			}
-			rv  = out_len - offset > count ? count : out_len - offset;
+			rv = ((out_len - offset) > count) ? count : (out_len - offset);
 			memcpy(buf, out + offset, rv);
 
 			sc_log_hex(card->ctx, "write_publickey", buf, rv);
