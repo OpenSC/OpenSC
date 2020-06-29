5098cfdb4034eb2b901edb0373fb8ecf866a42cd
Jakub Jelen <jjelen@redhat.com>
2020-06-29 18:53:16 +0200

    authentic: Do not leak memory on fail path
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=23788

--- src/libopensc/card-authentic.c
+++ src/libopensc/card-authentic.c
@@ -540,7 +540,10 @@ authentic_set_current_files(struct sc_card *card, struct sc_path *path,
 				file->path = *path;
 
 			rv = authentic_process_fci(card, file, resp, resplen);
-			LOG_TEST_RET(ctx, rv, "cannot set 'current file': FCI process error");
+			if (rv != SC_SUCCESS) {
+				sc_file_free(file);
+				LOG_TEST_RET(ctx, rv, "cannot set 'current file': FCI process error");
+			}
 
 			break;
 		default:
@@ -560,8 +563,10 @@ authentic_set_current_files(struct sc_card *card, struct sc_path *path,
 
 			if (cur_df_path.len)   {
 				if (cur_df_path.len + card->cache.current_df->path.len > sizeof card->cache.current_df->path.value
-						|| cur_df_path.len > sizeof card->cache.current_df->path.value)
+						|| cur_df_path.len > sizeof card->cache.current_df->path.value) {
+					sc_file_free(file);
 					LOG_FUNC_RETURN(ctx, SC_ERROR_UNKNOWN_DATA_RECEIVED);
+				}
 				memcpy(card->cache.current_df->path.value + cur_df_path.len,
 						card->cache.current_df->path.value,
 						card->cache.current_df->path.len);
