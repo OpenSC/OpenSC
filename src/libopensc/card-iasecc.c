d520003aea77318009d2520c7dd99624ef397e97
Zoltan Kelemen <zoltan.kelemen@saabgroup.com>
2020-07-01 09:09:10 +0200

    Improved PIN verification function: - Uses PIN padding from merged policy - Moved PIN-pad logic into this function instead of keeping separate

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -131,7 +131,6 @@ static int iasecc_process_fci(struct sc_card *card, struct sc_file *file, const 
 static int iasecc_get_serialnr(struct sc_card *card, struct sc_serial_number *serial);
 static int iasecc_sdo_get_data(struct sc_card *card, struct iasecc_sdo *sdo);
 static int iasecc_pin_get_policy (struct sc_card *card, struct sc_pin_cmd_data *data, struct iasecc_pin_policy *pin);
-static int iasecc_pin_is_verified(struct sc_card *card, struct sc_pin_cmd_data *pin_cmd, int *tries_left);
 static int iasecc_pin_get_status(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_left);
 static int iasecc_pin_get_info(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_left);
 static int iasecc_check_update_pin(struct sc_pin_cmd_data *data, struct sc_pin_cmd_pin *pin);
@@ -1911,34 +1910,6 @@ iasecc_se_at_to_chv_reference(struct sc_card *card, unsigned reference,
 
 
 static int
-iasecc_pin_is_verified(struct sc_card *card, struct sc_pin_cmd_data *pin_cmd_data,
-		int *tries_left)
-{
-	struct sc_context *ctx = card->ctx;
-	struct sc_pin_cmd_data pin_cmd;
-        struct sc_acl_entry acl = pin_cmd_data->pin1.acls[IASECC_ACLS_CHV_VERIFY];
-	int rv = SC_ERROR_SECURITY_STATUS_NOT_SATISFIED;
-
-	LOG_FUNC_CALLED(ctx);
-
-	if (pin_cmd_data->pin_type != SC_AC_CHV)
-		LOG_TEST_RET(ctx, SC_ERROR_NOT_SUPPORTED, "PIN type is not supported for the verification");
-
-	sc_log(ctx, "Verify ACL(method:%X;ref:%X)", acl.method, acl.key_ref);
-	if (acl.method != IASECC_SCB_ALWAYS)
-		LOG_FUNC_RETURN(ctx, SC_ERROR_SECURITY_STATUS_NOT_SATISFIED);
-
-	pin_cmd = *pin_cmd_data;
-	pin_cmd.pin1.data = (unsigned char *)"";
-	pin_cmd.pin1.len = 0;
-
-	rv = iasecc_chv_verify(card, &pin_cmd, tries_left);
-
-	LOG_FUNC_RETURN(ctx, rv);
-}
-
-
-static int
 iasecc_pin_get_status(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_left)
 {
 	struct sc_context *ctx = card->ctx;
@@ -1967,65 +1938,89 @@ iasecc_pin_get_status(struct sc_card *card, struct sc_pin_cmd_data *data, int *t
 
 
 static int
-iasecc_pin_verify(struct sc_card *card, unsigned type, unsigned reference,
-		const unsigned char *data, size_t data_len, int *tries_left)
+iasecc_pin_verify(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_left)
 {
 	struct sc_context *ctx = card->ctx;
+	unsigned type = data->pin_type;
+	unsigned reference = data->pin_reference;
 	struct sc_pin_cmd_data pin_cmd;
-	unsigned chv_ref = reference;
+	struct iasecc_pin_policy policy;
+	int tries_before_verify = -1;
 	int rv;
 
 	LOG_FUNC_CALLED(ctx);
 	sc_log(ctx,
-	       "Verify PIN(type:%X,ref:%i,data(len:%"SC_FORMAT_LEN_SIZE_T"u,%p)",
-	       type, reference, data_len, data);
+	       "Verify PIN(type:%X,ref:%i,data(len:%i,%p)",
+	       type, reference, data->pin1.len, data->pin1.data);
 
 	if (type == SC_AC_AUT)   {
 		rv =  iasecc_sm_external_authentication(card, reference, tries_left);
 		LOG_FUNC_RETURN(ctx, rv);
 	}
-	else if (type == SC_AC_SCB)   {
+
+	if (type == SC_AC_SCB)   {
 		if (reference & IASECC_SCB_METHOD_USER_AUTH)   {
 			type = SC_AC_SEN;
 			reference = reference & IASECC_SCB_METHOD_MASK_REF;
 		}
-		else   {
-			sc_log(ctx, "Do not try to verify non CHV PINs");
-			LOG_FUNC_RETURN(ctx, SC_SUCCESS);
-		}
 	}
 
 	if (type == SC_AC_SEN)   {
-		rv = iasecc_se_at_to_chv_reference(card, reference,  &chv_ref);
+		type = SC_AC_CHV;
+		rv = iasecc_se_at_to_chv_reference(card, reference,  &reference);
 		LOG_TEST_RET(ctx, rv, "SE AT to CHV reference error");
 	}
 
-	memset(&pin_cmd, 0, sizeof(pin_cmd));
+	if (type != SC_AC_CHV)   {
+		sc_log(ctx, "Do not try to verify non CHV PINs");
+		LOG_FUNC_RETURN(ctx, SC_ERROR_INVALID_ARGUMENTS);
+	}
+
+	pin_cmd = *data;
 	pin_cmd.pin_type = SC_AC_CHV;
-	pin_cmd.pin_reference = chv_ref;
+	pin_cmd.pin_reference = reference;
 	pin_cmd.cmd = SC_PIN_CMD_VERIFY;
 
-	rv = iasecc_pin_get_policy(card, &pin_cmd);
-	LOG_TEST_RET(ctx, rv, "Get 'PIN policy' error");
-
-	pin_cmd.pin1.data = data;
-	pin_cmd.pin1.len = data_len;
-
-	rv = iasecc_pin_is_verified(card, &pin_cmd, tries_left);
-	if (data && !data_len)
+	rv = iasecc_pin_get_status(card, &pin_cmd, tries_left);
+	if (data->pin1.data && !data->pin1.len)
 		LOG_FUNC_RETURN(ctx, rv);
 
 	if (!rv)   {
-		if (iasecc_chv_cache_is_verified(card, &pin_cmd))
-			LOG_FUNC_RETURN(ctx, SC_SUCCESS);
+		if (pin_cmd.pin1.logged_in == SC_PIN_STATE_LOGGED_IN)
+			if (iasecc_chv_cache_is_verified(card, &pin_cmd))
+				LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 	}
-	else if (rv != SC_ERROR_PIN_CODE_INCORRECT && rv != SC_ERROR_SECURITY_STATUS_NOT_SATISFIED)   {
+	else if (rv != SC_ERROR_SECURITY_STATUS_NOT_SATISFIED)   {
 		LOG_FUNC_RETURN(ctx, rv);
 	}
 
 	iasecc_chv_cache_clean(card, &pin_cmd);
 
-	rv = iasecc_chv_verify(card, &pin_cmd, tries_left);
+	rv = iasecc_pin_merge_policy(card, &pin_cmd, &pin_cmd.pin1, &policy);
+	LOG_TEST_RET(ctx, rv, "Failed to update PIN1 info");
+
+	/* PIN-pads work best with fixed-size lengths. Use PIN padding when length is available. */
+	if (pin_cmd.flags & SC_PIN_CMD_USE_PINPAD) {
+		tries_before_verify = pin_cmd.pin1.tries_left;
+		if (policy.stored_length > 0)
+			iasecc_set_pin_padding(&pin_cmd, &pin_cmd.pin1, policy.stored_length);
+	}
+
+	rv = iasecc_chv_verify(card, &pin_cmd, policy.scbs, tries_left);
+
+	/*
+	 * Detect and log PIN-pads which don't handle variable-length PIN - special case where they
+	 * forward the CHV verify command with Lc = 0 to the card, without updating Lc. An IAS-ECC
+	 * card responds to this command by returning the number of attempts left, without
+	 * decreasing the counter.
+	 */
+	if ((pin_cmd.flags & SC_PIN_CMD_USE_PINPAD) && !(pin_cmd.flags & SC_PIN_CMD_NEED_PADDING)) {
+		if (rv == SC_ERROR_PIN_CODE_INCORRECT && pin_cmd.pin1.tries_left == tries_before_verify) {
+			SC_TEST_RET(ctx, SC_LOG_DEBUG_VERBOSE, rv,
+				    "PIN-pad reader does not support variable-length PIN");
+		}
+	}
+
 	LOG_TEST_RET(ctx, rv, "PIN CHV verification error");
 
 	rv = iasecc_chv_cache_verified(card, &pin_cmd);
@@ -2586,7 +2581,7 @@ iasecc_pin_cmd(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_le
 
 	switch (data->cmd)   {
 	case SC_PIN_CMD_VERIFY:
-		rv = iasecc_pin_verify(card, data->pin_type, data->pin_reference, data->pin1.data, data->pin1.len, tries_left);
+		rv = iasecc_pin_verify(card, data, tries_left);
 		break;
 	case SC_PIN_CMD_CHANGE:
 		if (data->pin_type == SC_AC_AUT)
