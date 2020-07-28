9ffb9bae63c8b7fbababb8481a83f1b575e59a18
Julian Strobl <julian.strobl@telekom.de>
2020-07-28 12:22:32 +0200

    tcos: add missing encryption certificates

--- src/libopensc/pkcs15-tcos.c
+++ src/libopensc/pkcs15-tcos.c
@@ -382,6 +382,8 @@ static int detect_idkey(
 	insert_cert(p15card, "DF074332", 0x46, 1, "Signatur Zertifikat 2");
 	insert_cert(p15card, "DF074333", 0x47, 1, "Signatur Zertifikat 3");
 	insert_cert(p15card, "DF084331", 0x4B, 1, "Verschluesselungs Zertifikat 1");
+	insert_cert(p15card, "DF084332", 0x4C, 1, "Verschluesselungs Zertifikat 2");
+	insert_cert(p15card, "DF084333", 0x4D, 1, "Verschluesselungs Zertifikat 3");
 	/* TODO should others come here too? */
 
 	insert_key(p15card, "DF074E03", 0x45, 0x84, 2048, 1, "IDKey1");
@@ -391,6 +393,8 @@ static int detect_idkey(
 	insert_key(p15card, "DF074E07", 0x49, 0x88, 2048, 1, "IDKey5");
 	insert_key(p15card, "DF074E08", 0x4A, 0x89, 2048, 1, "IDKey6");
 	insert_key(p15card, "DF084E01", 0x4B, 0x81, 2048, 1, "IDKey7");
+	insert_key(p15card, "DF084E02", 0x4C, 0x82, 2048, 1, "IDKey8");
+	insert_key(p15card, "DF084E03", 0x4D, 0x83, 2048, 1, "IDKey9");
 
 	insert_pin(p15card, "5000", 1, 2, 0x00, 6, "PIN",
 		SC_PKCS15_PIN_FLAG_CASE_SENSITIVE | SC_PKCS15_PIN_FLAG_INITIALIZED
