7f0166ab123d54e2229b78261371e10d93cd2b4c
Jakub Jelen <jjelen@redhat.com>
2020-08-13 18:44:50 +0200

    p11test: Basic test for C_GetInterface API from PKCS#11 3.0

---
+++ src/tests/p11test/p11test_case_interface.h
@@ -0,0 +1,25 @@
+/*
+ * p11test_case_interface.h: Test new PKCS #11 3.0 interface
+ *
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * Author: Jakub Jelen <jjelen@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "p11test_case_common.h"
+
+void interface_test(void **state);
+
