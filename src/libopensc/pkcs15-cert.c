460a862ee043672b5114c1a0b8d429c587764c89
Jakub Jelen <jjelen@redhat.com>
2020-09-01 17:19:25 +0200

    pkcs15-cert: Avoid memory leaks
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=24056

--- src/libopensc/pkcs15-cert.c
+++ src/libopensc/pkcs15-cert.c
@@ -108,7 +108,6 @@ parse_x509_cert(sc_context_t *ctx, struct sc_pkcs15_der *der, struct sc_pkcs15_c
 	if (!pubkey)
 		LOG_TEST_GOTO_ERR(ctx, SC_ERROR_INVALID_ASN1_OBJECT, "Unable to decode subjectPublicKeyInfo from cert");
 
-	sc_asn1_clear_algorithm_id(&sig_alg);
 
 	if (serial && serial_len)   {
 		sc_format_asn1_entry(asn1_serial_number + 0, serial, &serial_len, 1);
@@ -129,6 +128,8 @@ parse_x509_cert(sc_context_t *ctx, struct sc_pkcs15_der *der, struct sc_pkcs15_c
 	}
 
 err:
+	/* not used for anything */
+	sc_asn1_clear_algorithm_id(&sig_alg);
 	free(serial);
 	free(subject);
 	free(issuer);
