f7a73a4061be777882035b285e94eebd5bfcda41
Jakub Jelen <jjelen@redhat.com>
2020-09-07 13:01:26 +0200

    pkcs15-itacns: Free allocated memory
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=25475

--- src/libopensc/pkcs15-itacns.c
+++ src/libopensc/pkcs15-itacns.c
@@ -546,16 +546,20 @@ static int itacns_add_data_files(sc_pkcs15_card_t *p15card)
 
 	if (p15_personaldata->data) {
 		char fullname[160];
-		if(get_name_from_EF_DatiPersonali(p15_personaldata->data,
+		if (get_name_from_EF_DatiPersonali(p15_personaldata->data,
 			fullname, sizeof(fullname))) {
 			sc_log(p15card->card->ctx,
 				"Could not parse EF_DatiPersonali: "
 				"keeping generic card name");
 			sc_pkcs15_free_data_object(p15_personaldata);
+			free(cinfo->data.value);
+			cinfo->data.value = NULL;
 			return SC_SUCCESS;
 		}
 		set_string(&p15card->tokeninfo->label, fullname);
 	}
+	free(cinfo->data.value);
+	cinfo->data.value = NULL;
 	sc_pkcs15_free_data_object(p15_personaldata);
 	return SC_SUCCESS;
 }
