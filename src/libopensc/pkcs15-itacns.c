595926471b034fe4b518af722b2e1fae4ed6c450
Jakub Jelen <jjelen@redhat.com>
2020-09-23 17:04:40 +0200

    itacns: Really do not read behind the buffer bounds
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=25885

--- src/libopensc/pkcs15-itacns.c
+++ src/libopensc/pkcs15-itacns.c
@@ -379,7 +379,7 @@ static int hextoint(char *src, unsigned int len)
 
 	if(len >= sizeof(hex))
 		return -1;
-	strncpy(hex, src, len+1);
+	strncpy(hex, src, len);
 	hex[len] = '\0';
 	res = strtol(hex, &end, 0x10);
 	if(end != (char*)&hex[len])
