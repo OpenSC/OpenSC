9675a90ee75bd7c4ca34f07af49b6d6b3bdb967d
Jakub Jelen <jjelen@redhat.com>
2020-12-11 20:13:00 +0100

    tests: Verify there are no duplicate symbols exported

---
+++ tests/test-duplicate-symbols.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SOURCE_PATH=../
+
+EXPORTS=`find "${SOURCE_PATH}" -name "*exports"`
+
+ERRORS=0
+for E in $EXPORTS; do
+	NUM_DUPES=`sort $E | uniq -D | wc -l`
+	if [[ "$NUM_DUPES" != 0 ]]; then
+		echo "There are duplicate symbols in '$E'"
+		ERRORS=1
+	fi
+done
+
+if [[ "$ERRORS" = 1 ]]; then
+	echo "There are duplicate symbols"
+	exit 1
+fi
+
+exit $ERRORS
