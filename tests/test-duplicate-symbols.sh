8d61d0d20db1df9b9c5e95b95dac3240e685b603
Jakub Jelen <jjelen@redhat.com>
2021-03-03 12:19:26 +0100

    Use more portable switch for uniq to unbreak osx build

--- tests/test-duplicate-symbols.sh
+++ tests/test-duplicate-symbols.sh
@@ -5,7 +5,7 @@ EXPORTS=`find "${SOURCE_PATH}" -name "*exports"`
 
 ERRORS=0
 for E in $EXPORTS; do
-	NUM_DUPES=`sort $E | uniq -D | wc -l`
+	NUM_DUPES=`sort $E | uniq -d | wc -l`
 	if [[ "$NUM_DUPES" != 0 ]]; then
 		echo "There are duplicate symbols in '$E'"
 		ERRORS=1
