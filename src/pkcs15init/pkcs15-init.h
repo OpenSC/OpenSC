ac7b02eacafec3c8e63c7ec4e827b6e8ac90e2ec
Veronika Hanulikova <vhanulik@redhat.com>
2021-08-03 13:01:00 +0200

    jcop: Remove pkcs15init support
    Remove support for JCOP/BlueZ cards due to no recent
    user or developer activity except general fixes.

--- src/pkcs15init/pkcs15-init.h
+++ src/pkcs15init/pkcs15-init.h
@@ -432,7 +432,6 @@ extern struct sc_pkcs15init_operations *sc_pkcs15init_get_miocos_ops(void);
 extern struct sc_pkcs15init_operations *sc_pkcs15init_get_cryptoflex_ops(void);
 extern struct sc_pkcs15init_operations *sc_pkcs15init_get_cyberflex_ops(void);
 extern struct sc_pkcs15init_operations *sc_pkcs15init_get_cardos_ops(void);
-extern struct sc_pkcs15init_operations *sc_pkcs15init_get_jcop_ops(void);
 extern struct sc_pkcs15init_operations *sc_pkcs15init_get_starcos_ops(void);
 extern struct sc_pkcs15init_operations *sc_pkcs15init_get_oberthur_ops(void);
 extern struct sc_pkcs15init_operations *sc_pkcs15init_get_setcos_ops(void);
