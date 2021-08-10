a96bd08884d96efeca3cdf4ac4710efeeb5bdfdd
Jakub Jelen <jjelen@redhat.com>
2021-08-10 14:29:02 +0200

    tests: Use explicit pkcs11 library in thread test
    Previously, the system-installed opensc was used, but there is no
    guarantee that it exists when running tests. Use the locally built
    library, even though there will probably not be any slots.
    
    Using softhsm causes a lot of failures as it looks like this is not well
    handled in there.

--- tests/test-pkcs11-tool-test-threads.sh
+++ tests/test-pkcs11-tool-test-threads.sh
@@ -3,32 +3,20 @@ SOURCE_PATH=${SOURCE_PATH:-../}
 
 source $SOURCE_PATH/tests/common.sh
 
-echo "======================================================="
-echo "Setup SoftHSM"
-echo "======================================================="
-if [[ ! -f $P11LIB ]]; then
-    echo "WARNINIG: The SoftHSM is not installed. Can not run this test"
-    exit 77;
-fi
-
-card_setup
+# Test our PKCS #11 module here
+P11LIB="../src/pkcs11/.libs/opensc-pkcs11.so"
 
 echo "======================================================="
 echo "Test pkcs11 threads IN "
 echo "======================================================="
-$PKCS11_TOOL --test-threads IN -L
+$PKCS11_TOOL --test-threads IN -L --module="$P11LIB"
 assert $? "Failed running tests"
 
 
 echo "======================================================="
 echo "Test pkcs11 threads ILGISLT0 "
 echo "======================================================="
-$PKCS11_TOOL --test-threads ILGISLT0 -L
+$PKCS11_TOOL --test-threads ILGISLT0 -L --module="$P11LIB"
 assert $? "Failed running tests"
-
-echo "======================================================="
-echo "Cleanup"
-echo "======================================================="
-card_cleanup
 
 exit $ERRORS
