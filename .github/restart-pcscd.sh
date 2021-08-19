f1737c2e261dcad055c911c727bbb616e9e30d29
Jakub Jelen <jjelen@redhat.com>
2021-08-19 11:58:23 +0200

    .github: Try to wait for pcscd up to 30 seconds

--- .github/restart-pcscd.sh
+++ .github/restart-pcscd.sh
@@ -3,14 +3,31 @@
 # This file is made to be sourced into other test scripts and not executed
 # manually because it sets trap to restore pcscd to working state
 
+# register cleanup handler
 function pcscd_cleanup {
+	echo "Process terminated: resetting pcscd"
 	sudo pkill pcscd
 	sudo systemctl start pcscd.socket
 }
-
-# register cleanup handler
 trap pcscd_cleanup EXIT
+
 
 # stop the pcscd service and run it from console to see possible errors
 sudo systemctl stop pcscd.service pcscd.socket
-sudo /usr/sbin/pcscd -f &
+sudo /usr/sbin/pcscd -f | sed -e 's/^/pcscd: /;' &
+
+
+# Try to wait up to 30 seconds for pcscd to come up and create PID file
+for ((i=1;i<=30;i++)); do
+	echo "Waiting for pcscd to start: $i s"
+	if [ -f "/var/run/pcscd/pcscd.pid" ]; then
+		break
+	fi
+	sleep 1
+done
+
+
+# if it did not come up, warn, but continue
+if [ ! -f "/var/run/pcscd/pcscd.pid" ]; then
+	echo "WARNING: The pcscd pid file does not exist ... trying anyway"
+fi
