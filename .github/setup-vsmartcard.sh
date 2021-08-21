1b10f79a58f6562a0d47fd03000f9a14d153bee8
Doug Engert <deengert@gmail.com>
2021-08-21 07:01:23 -0500

    test-actions-2 vsmartcard-0.8 See if cebe05e78d51124591cf93f0506112fbe31d34fa on 8/13 is the problem vcpd polling was changed

--- .github/setup-vsmartcard.sh
+++ .github/setup-vsmartcard.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -x
 if [ ! -d "vsmartcard" ]; then
 	git clone https://github.com/frankmorgner/vsmartcard.git
 fi
 pushd vsmartcard/virtualsmartcard
+git checkout -b  tag-0.8 virtualsmartcard-0.8
 autoreconf -vis && ./configure && make -j2 && sudo make install
 popd
