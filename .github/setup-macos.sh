282440d32317c3e4ac6db3eff15097703d01c309
Frank Morgner <frankmorgner@gmail.com>
2021-08-24 21:23:33 +0200

    ci: add signing keys when NOT in PR
    fixes logical error during migration from travis to gh actions

--- .github/setup-macos.sh
+++ .github/setup-macos.sh
@@ -24,7 +24,7 @@ git clone https://github.com/frankmorgner/OpenSCToken.git
 sudo rm -rf /Library/Developer/CommandLineTools;
 
 # TODO make the encrypted key working in github
-if [ "$GITHUB_EVENT_NAME" == "pull_request" -a -n "$PASS_SECRETS_TAR_ENC" ]; then
+if [ "$GITHUB_EVENT_NAME" != "pull_request" -a -n "$PASS_SECRETS_TAR_ENC" ]; then
     gpg --quiet --batch --yes --decrypt --passphrase="$PASS_SECRETS_TAR_ENC" --output .github/secrets.tar .github/secrets.tar.gpg
     .github/add_signing_key.sh;
 else
