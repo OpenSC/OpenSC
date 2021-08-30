7726eb8ff4631d35c4cedce548d732e9d83dbbe0
Frank Morgner <frankmorgner@gmail.com>
2021-08-30 22:27:38 +0200

    ci: no need to focus on non-PRs
    secrets are only available for collaborators, so just fire everything up

--- .github/cleanup-macos.sh
+++ .github/cleanup-macos.sh
@@ -2,6 +2,6 @@
 
 set -ex -o xtrace
 
-if [ "$GITHUB_EVENT_NAME" != "pull_request" -a -n "$PASS_SECRETS_TAR_ENC" ]; then
+if [ -n "$PASS_SECRETS_TAR_ENC" ]; then
     .github/remove_signing_key.sh
 fi
