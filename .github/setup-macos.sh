4aed6772fb0e718014d6e6c6987128748715db88
Frank Morgner <frankmorgner@gmail.com>
2021-08-30 22:27:38 +0200

    ci: no need to focus on non-PRs
    secrets are only available for collaborators, so just fire everything up

--- .github/setup-macos.sh
+++ .github/setup-macos.sh
@@ -25,7 +25,7 @@ export PATH="/usr/local/opt/ccache/libexec:$PATH"
 git clone https://github.com/frankmorgner/OpenSCToken.git
 sudo rm -rf /Library/Developer/CommandLineTools;
 
-if [ "$GITHUB_EVENT_NAME" != "pull_request" -a -n "$PASS_SECRETS_TAR_ENC" ]; then
+if [ -n "$PASS_SECRETS_TAR_ENC" ]; then
     gpg --quiet --batch --yes --decrypt --passphrase="$PASS_SECRETS_TAR_ENC" --output .github/secrets.tar .github/secrets.tar.gpg
     .github/add_signing_key.sh;
 else
