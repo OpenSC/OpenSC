98384f6b8824fd1758dcf60ede003758b9e0bcc1
Jakub Jelen <jjelen@redhat.com>
2021-09-24 16:22:40 +0200

    .github: Remove bogus semicolumns

--- .github/test-isoapplet.sh
+++ .github/test-isoapplet.sh
@@ -11,14 +11,14 @@ export LD_LIBRARY_PATH=/usr/local/lib
 
 # The ISO applet
 if [ ! -d IsoApplet ]; then
-	git clone https://github.com/philipWendland/IsoApplet.git;
+	git clone https://github.com/philipWendland/IsoApplet.git
 fi
-javac -classpath jcardsim/target/jcardsim-3.0.5-SNAPSHOT.jar IsoApplet/src/net/pwendland/javacard/pki/isoapplet/*.java;
-echo "com.licel.jcardsim.card.applet.0.AID=F276A288BCFBA69D34F31001" > isoapplet_jcardsim.cfg;
-echo "com.licel.jcardsim.card.applet.0.Class=net.pwendland.javacard.pki.isoapplet.IsoApplet" >> isoapplet_jcardsim.cfg;
-echo "com.licel.jcardsim.card.ATR=3B80800101" >> isoapplet_jcardsim.cfg;
-echo "com.licel.jcardsim.vsmartcard.host=localhost" >> isoapplet_jcardsim.cfg;
-echo "com.licel.jcardsim.vsmartcard.port=35963" >> isoapplet_jcardsim.cfg;
+javac -classpath jcardsim/target/jcardsim-3.0.5-SNAPSHOT.jar IsoApplet/src/net/pwendland/javacard/pki/isoapplet/*.java
+echo "com.licel.jcardsim.card.applet.0.AID=F276A288BCFBA69D34F31001" > isoapplet_jcardsim.cfg
+echo "com.licel.jcardsim.card.applet.0.Class=net.pwendland.javacard.pki.isoapplet.IsoApplet" >> isoapplet_jcardsim.cfg
+echo "com.licel.jcardsim.card.ATR=3B80800101" >> isoapplet_jcardsim.cfg
+echo "com.licel.jcardsim.vsmartcard.host=localhost" >> isoapplet_jcardsim.cfg
+echo "com.licel.jcardsim.vsmartcard.port=35963" >> isoapplet_jcardsim.cfg
 
 
 # prepare pcscd
@@ -27,18 +27,18 @@ echo "com.licel.jcardsim.vsmartcard.port=35963" >> isoapplet_jcardsim.cfg;
 
 # start the applet and run couple of commands against that
 java -noverify -cp IsoApplet/src/:jcardsim/target/jcardsim-3.0.5-SNAPSHOT.jar com.licel.jcardsim.remote.VSmartCard isoapplet_jcardsim.cfg >/dev/null &
-PID=$!;
-sleep 5;
-opensc-tool --card-driver default --send-apdu 80b800001a0cf276a288bcfba69d34f310010cf276a288bcfba69d34f3100100;
-opensc-tool -n;
-pkcs15-init --create-pkcs15 --so-pin 123456 --so-puk 0123456789abcdef;
-pkcs15-tool --change-pin --pin 123456 --new-pin 654321;
-pkcs15-tool --unblock-pin --puk 0123456789abcdef --new-pin 123456;
-pkcs15-init --generate-key rsa/2048     --id 1 --key-usage decrypt,sign --auth-id FF --pin 123456;
-pkcs15-init --generate-key rsa/2048     --id 2 --key-usage decrypt      --auth-id FF --pin 123456;
-pkcs15-init --generate-key ec/secp256r1 --id 3 --key-usage sign         --auth-id FF --pin 123456;
-pkcs15-tool -D;
-pkcs11-tool -l -t -p 123456;
+PID=$!
+sleep 5
+opensc-tool --card-driver default --send-apdu 80b800001a0cf276a288bcfba69d34f310010cf276a288bcfba69d34f3100100
+opensc-tool -n
+pkcs15-init --create-pkcs15 --so-pin 123456 --so-puk 0123456789abcdef
+pkcs15-tool --change-pin --pin 123456 --new-pin 654321
+pkcs15-tool --unblock-pin --puk 0123456789abcdef --new-pin 123456
+pkcs15-init --generate-key rsa/2048     --id 1 --key-usage decrypt,sign --auth-id FF --pin 123456
+pkcs15-init --generate-key rsa/2048     --id 2 --key-usage decrypt      --auth-id FF --pin 123456
+pkcs15-init --generate-key ec/secp256r1 --id 3 --key-usage sign         --auth-id FF --pin 123456
+pkcs15-tool -D
+pkcs11-tool -l -t -p 123456
 
 # run the tests
 pushd src/tests/p11test/
@@ -46,6 +46,6 @@ sleep 5
 ./p11test -s 0 -p 123456 -o isoapplet.json || true # ec_sign_size_test is failing here
 popd
 
-kill -9 $PID;
+kill -9 $PID
 
 diff -u3 src/tests/p11test/isoapplet{_ref,}.json
