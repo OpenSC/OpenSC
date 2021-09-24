5d1cf13804dbc7310e905d6402544dabecd0d842
Jakub Jelen <jjelen@redhat.com>
2021-09-24 14:41:17 +0200

    .github: Test with master version of vsmartcard

--- .github/setup-vsmartcard.sh
+++ .github/setup-vsmartcard.sh
@@ -6,6 +6,5 @@ if [ ! -d "vsmartcard" ]; then
 	git clone https://github.com/frankmorgner/vsmartcard.git
 fi
 pushd vsmartcard/virtualsmartcard
-git checkout -b  tag-0.8 virtualsmartcard-0.8
 autoreconf -vis && ./configure && make -j2 && sudo make install
 popd
