edca4a873ed1d4ef65f0639124bb0986447028b0
jozsefd <jozsef.dojcsak@gmail.com>
2021-11-03 11:11:58 +0100

    Removed flag SC_PKCS15_PRKEY_USAGE_ENCRYPT

--- src/libopensc/pkcs15-nqApplet.c
+++ src/libopensc/pkcs15-nqApplet.c
@@ -159,7 +159,7 @@ static int add_nqapplet_objects(sc_pkcs15_card_t *p15card)
 	LOG_TEST_RET(card->ctx, rv, "Failed to add Auth. certificate");
 
 	// 2.2) PrK.CH.Auth
-	rv = add_nqapplet_private_key(p15card, "1", 0x01, "PrK.CH.Auth", "1", SC_PKCS15_PRKEY_USAGE_SIGN | SC_PKCS15_PRKEY_USAGE_ENCRYPT | SC_PKCS15_PRKEY_USAGE_DECRYPT);
+	rv = add_nqapplet_private_key(p15card, "1", 0x01, "PrK.CH.Auth", "1", SC_PKCS15_PRKEY_USAGE_SIGN | SC_PKCS15_PRKEY_USAGE_DECRYPT);
 	LOG_TEST_RET(card->ctx, rv, "Failed to add Auth. private key");
 
 	// 3.1) C.CH.Encr
