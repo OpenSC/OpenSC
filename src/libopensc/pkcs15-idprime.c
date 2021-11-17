38da7e1062d4eb3beefa50a2c4838b8ac4ae7066
Veronika Hanulikova <vhanulik@redhat.com>
2021-11-17 18:42:35 +0100

    pkcs15-idprime: Stop iterator in case of failure
    For getting objects, idprime_card_ctl() starts iterator in
    list_t in card->drv_data. In case of fail, iterator is not set to
    stopped and variable iter_active in list_t remains 1. Then during
    memory release in sc_disconnect_card() list_t is not freed.
    
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=39268

--- src/libopensc/pkcs15-idprime.c
+++ src/libopensc/pkcs15-idprime.c
@@ -269,8 +269,10 @@ static int sc_pkcs15emu_idprime_init(sc_pkcs15_card_t *p15card)
 		cert_out->key = NULL;
 fail:
 		sc_pkcs15_free_certificate(cert_out);
-		if (r < 0)
+		if (r < 0) {
+			(card->ops->card_ctl)(card, SC_CARDCTL_IDPRIME_FINAL_GET_OBJECTS, &count);
 			LOG_FUNC_RETURN(card->ctx, r); /* should not fail */
+		}
 
 	}
 	r = (card->ops->card_ctl)(card, SC_CARDCTL_IDPRIME_FINAL_GET_OBJECTS, &count);
