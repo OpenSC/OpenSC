febb498b627dd157c776975261a2e80dabaa0842
Frank Morgner <frankmorgner@gmail.com>
2021-11-20 01:16:49 +0100

    PIV: use fread_to_eof to read offcard certs

--- src/libopensc/card-piv.c
+++ src/libopensc/card-piv.c
@@ -54,6 +54,7 @@
 #include "compression.h"
 #endif
 #include "simpletlv.h"
+#include "common/fread_to_eof.h"
 
 enum {
 	PIV_OBJ_CCC = 0,
@@ -688,73 +689,6 @@ static int piv_find_aid(sc_card_t * card)
 	}
 
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
-}
-
-/*
- * Read a DER encoded object from a file. Allocate and return the buf.
- * Used to read the file defined in offCardCertURL from a cache.
- * Also used for testing of History and Discovery objects from a file
- * when testing with a card that does not support these new objects.
- */
-static int piv_read_obj_from_file(sc_card_t * card, char * filename,
-	u8 **buf, size_t *buf_len)
-{
-	int r;
-	int r_tag;
-	int f = -1;
-	size_t len;
-	u8 tagbuf[16];
-	size_t rbuflen;
-	const u8 * body;
-	unsigned int cla_out, tag_out;
-	size_t bodylen;
-
-	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
-
-	*buf = NULL;
-	*buf_len = 0;
-	f = open(filename, O_RDONLY);
-	if (f < 0) {
-		sc_log(card->ctx, "Unable to load PIV off card file: \"%s\"",filename);
-			r = SC_ERROR_FILE_NOT_FOUND;
-			goto err;
-	}
-	len = read(f, tagbuf, sizeof(tagbuf)); /* get tag and length */
-	if (len < 2 || len > sizeof(tagbuf)) {
-		sc_log(card->ctx, "Problem with \"%s\"",filename);
-		r =  SC_ERROR_DATA_OBJECT_NOT_FOUND;
-		goto err;
-	}
-	body = tagbuf;
-	r_tag = sc_asn1_read_tag(&body, len, &cla_out, &tag_out, &bodylen);
-	if ((r_tag != SC_SUCCESS && r_tag != SC_ERROR_ASN1_END_OF_CONTENTS)
-			|| body == NULL) {
-		sc_log(card->ctx, "DER problem");
-		r = SC_ERROR_FILE_NOT_FOUND;
-		goto err;
-	}
-	rbuflen = body - tagbuf + bodylen;
-	*buf = malloc(rbuflen);
-	if (!*buf) {
-		r = SC_ERROR_OUT_OF_MEMORY;
-		goto err;
-	}
-	memcpy(*buf, tagbuf, len); /* copy first or only part */
-	if (rbuflen > len + sizeof(tagbuf)) {
-		len = read(f, *buf + sizeof(tagbuf), rbuflen - sizeof(tagbuf)); /* read rest */
-		if (len != rbuflen - sizeof(tagbuf)) {
-			r = SC_ERROR_INVALID_ASN1_OBJECT;
-			free (*buf);
-			*buf = NULL;
-			goto err;
-		}
-	}
-	r = rbuflen;
-	*buf_len = rbuflen;
-err:
-	if (f >= 0)
-		close(f);
-	LOG_FUNC_RETURN(card->ctx, r);
 }
 
 /* the tag is the PIV_OBJ_*  */
@@ -2806,9 +2740,8 @@ piv_process_history(sc_card_t *card)
 #endif
 		strcat(filename,fp);
 
-		r = piv_read_obj_from_file(card, filename,
-			 &ocfhfbuf, &ocfhflen);
-		if (r == SC_ERROR_FILE_NOT_FOUND) {
+		r = fread_to_eof(filename, &ocfhfbuf, &ocfhflen);
+		if (r != 1) {
 			r = 0;
 			goto err;
 		}
