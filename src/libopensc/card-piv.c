01b5d1afb188df58d4bfaa264f24daa7164bf0c7
Frank Morgner <frankmorgner@gmail.com>
2021-11-23 14:47:31 +0100

    PIV: deduplication of object description

--- src/libopensc/card-piv.c
+++ src/libopensc/card-piv.c
@@ -55,69 +55,7 @@
 #endif
 #include "simpletlv.h"
 #include "common/fread_to_eof.h"
-
-enum {
-	PIV_OBJ_CCC = 0,
-	PIV_OBJ_CHUI,
-	/*  PIV_OBJ_UCHUI is not in new with 800-73-2 */
-	PIV_OBJ_X509_PIV_AUTH,
-	PIV_OBJ_CHF,
-	PIV_OBJ_PI,
-	PIV_OBJ_CHFI,
-	PIV_OBJ_X509_DS,
-	PIV_OBJ_X509_KM,
-	PIV_OBJ_X509_CARD_AUTH,
-	PIV_OBJ_SEC_OBJ,
-	PIV_OBJ_DISCOVERY,
-	PIV_OBJ_HISTORY,
-	PIV_OBJ_RETIRED_X509_1,
-	PIV_OBJ_RETIRED_X509_2,
-	PIV_OBJ_RETIRED_X509_3,
-	PIV_OBJ_RETIRED_X509_4,
-	PIV_OBJ_RETIRED_X509_5,
-	PIV_OBJ_RETIRED_X509_6,
-	PIV_OBJ_RETIRED_X509_7,
-	PIV_OBJ_RETIRED_X509_8,
-	PIV_OBJ_RETIRED_X509_9,
-	PIV_OBJ_RETIRED_X509_10,
-	PIV_OBJ_RETIRED_X509_11,
-	PIV_OBJ_RETIRED_X509_12,
-	PIV_OBJ_RETIRED_X509_13,
-	PIV_OBJ_RETIRED_X509_14,
-	PIV_OBJ_RETIRED_X509_15,
-	PIV_OBJ_RETIRED_X509_16,
-	PIV_OBJ_RETIRED_X509_17,
-	PIV_OBJ_RETIRED_X509_18,
-	PIV_OBJ_RETIRED_X509_19,
-	PIV_OBJ_RETIRED_X509_20,
-	PIV_OBJ_IRIS_IMAGE,
-	PIV_OBJ_9B03,
-	PIV_OBJ_9A06,
-	PIV_OBJ_9C06,
-	PIV_OBJ_9D06,
-	PIV_OBJ_9E06,
-	PIV_OBJ_8206,
-	PIV_OBJ_8306,
-	PIV_OBJ_8406,
-	PIV_OBJ_8506,
-	PIV_OBJ_8606,
-	PIV_OBJ_8706,
-	PIV_OBJ_8806,
-	PIV_OBJ_8906,
-	PIV_OBJ_8A06,
-	PIV_OBJ_8B06,
-	PIV_OBJ_8C06,
-	PIV_OBJ_8D06,
-	PIV_OBJ_8E06,
-	PIV_OBJ_8F06,
-	PIV_OBJ_9006,
-	PIV_OBJ_9106,
-	PIV_OBJ_9206,
-	PIV_OBJ_9306,
-	PIV_OBJ_9406,
-	PIV_OBJ_9506,
-	PIV_OBJ_LAST_ENUM
-};
+#include "card-piv.h"
 
 /*
  * Flags in the piv_obj_cache:
@@ -284,19 +222,9 @@ struct sc_aid piv_aid = { {0xA0, 0x00, 0x00, 0x03, 0x08, 0x00, 0x00, 0x10, 0x00}
 #define PIV_OBJECT_TYPE_PUBKEY		2
 #define PIV_OBJECT_NOT_PRESENT		4
 
-struct piv_object {
-	int enumtag;
-	const char * name;
-	const char * oidstring;
-	size_t tag_len;
-	u8  tag_value[3];
-	u8  containerid[2];	/* will use as relative paths for simulation */
-	int flags;              /* object has some internal object like a cert */
-};
-
 /* Must be in order, and one per enumerated PIV_OBJ */
 // clang-format off
-static const struct piv_object piv_objects[] = {
+const struct piv_object piv_objects[] = {
 	{ PIV_OBJ_CCC, "Card Capability Container",
 			"2.16.840.1.101.3.7.1.219.0", 3, "\x5F\xC1\x07", "\xDB\x00", 0},
 	{ PIV_OBJ_CHUI, "Card Holder Unique Identifier",
@@ -387,7 +315,7 @@ static const struct piv_object piv_objects[] = {
 	{ PIV_OBJ_IRIS_IMAGE, "Cardholder Iris Images",
 			"2.16.840.1.101.3.7.2.16.21", 3, "\x5F\xC1\x21", "\x10\x15", 0},
 
-/* following not standard , to be used by piv-tool only for testing */
+	/* following not standard , to be used by piv-tool only for testing */
 	{ PIV_OBJ_9B03, "3DES-ECB ADM",
 			"2.16.840.1.101.3.7.2.9999.3", 2, "\x9B\x03", "\x9B\x03", 0},
 	/* Only used when signing a cert req, usually from engine
