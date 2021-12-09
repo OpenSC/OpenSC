bff01723105fb325bf236ce7ed4551e3c36f7265
Frank Morgner <frankmorgner@gmail.com>
2021-12-09 14:50:24 +0100

    PIV: beautify CHUID ASN.1 parsing
    Note: We skip checking for the agency identifier 9999 as it is not
    required by 800-73-4 anymore. Just use the GUID if its set and the
    FASC-N otherwise.

--- src/libopensc/card-piv.c
+++ src/libopensc/card-piv.c
@@ -1809,13 +1809,41 @@ static int
 piv_get_serial_nr_from_CHUI(sc_card_t* card, sc_serial_number_t* serial)
 {
 	int r;
-	int i;
-	u8 gbits;
 	u8 *rbuf = NULL;
-	const u8 *body;
-	const u8 *fascn;
-	const u8 *guid;
-	size_t rbuflen = 0, bodylen, fascnlen, guidlen;
+	size_t rbuflen = 0;
+	u8 fascn[25], guid[16], empty_guid[16] = { 0x00 };
+	size_t fascn_len = sizeof fascn, guid_len = sizeof guid;
+	struct sc_asn1_entry chuid[] = {
+		{ "Buffer Lenght (Optional)",
+			SC_ASN1_OCTET_STRING, SC_ASN1_PRV|SC_ASN1_CONS|0x0E,
+			SC_ASN1_OPTIONAL, NULL, NULL },
+		{ "FASC-N",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x10,
+			0, NULL, 0 },
+		{ "Organizational Identifier (Optional)",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x12,
+			SC_ASN1_OPTIONAL, NULL, NULL },
+		{ "DUNS (Optional)",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x13,
+			SC_ASN1_OPTIONAL, NULL, NULL },
+		{ "GUID",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x14,
+			0, guid, &guid_len },
+		{ "Expiration Date",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x15,
+			0, NULL, NULL },
+		{ "Cardholder UUID (Optional)",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x16,
+			SC_ASN1_OPTIONAL, NULL, NULL },
+		{ "Issuer Asymmetric Signature",
+			SC_ASN1_OCTET_STRING, SC_ASN1_UNI|SC_ASN1_CONS|0x1E,
+			0, NULL, NULL },
+		{ "Error Detection Code",
+			SC_ASN1_OCTET_STRING, SC_ASN1_PRV|SC_ASN1_CONS|0x1E,
+			0, NULL, NULL },
+		{ NULL, 0, 0, 0, NULL, NULL }
+	};
+	struct sc_asn1_entry proprietary_do[sizeof c_proprietary_do/sizeof *c_proprietary_do];
 
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
 	if (card->serialnr.len)   {
@@ -1824,54 +1852,30 @@ piv_get_serial_nr_from_CHUI(sc_card_t* card, sc_serial_number_t* serial)
 	}
 
 	/*
-	 * 800-73-3 Part 1 and CIO Council docs say for PIV Compatible cards
-	 * the FASC-N Agency code should be 9999 and there should be a GUID
-	 * based on RFC 4122. If GUID present and not zero
-	 * we will use the GUID as the serial number.
+	 * 800-73-4 Part 1 and doc says for PIV Compatible cards there should be a
+	 * GUID based on RFC 4122. If GUID present and not zero we will use the
+	 * GUID as the serial number. Otherwise we use the FASC-N.
 	 */
 
 	r = piv_get_cached_data(card, PIV_OBJ_CHUI, &rbuf, &rbuflen);
 	LOG_TEST_RET(card->ctx, r, "Failure retrieving CHUI");
 
-	r = SC_ERROR_INTERNAL;
-	if (rbuflen != 0) {
-		body = sc_asn1_find_tag(card->ctx, rbuf, rbuflen, 0x53, &bodylen); /* Pass the outer wrapper asn1 */
-		if (body != NULL && bodylen != 0 && rbuf[0] == 0x53) {
-			fascn = sc_asn1_find_tag(card->ctx, body, bodylen, 0x30, &fascnlen); /* Find the FASC-N data */
-			guid = sc_asn1_find_tag(card->ctx, body, bodylen, 0x34, &guidlen);
+	sc_copy_asn1_entry(c_proprietary_do, proprietary_do);
+	sc_format_asn1_entry(proprietary_do + 0, &chuid, NULL, 0);
+	r = sc_asn1_decode(card->ctx, proprietary_do, rbuf, rbuflen, NULL, NULL);
+	LOG_TEST_RET(card->ctx, r, "Failure parsing CHUI");
 
-			gbits = 0; /* if guid is valid, gbits will not be zero */
-			if (guid && guidlen == 16) {
-				for (i = 0; i < 16; i++) {
-					gbits = gbits | guid[i]; /* if all are zero, gbits will be zero */
-				}
-			}
-			sc_log(card->ctx,
-			       "fascn=%p,fascnlen=%"SC_FORMAT_LEN_SIZE_T"u,guid=%p,guidlen=%"SC_FORMAT_LEN_SIZE_T"u,gbits=%2.2x",
-			       fascn, fascnlen, guid, guidlen, gbits);
-
-			if (fascn && fascnlen == 25) {
-				/* test if guid and the fascn starts with ;9999 (in ISO 4bit + parity code) */
-				if (!(gbits && fascn[0] == 0xD4 && fascn[1] == 0xE7
-						    && fascn[2] == 0x39 && (fascn[3] | 0x7F) == 0xFF)) {
-					/* fascnlen is 25 */
-					serial->len = fascnlen;
-					memcpy (serial->value, fascn, serial->len);
-					r = SC_SUCCESS;
-					gbits = 0; /* set to skip using guid below */
-				}
-			}
-			if (guid && gbits) {
-				/* guidlen is 16 */
-				serial->len = guidlen;
-				memcpy (serial->value, guid, serial->len);
-				r = SC_SUCCESS;
-			}
-		}
+	if (guid_len == sizeof guid && 0 != memcmp(guid, empty_guid, 16)) {
+		serial->len = guid_len;
+		memcpy (serial->value, guid, guid_len);
+	} else if (fascn_len == sizeof fascn) {
+		serial->len = fascn_len;
+		memcpy (serial->value, fascn, fascn_len);
 	}
 
 	card->serialnr = *serial;
-	LOG_FUNC_RETURN(card->ctx, r);
+
+	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 }
 
 /*
