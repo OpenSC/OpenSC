3a3efe765e0c5d4828eec9eb7a2580c18db9a69e
Veronika Hanulikova <vhanulik@redhat.com>
2022-01-27 13:58:32 +0100

    fuzzing: Put reader functions into separate file

---
+++ src/tests/fuzzing/fuzzer_reader.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2019 Frank Morgner <frankmorgner@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "libopensc/internal.h"
+#include <stdlib.h>
+#include <string.h>
+
+void fuzz_get_chunk(sc_reader_t *reader, const uint8_t **chunk, uint16_t *chunk_size);
+struct sc_reader_driver *sc_get_fuzz_driver(void);
+void fuzz_add_reader(struct sc_context *ctx, const uint8_t *Data, size_t Size);
