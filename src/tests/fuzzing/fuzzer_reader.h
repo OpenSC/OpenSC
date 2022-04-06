fd4ebe42c5a2264bf062d9aa0f9bd51e50c8f3aa
Veronika Hanulikova <vhanulik@redhat.com>
2022-04-06 15:08:08 +0200

    Add missing header

--- src/tests/fuzzing/fuzzer_reader.h
+++ src/tests/fuzzing/fuzzer_reader.h
@@ -16,12 +16,18 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "libopensc/internal.h"
+#ifndef FUZZER_READER_H
+#define FUZZER_READER_H
+
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "libopensc/internal.h"
 
 void fuzz_get_chunk(sc_reader_t *reader, const uint8_t **chunk, uint16_t *chunk_size);
 struct sc_reader_driver *sc_get_fuzz_driver(void);
 void fuzz_add_reader(struct sc_context *ctx, const uint8_t *Data, size_t Size);
 int fuzz_connect_card(sc_context_t *ctx, sc_card_t **card, sc_reader_t **reader_out,
                       const uint8_t *data, size_t size);
+
+#endif /* FUZZER_TOOL_H */
