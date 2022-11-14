7af2f4e0cd14a5bd363e8b5367b6860de4664f79
Claus Steuer <claus.steuer@swissbit.com>
2022-11-14 13:13:45 +0100

    undo ci build changes

--- .github/test-isoapplet.sh
+++ .github/test-isoapplet.sh
@@ -11,7 +11,7 @@ export LD_LIBRARY_PATH=/usr/local/lib
 
 # The ISO applet
 if [ ! -d IsoApplet ]; then
-	git clone https://github.com/swissbit-eis/IsoApplet.git
+	git clone https://github.com/philipWendland/IsoApplet.git
 	# enable IsoApplet key import patch
 	sed "s/DEF_PRIVATE_KEY_IMPORT_ALLOWED = false/DEF_PRIVATE_KEY_IMPORT_ALLOWED = true/g" -i IsoApplet/src/net/pwendland/javacard/pki/isoapplet/IsoApplet.java
 fi
