1681041cd192747c3512952b536bf0ef66d11a40
Jakub Jelen <jjelen@redhat.com>
2022-12-12 12:16:17 +0100

    p11test: Fix error detection when reading bool attributes

--- src/tests/p11test/p11test_case_common.c
+++ src/tests/p11test/p11test_case_common.c
@@ -309,19 +309,19 @@ int callback_private_keys(test_certs_t *objects,
 
 	/* Store attributes, flags and handles */
 	o->private_handle = object_handle;
-	o->sign = (template[0].ulValueLen != (CK_ULONG) -1)
+	o->sign = (template[0].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[0].pValue) : CK_FALSE;
-	o->decrypt = (template[1].ulValueLen != (CK_ULONG) -1)
+	o->decrypt = (template[1].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[1].pValue) : CK_FALSE;
-	o->key_type = (template[2].ulValueLen != (CK_ULONG) -1)
+	o->key_type = (template[2].ulValueLen == sizeof(CK_KEY_TYPE))
 		? *((CK_KEY_TYPE *) template[2].pValue) : (CK_KEY_TYPE) -1;
-	o->always_auth = (template[4].ulValueLen != (CK_ULONG) -1)
+	o->always_auth = (template[4].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[4].pValue) : CK_FALSE;
-	o->unwrap = (template[5].ulValueLen != (CK_ULONG) -1)
+	o->unwrap = (template[5].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[5].pValue) : CK_FALSE;
-	o->derive_priv = (template[6].ulValueLen != (CK_ULONG) -1)
+	o->derive_priv = (template[6].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[6].pValue) : CK_FALSE;
-	o->extractable = (template[8].ulValueLen != (CK_ULONG) -1)
+	o->extractable = (template[8].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[8].pValue) : CK_FALSE;
 
 	debug_print(" [  OK %s ] Private key loaded successfully S:%d D:%d T:%02lX",
@@ -359,16 +359,16 @@ int callback_public_keys(test_certs_t *objects,
 	}
 
 	o->public_handle = object_handle;
-	o->verify = (template[0].ulValueLen != (CK_ULONG) -1)
+	o->verify = (template[0].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[0].pValue) : CK_FALSE;
-	o->encrypt = (template[1].ulValueLen != (CK_ULONG) -1)
+	o->encrypt = (template[1].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[1].pValue) : CK_FALSE;
 	/* store key type in case there is no corresponding private key */
-	o->key_type = (template[2].ulValueLen != (CK_ULONG) -1)
+	o->key_type = (template[2].ulValueLen == sizeof(CK_KEY_TYPE))
 		? *((CK_KEY_TYPE *) template[2].pValue) : (CK_KEY_TYPE) -1;
-	o->wrap = (template[8].ulValueLen != (CK_ULONG) -1)
+	o->wrap = (template[8].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[8].pValue) : CK_FALSE;
-	o->derive_pub = (template[9].ulValueLen != (CK_ULONG) -1)
+	o->derive_pub = (template[9].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[9].pValue) : CK_FALSE;
 
 	/* check if we get the same public key as from the certificate */
@@ -390,7 +390,7 @@ int callback_public_keys(test_certs_t *objects,
 				BN_free(cert_n);
 				BN_free(n);
 				BN_free(e);
-				return -1; 
+				return -1;
 			}
 #endif
 			rv = BN_cmp(cert_n, n) == 0 && BN_cmp(cert_e, e) == 0;
@@ -751,23 +751,23 @@ int callback_secret_keys(test_certs_t *objects,
 	o->private_handle = object_handle;
 	/* For verification/encryption, we use the same key */
 	o->public_handle = object_handle;
-	o->key_type = (template[0].ulValueLen != (CK_ULONG) -1)
+	o->key_type = (template[0].ulValueLen == sizeof(CK_KEY_TYPE))
 		? *((CK_KEY_TYPE *) template[0].pValue) : (CK_KEY_TYPE) -1;
-	o->sign = (template[3].ulValueLen != (CK_ULONG) -1)
+	o->sign = (template[3].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[3].pValue) : CK_FALSE;
-	o->sign = (template[4].ulValueLen != (CK_ULONG) -1)
+	o->sign = (template[4].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[4].pValue) : CK_FALSE;
-	o->encrypt = (template[5].ulValueLen != (CK_ULONG) -1)
+	o->encrypt = (template[5].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[5].pValue) : CK_FALSE;
-	o->decrypt = (template[6].ulValueLen != (CK_ULONG) -1)
+	o->decrypt = (template[6].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[6].pValue) : CK_FALSE;
-	o->derive_priv = (template[7].ulValueLen != (CK_ULONG) -1)
+	o->derive_priv = (template[7].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[7].pValue) : CK_FALSE;
-	o->wrap = (template[8].ulValueLen != (CK_ULONG) -1)
+	o->wrap = (template[8].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[8].pValue) : CK_FALSE;
-	o->unwrap = (template[9].ulValueLen != (CK_ULONG) -1)
+	o->unwrap = (template[9].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[9].pValue) : CK_FALSE;
-	o->extractable = (template[12].ulValueLen != (CK_ULONG) -1)
+	o->extractable = (template[12].ulValueLen == sizeof(CK_BBOOL))
 		? *((CK_BBOOL *) template[12].pValue) : CK_FALSE;
 
 	if (template[10].ulValueLen > 0) {
