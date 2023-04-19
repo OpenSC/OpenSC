db0601f44fded9fd28631329a392581f3db1b3a7
Jakub Jelen <jjelen@redhat.com>
2023-04-19 16:47:37 +0200

    ci: Split code download and build
    This is useful for running the CI locally in containers, which makes
    things much faster than it would need to download, build and install
    all the test tools.

--- .github/setup-vsmartcard.sh
+++ .github/setup-vsmartcard.sh
@@ -4,7 +4,10 @@ set -ex -o xtrace
 
 if [ ! -d "vsmartcard" ]; then
 	git clone https://github.com/frankmorgner/vsmartcard.git
+	pushd vsmartcard/virtualsmartcard
+	autoreconf -vis && ./configure && make -j2
+	popd
 fi
 pushd vsmartcard/virtualsmartcard
-autoreconf -vis && ./configure && make -j2 && sudo make install
+sudo make install
 popd
