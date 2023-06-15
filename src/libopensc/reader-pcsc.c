8879ef5014621179c1b3a4492bf177b46fbe70ad
Veronika Hanulíková <vhanulik@redhat.com>
2023-06-15 10:59:00 +0200

    reader-pcsc: check card status when timeout is received
    When the reader is removed and inserted back between
    two consecutive callings to SCardGetStatusChange(),
    the change is not reported. The SC_READER_CARD_CHANGED
    should be set and propagated into higher levels.
    
    When no reconnection is done afterwards,
    SCardGetStatusChange() will still return timeout
    and card handle will be invalid, but SCardStatus()
    does not report that situation.

--- src/libopensc/reader-pcsc.c
+++ src/libopensc/reader-pcsc.c
@@ -378,14 +378,28 @@ static int refresh_attributes(sc_reader_t *reader)
 
 	if (rv != SCARD_S_SUCCESS) {
 		if (rv == (LONG)SCARD_E_TIMEOUT) {
-			/* Timeout, no change from previous recorded state. Make sure that
-			 * changed flag is not set. */
-			reader->flags &= ~SC_READER_CARD_CHANGED;
+			unsigned char atr[SC_MAX_ATR_SIZE];
+
 			/* Make sure to preserve the CARD_PRESENT flag if the reader was
 			 * reattached and we called the refresh_attributes too recently */
 			if (priv->reader_state.dwEventState & SCARD_STATE_PRESENT) {
 				reader->flags |= SC_READER_CARD_PRESENT;
 			}
+
+			/* Timeout should denote no change from previous recorded state,
+			 * but check if the card handle is valid */
+			reader->flags &= ~SC_READER_CARD_CHANGED;
+			if (priv->pcsc_card != 0) {
+				DWORD readers_len = 0, cstate = 0, prot, atr_len = SC_MAX_ATR_SIZE;
+				/* When reader is removed between two subsequent calls to refresh_attributes,
+				 * SCardGetStatusChange does not notice the change, test the card handle with SCardStatus */
+				rv = priv->gpriv->SCardStatus(priv->pcsc_card, NULL, &readers_len, &cstate, &prot, atr, &atr_len);
+				if (rv == (LONG)SCARD_W_REMOVED_CARD || rv == (LONG)SCARD_E_INVALID_VALUE)
+					reader->flags |= SC_READER_CARD_CHANGED;
+				/* If this happens, card must be reconnected, otherwise SCardGetStatusChange() will still return timeout
+				 * and card handle will be invalid. */
+			}
+
 			LOG_FUNC_RETURN(reader->ctx, SC_SUCCESS);
 		}
 
