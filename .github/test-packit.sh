babffb50342d39d1d92c8357b0523c6b9f6eb660
Jakub Jelen <jjelen@redhat.com>
2023-08-14 12:12:57 +0200

    ci: Fix building packit action and improve error reporting

--- .github/test-packit.sh
+++ .github/test-packit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -ex -o xtrace
+
 # Install build requirements
 REQUIREMENTS=$(sed -n -e '/^BuildRequires*/p' packaging/opensc.spec | sed 's/[^ ]* //')
-sudo dnf install -y ${REQUIREMENTS}
+dnf install -y ${REQUIREMENTS}
 
 # Run packit
 packit --debug build locally
