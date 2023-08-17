e02fbc3c3f26b563e9c33af19ad952234b36ef26
Jakub Jelen <jjelen@redhat.com>
2023-08-17 11:21:36 +0200

    setcos: Reformat for readability

--- src/pkcs15init/pkcs15-setcos.c
+++ src/pkcs15init/pkcs15-setcos.c
@@ -334,10 +334,10 @@ setcos_create_key(sc_profile_t *profile, sc_pkcs15_card_t *p15card,
 		LOG_TEST_RET(ctx, SC_ERROR_NOT_SUPPORTED, "Create key failed: RSA only supported");
 
 	/* Parameter check */
-	if ( (keybits < 512) || (keybits > 1024) || (keybits & 0x7))
+	if ((keybits < 512) || (keybits > 1024) || (keybits & 0x7))
 		LOG_TEST_RET(ctx, SC_ERROR_INVALID_ARGUMENTS, "Invalid key length");
 
-        sc_log(ctx,  "create private key ID:%s\n",  sc_pkcs15_print_id(&key_info->id));
+	sc_log(ctx,  "create private key ID:%s\n",  sc_pkcs15_print_id(&key_info->id));
 
 	/* Get the private key file */
 	r = setcos_new_file(profile, p15card->card, SC_PKCS15_TYPE_PRKEY_RSA, key_info->key_reference, &file);
@@ -350,20 +350,19 @@ setcos_create_key(sc_profile_t *profile, sc_pkcs15_card_t *p15card,
 	/* Replace the path of instantiated key template by the path from the object data. */
 	memcpy(&file->path, &key_info->path, sizeof(file->path));
 	file->id = file->path.value[file->path.len - 2] * 0x100
-	+ file->path.value[file->path.len - 1];
+			+ file->path.value[file->path.len - 1];
 
 	key_info->key_reference = file->path.value[file->path.len - 1] & 0xFF;
 
 	sc_log(ctx,  "Path of private key file to create %s\n", sc_print_path(&file->path));
 
 	r = sc_select_file(p15card->card, &file->path, NULL);
-	if (!r)   {
+	if (!r) {
 		r = sc_pkcs15init_delete_by_path(profile, p15card, &file->path);
 		if (r != SC_SUCCESS)
 			sc_file_free(file);
 		LOG_TEST_RET(ctx, r, "Failed to delete private key file");
-	}
-        else if (r != SC_ERROR_FILE_NOT_FOUND)    {
+	} else if (r != SC_ERROR_FILE_NOT_FOUND) {
 		sc_file_free(file);
 		LOG_TEST_RET(ctx, r, "Select private key file error");
 	}
@@ -492,7 +491,7 @@ setcos_generate_key(struct sc_profile *profile, struct sc_pkcs15_card *p15card,
 
 		keybits = ((raw_pubkey[0] * 256) + raw_pubkey[1]);  /* modulus bit length */
 		if (keybits != key_info->modulus_length)  {
-			sc_log(ctx, 
+			sc_log(ctx,
 				 "key-size from card[%"SC_FORMAT_LEN_SIZE_T"u] does not match[%"SC_FORMAT_LEN_SIZE_T"u]\n",
 				 keybits, key_info->modulus_length);
 			LOG_TEST_RET(ctx, SC_ERROR_PKCS15INIT, "Failed to generate key");
