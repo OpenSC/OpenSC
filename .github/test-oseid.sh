863d1ab0b453847d7503866be1fe7fe62502ab8e
Peter Popovec <popovec.peter@gmail.com>
2023-10-13 11:25:18 +0200

    Use the p11test utility on the OsEID card as well.
    	modified:   .github/test-oseid.sh
    	new file:   src/tests/p11test/oseid_ref.json

--- .github/test-oseid.sh
+++ .github/test-oseid.sh
@@ -47,12 +47,22 @@ echo | ./OsEID-tool INIT
 ./OsEID-tool UNWRAP-WRAP-TEST
 ./OsEID-tool DES-AES-UPLOAD-KEYS
 ./OsEID-tool SYM-CRYPT-TEST
+./OsEID-tool ERASE-CARD
+
+# initialize card for p11test
+
+pkcs15-init -C --so-pin 00000000 --so-puk 00000000
+pkcs15-init --store-pin --id 01 --pin 11111111 --puk 11111111 --so-pin 00000000
+pkcs15-init -F
+pkcs15-init --generate-key rsa/2048 --key-usage sign --pin 11111111 --auth-id 01 --id 1 --label 'RSA2k key'
+pkcs15-init --generate-key rsa/2048 --key-usage decrypt --pin 11111111 --auth-id 01 --id 2 --label 'RSA2k encryption key'
+pkcs15-init --generate-key ec/prime256v1 --key-usage sign --pin 11111111 --auth-id 01 --id 3
 popd
 
-# this does not work as we have random key IDs in here
 pushd src/tests/p11test/
-$VALGRIND ./p11test -s 0 -p 11111111 -o oseid.json || true
-#diff -u3 oseid_ref.json oseid.json
+
+$VALGRIND ./p11test -s 0 -p 11111111 -o oseid.json
+diff -u3 oseid_ref.json oseid.json
 popd
 
 # cleanup -- this would break later uses of pcscd
