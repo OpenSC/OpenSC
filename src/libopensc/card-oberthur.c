8b56e9ee54dbf7522c11ca7b707301d831c5ae08
Jakub Jelen <jjelen@redhat.com>
2023-10-31 10:17:11 +0100

    oberthur: Avoid memory leaks on error
    Thanks oss-fuzz
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=63745

--- src/libopensc/card-oberthur.c
+++ src/libopensc/card-oberthur.c
@@ -1288,9 +1288,9 @@ auth_generate_key(struct sc_card *card, int use_sm,
 	apdu.datalen = rv + 4;
 
 	rv = sc_transmit_apdu(card, &apdu);
-	LOG_TEST_RET(card->ctx, rv, "APDU transmit failed");
+	LOG_TEST_GOTO_ERR(card->ctx, rv, "APDU transmit failed");
 	rv = sc_check_sw(card, apdu.sw1, apdu.sw2);
-	LOG_TEST_RET(card->ctx, rv, "Card returned error");
+	LOG_TEST_GOTO_ERR(card->ctx, rv, "Card returned error");
 
 	memset(&tmp_path, 0, sizeof(struct sc_path));
 	tmp_path.type = SC_PATH_TYPE_FILE_ID;
@@ -1298,17 +1298,19 @@ auth_generate_key(struct sc_card *card, int use_sm,
 	memcpy(tmp_path.value, sbuf, 2);
 
 	rv = auth_select_file(card, &tmp_path, NULL);
-	LOG_TEST_RET(card->ctx, rv, "cannot select public key");
+	LOG_TEST_GOTO_ERR(card->ctx, rv, "cannot select public key");
 
 	rv = auth_read_component(card, SC_CARDCTL_OBERTHUR_KEY_RSA_PUBLIC,
 			1, apdu.resp, data->key_bits/8);
-	LOG_TEST_RET(card->ctx, rv, "auth_read_component() returned error");
+	LOG_TEST_GOTO_ERR(card->ctx, rv, "auth_read_component() returned error");
 
 	apdu.resplen = rv;
 
 	if (data->pubkey)   {
-		if (data->pubkey_len < apdu.resplen)
-			LOG_FUNC_RETURN(card->ctx, SC_ERROR_INVALID_ARGUMENTS);
+		if (data->pubkey_len < apdu.resplen) {
+			rv = SC_ERROR_INVALID_ARGUMENTS;
+			LOG_TEST_GOTO_ERR(card->ctx, rv, "invalid length received");
+		}
 
 		memcpy(data->pubkey,apdu.resp,apdu.resplen);
 	}
@@ -1319,6 +1321,9 @@ auth_generate_key(struct sc_card *card, int use_sm,
 	sc_log(card->ctx, "resulted public key len %"SC_FORMAT_LEN_SIZE_T"u",
 	       apdu.resplen);
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
+err:
+	free(apdu.resp);
+	return rv;
 }
 
 
