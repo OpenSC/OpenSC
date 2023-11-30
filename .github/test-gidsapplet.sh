4512efc64a3c7222e2f173446aa54bcc5dd94cb9
Jakub Jelen <jjelen@redhat.com>
2023-11-30 18:33:50 +0100

    ci: Fix GidsApplet build after https://github.com/vletoux/GidsApplet/pull/21

--- .github/test-gidsapplet.sh
+++ .github/test-gidsapplet.sh
@@ -15,7 +15,7 @@ export LD_LIBRARY_PATH=/usr/local/lib
 if [ ! -d "GidsApplet" ]; then
 	git clone https://github.com/vletoux/GidsApplet.git;
 fi
-javac -classpath jcardsim/target/jcardsim-3.0.5-SNAPSHOT.jar GidsApplet/src/com/mysmartlogon/gidsApplet/*.java;
+javac -classpath jcardsim/target/jcardsim-3.0.5-SNAPSHOT.jar GidsApplet/src/import4096/com/mysmartlogon/gidsApplet/Config.java GidsApplet/src/com/mysmartlogon/gidsApplet/*.java;
 echo "com.licel.jcardsim.card.applet.0.AID=A000000397425446590201" > gids_jcardsim.cfg;
 echo "com.licel.jcardsim.card.applet.0.Class=com.mysmartlogon.gidsApplet.GidsApplet" >> gids_jcardsim.cfg;
 echo "com.licel.jcardsim.card.ATR=3B80800101" >> gids_jcardsim.cfg;
