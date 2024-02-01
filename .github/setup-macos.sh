40226d32695206807d5adb813cf650af5e3528cf
Frank Morgner <frankmorgner@gmail.com>
2024-02-01 10:55:13 +0100

    CI: add worker for M1 macOS runner

--- .github/setup-macos.sh
+++ .github/setup-macos.sh
@@ -2,7 +2,7 @@
 
 set -ex -o xtrace
 
-brew install automake gengetopt help2man pkgconfig
+brew install automake gengetopt help2man pkgconfig libtool
 
 # openSCToken
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
