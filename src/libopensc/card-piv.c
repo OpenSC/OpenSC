OpenSC/OpenSC

commit f6b4a2e6582d5c1a37a04be475df88bee86f0bc3

PIV Secure Messaging as defined in NIST 800-73-4 5/2015
    Looking for other users who have cards that
    support PIV SM and VCI for testing.

    Changes to PIV code for SM as defined in NIST 800-73-4.
    Section 4.1 The Key Establishment Protocol is done in piv_sm_open.
    Step names and variable names were chosen to match
    the names used used in 800-73-4.
    piv_get_sm_apdu, piv_free_sm_apdu, and piv_sm_close
    use the builtin SM apdu handling.

    This version calls piv_sm_open once from piv_init.
    and card->sm_ctx.sm_mode is set. See TODO below.

    PR has been tested with pkcs11-tool -O and --test --login
    using a "IDEMIA ID-One PIV 2.4 on Cosmo V8.1" with vendor
    provided certificates (about 25 certificates and keys)
    and other data objects.

    The test card does not have a "pairing code object" need for VCI
    for use over a contactless interface (NFC), But code
    has been added to support pairing to allow testing.

    The PIV SM code is only enabled if ENABLE_SM, ENABLE_OPENSSL and
    OPENSSL_NO_EC  is not defined. It was tested with --disable_sm

    A card indicates it can suport SM in the response to SELECT_AID.
    If card can support SM, but OpenSC was built without ENABLE_SM a
    sc_log message will say so.

    card-piv.c use SC_APDU_FLAGS_SM_CHAINING from previous commit.
    This allows the PIV to pass a plain ADPU which needs
    command chaining, to SM and piv_get_sm_apdu and will
    encrypt and MAC the data before command chaining is done
    in apdu.c

    NIST sp800-73-4 3.3.2 extends pin policy usage flags for
    optional VCI and OCC are defined.

    Checked with valgrind, pkcs11 -O and pkcs11-tool --test --login

    If card supports SM it is turned on in piv_init, so if
    card is reset or interfered with from other process, SM will
    not restart.

    Signer certificates and CVC certificates are verified.

    If interfered with by other processes, and
    SM session is lost, it is restarted.

src/libopensc/pkcs15-piv.c
@@ -4,6 +4,7 @@
  *
  * Copyright (C) 2005,2006,2007,2008,2009,2010
  *               Douglas E. Engert <deengert@anl.gov>
+ * Copyright (C) 2020 Douglas E. Engert <deengert@gmail.com>
  *               2004, Nils Larsch <larsch@trustcenter.de>
  * Copyright (C) 2006, Identity Alliance,
  *               Thomas Harning <thomas.harning@identityalliance.com>
@@ -236,8 +237,8 @@ static int piv_detect_card(sc_pkcs15_card_t *p15card)
 	sc_card_t *card = p15card->card;
 
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
-	if (card->type < SC_CARD_TYPE_PIV_II_GENERIC
-		|| card->type >= SC_CARD_TYPE_PIV_II_GENERIC+1000)
+	if (card->type < SC_CARD_TYPE_PIV_II_BASE
+		|| card->type >= SC_CARD_TYPE_PIV_II_BASE + 1000)
 		return SC_ERROR_INVALID_CARD;
 	return SC_SUCCESS;
 }
@@ -320,21 +321,30 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 			"2.16.840.1.101.3.7.2.16.19", NULL, "1013", 0},
 	{"34", "Retired X.509 Certificate for Key Management 20",
 			"2.16.840.1.101.3.7.2.16.20", NULL, "1014", 0},
+	/* new in 800-73-4 */
+	{"35", "Biometric Information Templates Group Template",
+			"2.16.840.1.101.3.7.2.16.22", NULL, "1016", 0},
+	{"36", "Secure Messaging Certificate Signer",
+			"2.16.840.1.101.3.7.2.16.23", NULL, "1017", 0},
+	{"37", "Pairing Code Reference Data Container",
+			"2.16.840.1.101.3.7.2.16.24", NULL, "1018", SC_PKCS15_CO_FLAG_PRIVATE},
 	{NULL, NULL, NULL, NULL, NULL, 0}
 	};
 	// clang-format on
-	/*
-	 * NIST 800-73-1 lifted the restriction on
+	/* NIST 800-73-1 lifted the restriction on
 	 * requiring pin protected certs. Thus the default is to
 	 * not require this.
+	 *
+	 * Certs will be pulled out from the cert objects 
+	 * But there may be extra certs (SM Signer cert) that do
+	 * not have a private keys on the card. These certs must be last
 	 */
-	/* certs will be pulled out from the cert objects */
-	/* the number of cert, pubkey and prkey triplets */
 
-#define PIV_NUM_CERTS_AND_KEYS 24
+#define PIV_NUM_CERTS 25
+#define PIV_NUM_KEYS  24
 
 	// clang-format off
-	static const cdata certs[PIV_NUM_CERTS_AND_KEYS] = {
+	static const cdata certs[PIV_NUM_CERTS] = {
 		{"01", "Certificate for PIV Authentication", "0101cece", 0, 0},
 		{"02", "Certificate for Digital Signature", "0100cece", 0, 0},
 		{"03", "Certificate for Key Management", "0102cece", 0, 0},
@@ -358,7 +368,8 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 		{"21", "Retired Certificate for Key Management 17", "1011cece", 0, 0},
 		{"22", "Retired Certificate for Key Management 18", "1012cece", 0, 0},
 		{"23", "Retired Certificate for Key Management 19", "1013cece", 0, 0},
-		{"24", "Retired Certificate for Key Management 20", "1014cece", 0, 0}
+		{"24", "Retired Certificate for Key Management 20", "1014cece", 0, 0},
+		{"25", "Secure Messaging Certificate Signer",   "1017cece", 0, 0} /* no keys on card */
 	};
 	// clang-format on
 
@@ -395,7 +406,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	 * RSA and EC have different sets of usage
 	 */
 	// clang-format off
-	static const pubdata pubkeys[PIV_NUM_CERTS_AND_KEYS] = {
+	static const pubdata pubkeys[PIV_NUM_KEYS] = {
 		{ "01", "PIV AUTH pubkey",
 			 	/*RSA*/SC_PKCS15_PRKEY_USAGE_ENCRYPT |
 			 		SC_PKCS15_PRKEY_USAGE_WRAP |
@@ -505,11 +516,14 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	// clang-format on
 
 /*
- * note some of the SC_PKCS15_PRKEY values are dependent
+ * Note some of the SC_PKCS15_PRKEY values are dependent
  * on the key algorithm, and will be reset.
+ 
+ * No SM Signer private Key on card
+ * The 04 SM ECC CVC pubkey is in response to SELECT AID
  */
 	// clang-format off
-	static const prdata prkeys[PIV_NUM_CERTS_AND_KEYS] = {
+	static const prdata prkeys[PIV_NUM_KEYS] = {
 		{ "01", "PIV AUTH key",
 				/*RSA*/SC_PKCS15_PRKEY_USAGE_DECRYPT |
 					SC_PKCS15_PRKEY_USAGE_UNWRAP |
@@ -614,6 +628,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 				/*RSA*/SC_PKCS15_PRKEY_USAGE_DECRYPT | SC_PKCS15_PRKEY_USAGE_UNWRAP,
 				/*EC*/SC_PKCS15_PRKEY_USAGE_DERIVE,
 			"", 0x95, "01", SC_PKCS15_CO_FLAG_PRIVATE, 0}
+		/* SM Signer certificate does not have private key on card */
 	};
 	// clang-format on
 
@@ -621,7 +636,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	sc_card_t *card = p15card->card;
 	sc_serial_number_t serial;
 	char buf[SC_MAX_SERIALNR * 2 + 1];
-	common_key_info ckis[PIV_NUM_CERTS_AND_KEYS];
+	common_key_info ckis[PIV_NUM_CERTS];
 	int follows_nist_fascn = 0;
 	char *token_name = NULL;
 
@@ -685,6 +700,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 		
 		r = sc_pkcs15emu_object_add(p15card, SC_PKCS15_TYPE_DATA_OBJECT,
 			&obj_obj, &obj_info);
+
 		if (r < 0)
 			LOG_FUNC_RETURN(card->ctx, r);
 /* TODO
@@ -716,7 +732,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	 */
 	/* set certs */
 	sc_log(card->ctx,  "PIV-II adding certs...");
-	for (i = 0; i < PIV_NUM_CERTS_AND_KEYS; i++) {
+	for (i = 0; i < PIV_NUM_CERTS; i++) {
 		struct sc_pkcs15_cert_info cert_info;
 		struct sc_pkcs15_object    cert_obj;
 		sc_pkcs15_der_t   cert_der;
@@ -991,7 +1007,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	 * at a later time. The piv-tool can stash  pubkey in file
 	 */
 	sc_log(card->ctx,  "PIV-II adding pub keys...");
-	for (i = 0; i < PIV_NUM_CERTS_AND_KEYS; i++) {
+	for (i = 0; i < PIV_NUM_KEYS; i++) {
 		struct sc_pkcs15_pubkey_info pubkey_info;
 		struct sc_pkcs15_object     pubkey_obj;
 		struct sc_pkcs15_pubkey *p15_key = NULL;
@@ -1141,7 +1157,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 
 	/* set private keys */
 	sc_log(card->ctx,  "PIV-II adding private keys...");
-	for (i = 0; i < PIV_NUM_CERTS_AND_KEYS; i++) {
+	for (i = 0; i < PIV_NUM_KEYS; i++) {
 		struct sc_pkcs15_prkey_info prkey_info;
 		struct sc_pkcs15_object     prkey_obj;
 
@@ -1218,7 +1234,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 err:
-	for (i = 0; i < PIV_NUM_CERTS_AND_KEYS; i++) {
+	for (i = 0; i < PIV_NUM_CERTS; i++) {
 		sc_pkcs15_free_pubkey(ckis[i].pubkey_from_cert);
 	}
 	sc_pkcs15_card_clear(p15card);

etc/opensc.conf.example.in
@@ -226,6 +226,44 @@ app default {
 		# in the 'pkcs11' section below
 	}
 
+	card_driver PIV-II {
+		# "piv_max_object_size"
+		# defines the buffer size used to read PIV objects
+		# Although NIST sp800-73-4 lists object sizes, these are not hard limits.
+		# Other PIV-like cards may have larger objects.
+		# Maximum: 65535
+		# Minimum: 16384
+		# Default: 16384
+		# piv_max_object_size = 16384;
+		# May be set via environment: PIV_MAX_OBJECT_SIZE=16384
+
+		# "piv_use_sm"
+		# NIST sp800-73-4 supports Secure Messaging (SM), an optional feature.
+		#
+		# "piv_use_sm" may be set to:
+		#	never - Don't use it. Card will react the same as cards without SM.
+		#	always - Always use SM if card supports it for contact and contactless
+		#	default - If card supports SM use for contactless (NFC) and for PIN, crypto,
+		#                 and sensitive objects with contact.
+		# piv_use_sm = "default";
+		# May be set via environment: PIV_USE_SM=default
+
+		# "piv_pairing_code"
+		# Virtual Contact Interface (VCI) an optional feature, to allow
+		# contactless access to card as if it was using the contact reader.
+		# VCI requires SM. It may also require a pairing code which may be printed on the
+		# card, given to card owner when card issued or available from the card when used
+		# with a contact reader. Contact your card issuing agency for details on what your
+		# card supports.
+		# Pairing code can be set in opensc.conf or via environment (recommended).
+		# It is an 8 digit string.
+		# Default: no pairing code.
+		# piv_pairing_code = 12345678;
+		# May be set via environment: PIV_PAIRING_CODE=12345678
+
+		# Environment variables override opensc.conf
+	}
+
 	# In addition to the built-in list of known cards in the
 	# card driver, you can configure a new card for the driver
 	# using the card_atr block. The goal is to centralize

src/libopensc/cards.h
@@ -138,6 +138,7 @@ enum {
 	SC_CARD_TYPE_PIV_II_OBERTHUR,
 	SC_CARD_TYPE_PIV_II_PIVKEY,
 	SC_CARD_TYPE_PIV_II_SWISSBIT,
+	SC_CARD_TYPE_PIV_II_800_73_4,
 
 	/* MuscleApplet */
 	SC_CARD_TYPE_MUSCLE_BASE = 15000,

src/libopensc/types.h
@@ -311,7 +311,7 @@ typedef struct sc_file {
 #define SC_APDU_FLAGS_NO_RETRY_WL	0x00000004UL
 /* APDU is from Secure Messaging  */
 #define SC_APDU_FLAGS_NO_SM		0x00000008UL
-/* let SM do the comand haining  */
+/* let SM do the command chaining  */
 #define SC_APDU_FLAGS_SM_CHAINING	0x00000010UL
 
 #define SC_APDU_ALLOCATE_FLAG		0x01

commit 3b7aa83d6c3b44722dc8fe26b92b48bb610a67ec

PIV: added support for Yubikey's touch/PIN policy
fixes detection of Yubikey version, which was broken since f6b4a2e, see
https://github.com/OpenSC/OpenSC/pull/3071#discussion_r1539160476

fixes https://github.com/OpenSC/OpenSC/issues/1769

src/libopensc/card-piv.c
@@ -60,10 +60,11 @@
 #include "compression.h"
 #endif
 
-#include "internal.h"
 #include "asn1.h"
 #include "cardctl.h"
+#include "internal.h"
 #include "simpletlv.h"
+#include "ui/notify.h"
 
 enum {
 	PIV_OBJ_CCC = 0,
@@ -414,6 +415,11 @@ typedef struct piv_private_data {
 	unsigned int card_issues; /* card_issues flags for this card */
 	int object_test_verify; /* Can test this object to set verification state of card */
 	int yubico_version; /* 3 byte version number of NEO or Yubikey4  as integer */
+	struct {
+		u8 slot;
+		u8 policy;
+		u8 touch;
+	} yk_pin[26];
 	unsigned int ccc_flags;	    /* From  CCC indicate if CAC card */
 	unsigned int pin_policy; /* from discovery */
 	unsigned int init_flags;
@@ -4378,6 +4384,78 @@ static int piv_get_pin_preference(sc_card_t *card, int *pin_ref)
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 }
 
+static void
+piv_yk_metadata_get_policy(sc_context_t *ctx, u8 *buf, size_t buflen, u8 *pin, u8 *touch)
+{
+	size_t policylen;
+	const u8 *policy = sc_asn1_find_tag(ctx, buf, buflen, 0x02, &policylen);
+	if (policy && policylen == 2) {
+		*pin = policy[0];
+		*touch = policy[1];
+	} else {
+		sc_log(ctx, "Yubikey PIN policy not found");
+	}
+}
+
+static void
+piv_yk_get_metadata(sc_card_t *card, u8 slot, u8 *pin_policy, u8 *touch_policy)
+{
+	sc_apdu_t apdu;
+	u8 resp[100];
+	size_t i;
+	piv_private_data_t *priv = PIV_DATA(card);
+
+	/* initialize with the default behaviour */
+	if (pin_policy)
+		*pin_policy = 0x00;
+	if (touch_policy)
+		*touch_policy = 0x00;
+
+	if (priv->yubico_version < 0x00050300) {
+		if (priv->yubico_version != 0)
+			sc_log(card->ctx, "Yubikey's PIN and touch policy not available");
+		return;
+	}
+
+	for (i = 0; i < (sizeof(priv->yk_pin) / sizeof(*priv->yk_pin) - 1); i++) {
+		if (priv->yk_pin[i].slot == 0x00)
+			/* reached the last initialized entry */
+			break;
+
+		if (priv->yk_pin[i].slot == slot)
+			/* metadata already initialized */
+			break;
+	}
+
+	if (priv->yk_pin[i].slot == 0x00) {
+		/* initialize this entry */
+		sc_format_apdu_ex(&apdu, 0x00, 0xF7, 0x00, slot, NULL, 0, resp, sizeof resp);
+		if (SC_SUCCESS == sc_transmit_apdu(card, &apdu) && SC_SUCCESS == sc_check_sw(card, apdu.sw1, apdu.sw2)) {
+			piv_yk_metadata_get_policy(card->ctx, resp, apdu.resplen,
+					&priv->yk_pin[i].policy,
+					&priv->yk_pin[i].touch);
+			sc_log(card->ctx, "PIN policy for slot 0x%02X: 0x%02X (touch 0x%02X)",
+					slot, priv->yk_pin[i].policy, priv->yk_pin[i].touch);
+			priv->yk_pin[i].slot = slot;
+		}
+	}
+
+	if (priv->yk_pin[i].slot == slot) {
+		if (pin_policy)
+			*pin_policy = priv->yk_pin[i].policy;
+		if (touch_policy)
+			*touch_policy = priv->yk_pin[i].touch;
+	}
+}
+
+static int
+piv_yk_pin_policy(sc_card_t *card, u8 *ptr)
+{
+	u8 slot = *ptr;
+	piv_yk_get_metadata(card, slot, ptr, NULL);
+	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
+}
+
 static int piv_card_ctl(sc_card_t *card, unsigned long cmd, void *ptr)
 {
 	piv_private_data_t * priv = PIV_DATA(card);
@@ -4414,6 +4492,9 @@ static int piv_card_ctl(sc_card_t *card, unsigned long cmd, void *ptr)
 			break;
 		case SC_CARDCTL_PIV_OBJECT_PRESENT:
 			return piv_is_object_present(card, ptr);
+			break;
+		case SC_CARDCTL_PIV_YK_PIN_POLICY:
+			return piv_yk_pin_policy(card, ptr);
 			break;
 	}
 
@@ -4527,6 +4608,22 @@ static int piv_restore_security_env(sc_card_t *card, int se_num)
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 }
 
+static void
+piv_yk_notify_touch_policy(sc_card_t *card, u8 key_ref)
+{
+	u8 touch_policy;
+	const char *title = "Touch your Yubikey to continue";
+
+	piv_yk_get_metadata(card, key_ref, NULL, &touch_policy);
+	switch (touch_policy) {
+	case 0x02:
+		sc_notify(title, "Touching the token is required for unlocking the key");
+		break;
+	case 0x03:
+		sc_notify(title, "Touching the token is required unlocking the key (needed again after 15 seconds)");
+		break;
+	}
+}
 
 static int piv_validate_general_authentication(sc_card_t *card,
 					const u8 * data, size_t datalen,
@@ -4589,6 +4686,7 @@ static int piv_validate_general_authentication(sc_card_t *card,
 	}
 	/* EC alg_id was already set */
 
+	piv_yk_notify_touch_policy(card, priv->key_ref);
 	r = piv_general_io(card, 0x87, real_alg_id, priv->key_ref,
 			sbuf, p - sbuf, rbuf, sizeof rbuf);
 	if (r < 0)
@@ -4627,6 +4725,7 @@ piv_compute_signature(sc_card_t *card, const u8 * data, size_t datalen,
 	u8 rbuf[128]; /* For EC conversions  384 will fit */
 
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
 	/* The PIV returns a DER SEQUENCE{INTEGER, INTEGER}
 	 * Which may have leading 00 to force a positive integer
 	 * But PKCS11 just wants 2* field_length in bytes
@@ -5493,7 +5592,7 @@ static int piv_match_card_continued(sc_card_t *card)
 			apdu.resplen = sizeof(yubico_version_buf);
 			apdu.le = apdu.resplen;
 			r2 = sc_transmit_apdu(card, &apdu); /* on error yubico_version == 0 */
-			if (r2 >= 3) {
+			if (r2 == SC_SUCCESS && apdu.resplen == 3) {
 				priv->yubico_version = (yubico_version_buf[0]<<16) | (yubico_version_buf[1] <<8) | yubico_version_buf[2];
 				sc_log(card->ctx, "Yubico card->type=%d, r=0x%08x version=0x%08x", card->type, r, priv->yubico_version);
 			}

src/libopensc/cardctl.h
@@ -178,6 +178,7 @@ enum {
 	SC_CARDCTL_PIV_GENERATE_KEY,
 	SC_CARDCTL_PIV_PIN_PREFERENCE,
 	SC_CARDCTL_PIV_OBJECT_PRESENT,
+	SC_CARDCTL_PIV_YK_PIN_POLICY,
 
 	/*
 	 * CAC specific calls

src/libopensc/pkcs15-piv.c
@@ -1150,6 +1150,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	for (i = 0; i < PIV_NUM_KEYS; i++) {
 		struct sc_pkcs15_prkey_info prkey_info;
 		struct sc_pkcs15_object     prkey_obj;
+		u8 pin_policy = prkeys[i].ref;
 
 		memset(&prkey_info, 0, sizeof(prkey_info));
 		memset(&prkey_obj,  0, sizeof(prkey_obj));
@@ -1166,8 +1167,21 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 		prkey_obj.flags = prkeys[i].obj_flags;
 		prkey_obj.user_consent = prkeys[i].user_consent; /* only Sign key */
 
-		if (prkeys[i].auth_id)
+		sc_card_ctl(p15card->card, SC_CARDCTL_PIV_YK_PIN_POLICY, &pin_policy);
+		switch (pin_policy) {
+		case 0x02:
+			/* PIN is checked once for the session */
+			prkey_obj.user_consent = 0;
+			break;
+		case 0x03:
+			/* PIN is verified just before operation */
+			prkey_obj.user_consent = 1;
+		}
+		/* PIN is never checked for operations if PIN policy is set to 0x01 */
+		if (prkeys[i].auth_id && pin_policy != 0x01)
 			sc_pkcs15_format_id(prkeys[i].auth_id, &prkey_obj.auth_id);
+		else
+			prkey_obj.flags &= ~SC_PKCS15_CO_FLAG_PRIVATE;
 
 		/*
 		 * When no cert is present and a pubkey in a file was found,