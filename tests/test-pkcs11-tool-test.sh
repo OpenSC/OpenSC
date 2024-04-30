5289269d4a57563bdcd604ec091e6eda0f0547fd
Sergio Arroutbi <sarroutb@redhat.com>
2024-04-30 18:30:16 +0200

    Include URIs for PKCS#11 objects
    Fixes: #3129
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tests/test-pkcs11-tool-test.sh
+++ tests/test-pkcs11-tool-test.sh
@@ -20,12 +20,28 @@ assert $? "Failed to set up card"
 echo "======================================================="
 echo "Test"
 echo "======================================================="
-$PKCS11_TOOL --test -p $PIN --module $P11LIB
+$PKCS11_TOOL --test -p "${PIN}" --module "${P11LIB}"
 assert $? "Failed running tests"
+
+echo "======================================================="
+echo "Test objects URI"
+echo "======================================================="
+$PKCS11_TOOL -O 2>/dev/null | grep 'uri:' 2>/dev/null >/dev/null
+assert $? "Failed running objects URI tests"
+$PKCS11_TOOL -O 2>/dev/null | grep 'uri:' | awk -F 'uri:' '{print $2}' | tr -d ' ' | grep ^"pkcs11:" 2>/dev/null >/dev/null
+assert $? "Failed running objects URI tests"
+
+echo "======================================================="
+echo "Test slots URI"
+echo "======================================================="
+$PKCS11_TOOL -L 2>/dev/null | grep 'uri' 2>/dev/null >/dev/null
+assert $? "Failed running slots URI tests"
+$PKCS11_TOOL -O 2>/dev/null | grep 'uri' | awk -F 'uri*:' '{print $2}' | tr -d ' '  | grep ^"pkcs11:" 2>/dev/null >/dev/null
+assert $? "Failed running slots URI tests"
 
 echo "======================================================="
 echo "Cleanup"
 echo "======================================================="
 card_cleanup
 
-exit $ERRORS
+exit "${ERRORS}"
