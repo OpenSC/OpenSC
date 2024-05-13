OpenSC/OpenSC

commit 460a862ee043672b5114c1a0b8d429c587764c89

pkcs15-cert: Avoid memory leaks
Thanks oss-fuzz

https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=24056

src/libopensc/pkcs15-cert.c
@@ -108,7 +108,6 @@ parse_x509_cert(sc_context_t *ctx, struct sc_pkcs15_der *der, struct sc_pkcs15_c
 	if (!pubkey)
 		LOG_TEST_GOTO_ERR(ctx, SC_ERROR_INVALID_ASN1_OBJECT, "Unable to decode subjectPublicKeyInfo from cert");
 
-	sc_asn1_clear_algorithm_id(&sig_alg);
 
 	if (serial && serial_len)   {
 		sc_format_asn1_entry(asn1_serial_number + 0, serial, &serial_len, 1);
@@ -129,6 +128,8 @@ parse_x509_cert(sc_context_t *ctx, struct sc_pkcs15_der *der, struct sc_pkcs15_c
 	}
 
 err:
+	/* not used for anything */
+	sc_asn1_clear_algorithm_id(&sig_alg);
 	free(serial);
 	free(subject);
 	free(issuer);

commit 36dedb74003fef769e038f5a4c10e2a262009161

cert: Avoid use of uninitialized memory on errors
Thanks oss-fuzz and deengert for pointing the issue out.

This issue was introduced in
460a862ee043672b5114c1a0b8d429c587764c89

https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=66192

src/libopensc/pkcs15-cert.c
@@ -39,7 +39,7 @@ static int
 parse_x509_cert(sc_context_t *ctx, struct sc_pkcs15_der *der, struct sc_pkcs15_cert *cert)
 {
 	int r;
-	struct sc_algorithm_id sig_alg;
+	struct sc_algorithm_id sig_alg = {0};
 	struct sc_pkcs15_pubkey *pubkey = NULL;
 	unsigned char *serial = NULL, *issuer = NULL, *subject = NULL, *buf =  der->value;
 	size_t serial_len = 0, issuer_len = 0, subject_len = 0, data_len = 0, buflen = der->len;