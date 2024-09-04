1e39a07c331a1a8c784fd460008ef62c07353171
dlegaultbbry <dlegault@blackberry.com>
2024-09-04 08:45:12 -0400

    pkcs11-tool: Add Edwards/Montgomery Curve Information
    - Add the information for the various RFC versions of
      the Edwards (Ed25519/Ed448) curves
    - Add the information for the various RFC versions of
      the Montgomery (X25519/X448) curves
    
    This was pulled as a separate change from Doug Engert's X25519 improvements
    changeset so that EdDSA support in pkcs11-tool had proper curve support.
    
    Related #2952

--- src/tools/pkcs11-tool.c
+++ src/tools/pkcs11-tool.c
@@ -147,8 +147,23 @@ static struct ec_curve_info {
 	{"secp256k1",		"1.3.132.0.10", "06052B8104000A", 256, 0},
 	{"secp521k1",		"1.3.132.0.35", "06052B81040023", 521, 0},
 
-	{"edwards25519","1.3.6.1.4.1159.15.1", "130c656477617264733235353139", 255, CKM_EC_EDWARDS_KEY_PAIR_GEN},
-	{"curve25519", "1.3.6.1.4.3029.1.5.1", "130b63757276653235353139", 255, CKM_EC_MONTGOMERY_KEY_PAIR_GEN},
+	/* Some of the following may not yet be supported by the OpenSC module, but may be by other modules */
+	/* OpenPGP extensions by Yubikey and GNUK are not defined in RFCs, so pass by printable string */
+	/* See PKCS#11 3.0 2.3.7 */
+	{"edwards25519", "1.3.6.1.4.1.11591.15.1", "130c656477617264733235353139", 255, CKM_EC_EDWARDS_KEY_PAIR_GEN}, /* send by curve name */
+	{"curve25519",   "1.3.6.1.4.1.3029.1.5.1", "130a63757276653235353139",     255, CKM_EC_MONTGOMERY_KEY_PAIR_GEN}, /* send by curve name */
+
+	/* RFC8410, EDWARDS and MONTGOMERY curves are used by GnuPG and also by OpenSSL */
+
+	{"X25519",  "1.3.101.110", "06032b656e", 255, CKM_EC_MONTGOMERY_KEY_PAIR_GEN}, /* RFC 4810 send by OID */
+	{"X448",    "1.3.101.111", "06032b656f", 448, CKM_EC_MONTGOMERY_KEY_PAIR_GEN}, /* RFC 4810 send by OID */
+	{"Ed25519", "1.3.101.112", "06032b6570", 255, CKM_EC_EDWARDS_KEY_PAIR_GEN}, /* RFC 4810 send by OID */
+	{"Ed448",   "1.3.101.113", "06032b6571", 448, CKM_EC_EDWARDS_KEY_PAIR_GEN}, /* RFC 4810 send by OID */
+
+	/* GnuPG openpgp curves as used in gnupg-card are equivalent to RFC8410 OIDs */
+	{"cv25519", "1.3.101.110", "06032b656e", 255, CKM_EC_MONTGOMERY_KEY_PAIR_GEN},
+	{"ed25519", "1.3.101.112", "06032b6570", 255, CKM_EC_EDWARDS_KEY_PAIR_GEN},
+	/* OpenSC card-openpgp.c will map these to what is need on the card */
 
 	{NULL, NULL, NULL, 0, 0},
 };
