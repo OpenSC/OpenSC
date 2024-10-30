7ac7452f4c78b8bb261a589501f111b05f9a70a0
Doug Engert <deengert@gmail.com>
2024-10-30 08:43:45 -0500

    pkcs15-init.c - fixup spelling
     On branch X25519-improvements-2
     Changes to be committed:
    	modified:   tools/pkcs15-init.c

--- src/tools/pkcs15-init.c
+++ src/tools/pkcs15-init.c
@@ -791,7 +791,7 @@ parse_alg_spec(const struct alg_spec *types, const char *spec, unsigned int *key
 				util_error("If specified, number of key bits must be \"%d\" for \"%s\"", *keybits, types[types_idx].spec);
 				return SC_ERROR_INVALID_ARGUMENTS;
 			}
-		} else {  /* rsa or symetric key */
+		} else {  /* rsa or symmetric key */
 			*keybits = (unsigned)strtoul(spec, &end, 10);
 			if (*end) {
 				util_error("Invalid number of key bits \"%s\"", spec);
