5c939d949dca308349140d6c5a963a8aef4ab251
Doug Engert <deengert@gmail.com>
2024-11-25 21:16:38 -0600

    test-pkcs11-tool-test-threads.sh  - skip if opensc-tool not present
    As suggested in PR comments
    
     On branch X25519-improvements-2
     Changes to be committed:
    	modified:   tests/test-pkcs11-tool-test-threads.sh

--- tests/test-pkcs11-tool-test-threads.sh
+++ tests/test-pkcs11-tool-test-threads.sh
@@ -20,6 +20,17 @@ fi
 echo "======================================================="
 echo "Test pkcs11 threads IN "
 echo "======================================================="
+OPENSC_TOOL="../src/tools/opensc-tool"
+echo "check for opensc-tool"
+if [[ -f $OPENSC_TOOL ]] ; then
+echo "trying opensc-tool -a"
+	$OPENSC_TOOL -a
+	if [[ "$?" -ne "0" ]] ; then
+		echo "No token found, skipping Test pkcs11 threads "
+		exit 77
+	fi
+fi
+
 $PKCS11_TOOL --test-threads IN -L --module="$P11LIB"
 assert $? "Failed running tests"
 
