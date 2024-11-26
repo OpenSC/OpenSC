0a7b390dbeabf3eac256731481ff2aefef3a9866
swissbit-zlu <zeren.lu@swissbit.com>
2024-11-26 16:51:20 +0100

    add nxp ishield key pro atr (#7)

--- src/libopensc/card-piv.c
+++ src/libopensc/card-piv.c
@@ -529,6 +529,11 @@ static const struct sc_atr_table piv_atrs[] = {
 	{ "3b:97:11:81:21:75:69:53:68:69:65:6c:64:05", NULL, NULL, SC_CARD_TYPE_PIV_II_SWISSBIT, 0, NULL },
 	/* Swissbit iShield Key Pro with PIV endpoint applet contactless */
 	{ "3b:87:80:01:69:53:68:69:65:6c:64:50", NULL, NULL, SC_CARD_TYPE_PIV_II_SWISSBIT, 0, NULL },
+	/* Swissbit iShield Key 2 with PIV endpoint applet */
+	{ "3b:d5:18:ff:81:b1:fe:45:1f:c3:80:73:c8:21:10:6f", NULL, NULL,
+	SC_CARD_TYPE_PIV_II_SWISSBIT, 0, NULL },
+	/* Swissbit iShield Key 2 with PIV endpoint applet contactless */
+	{ "3b:85:80:01:80:73:c8:21:10:0e", NULL, NULL, SC_CARD_TYPE_PIV_II_SWISSBIT, 0, NULL },
 
 	/* ID-One PIV 2.4.1 on Cosmo V8.1 NIST sp800-73-4 with Secure Messaging and VCI  2020 */
 	{ "3b:d6:96:00:81:b1:fe:45:1f:87:80:31:c1:52:41:1a:2a", NULL, NULL, SC_CARD_TYPE_PIV_II_800_73_4, 0, NULL },
