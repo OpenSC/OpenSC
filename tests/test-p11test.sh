6515f1dad973eb730a185b322bf65165a6235eea
Veronika Hanulíková <vhanulik@redhat.com>
2025-01-09 15:12:25 +0100

    Fix running p11test in FIPS mode

--- tests/test-p11test.sh
+++ tests/test-p11test.sh
@@ -32,7 +32,12 @@ function filter_log() {
 	sed -n '/readonly_tests/,$p' $1
 }
 
-diff -U3 <(filter_log $SOURCE_PATH/tests/softhsm_ref.json) <(filter_log softhsm.json)
+REF_FILE="$SOURCE_PATH/tests/softhsm_ref.json"
+if [[ -f "/proc/sys/crypto/fips_enabled" && $(cat /proc/sys/crypto/fips_enabled) == "1" ]]; then
+	REF_FILE="$SOURCE_PATH/tests/softhsm_fips_ref.json"
+fi
+
+diff -U3 <(filter_log $REF_FILE) <(filter_log softhsm.json)
 assert $? "Unexpected results"
 
 echo "======================================================="
@@ -42,7 +47,7 @@ export PKCS11SPY="$P11LIB"
 $VALGRIND ./../src/tests/p11test/p11test -v -m ../src/pkcs11/.libs/pkcs11-spy.so -o softhsm.json -p $PIN
 assert $? "Failed running tests"
 
-diff -U3 <(filter_log $SOURCE_PATH/tests/softhsm_ref.json) <(filter_log softhsm.json)
+diff -U3 <(filter_log $REF_FILE) <(filter_log softhsm.json)
 assert $? "Unexpected results with PKCS11 spy"
 
 rm softhsm.json
