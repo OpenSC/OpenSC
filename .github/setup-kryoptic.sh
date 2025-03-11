7725950090753362d7931cab7fcdafdecfd1b7f9
Veronika Hanulíková <vhanulik@redhat.com>
2025-03-11 10:08:19 +0100

    Remove pulling openssl submodules

--- .github/setup-kryoptic.sh
+++ .github/setup-kryoptic.sh
@@ -5,7 +5,5 @@ if [ ! -d "kryoptic" ]; then
 	git clone https://github.com/latchset/kryoptic.git
 fi
 pushd kryoptic
-git submodule init
-git submodule update
 cargo build --features dynamic,standard,nssdb
 popd
