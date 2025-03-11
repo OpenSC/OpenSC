e23688d0e14f4ca770aa608d007ea37d43b1dacc
Veronika Hanulíková <vhanulik@redhat.com>
2025-03-11 10:24:02 +0100

    Add separate test scripts for each token

---
+++ tests/test-kryoptic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SOURCE_PATH=${SOURCE_PATH:-..}
+
+echo "Running all supported tests for Kryoptic token..."
+
+$SOURCE_PATH/tests/test-pkcs11-tool-sign-verify.sh kryoptic
+$SOURCE_PATH/tests/test-pkcs11-tool-import.sh kryoptic kryoptic
+$SOURCE_PATH/tests/test-pkcs11-tool-allowed-mechanisms.sh kryoptic
+$SOURCE_PATH/tests/test-pkcs11-tool-test.sh kryoptic
