9daeb8bcc0683ad72931c33b6efd585067acc614
Veronika Hanulíková <vhanulik@redhat.com>
2025-03-12 09:49:01 +0100

    Run p11test also on Kryoptic

--- tests/test-kryoptic.sh
+++ tests/test-kryoptic.sh
@@ -3,6 +3,7 @@ SOURCE_PATH=${SOURCE_PATH:-..}
 
 echo "Running all supported tests for Kryoptic token..."
 
+$SOURCE_PATH/tests/test-p11test.sh kryoptic
 $SOURCE_PATH/tests/test-pkcs11-tool-sign-verify.sh kryoptic
 $SOURCE_PATH/tests/test-pkcs11-tool-import.sh kryoptic kryoptic
 $SOURCE_PATH/tests/test-pkcs11-tool-allowed-mechanisms.sh kryoptic
