75039acf88d0a3d16902fe1d677a829e09c8a378
Veronika Hanulíková <vhanulik@redhat.com>
2025-03-12 09:48:44 +0100

    Add rest of the tests for SoftHSM

--- tests/test-softhsm.sh
+++ tests/test-softhsm.sh
@@ -3,7 +3,11 @@ SOURCE_PATH=${SOURCE_PATH:-..}
 
 echo "Running all supported tests for SoftHSM token..."
 
-$SOURCE_PATH/tests/test-pkcs11-tool-sign-verify.sh softhsm
-$SOURCE_PATH/tests/test-pkcs11-tool-import.sh softhsm
+$SOURCE_PATH/tests/test-p11test.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-allowed-mechanisms.sh softhsm
+$SOURCE_PATH/tests/test-pkcs11-tool-import.sh softhsm
+$SOURCE_PATH/tests/test-pkcs11-tool-sign-verify.sh softhsm
+$SOURCE_PATH/tests/test-pkcs11-tool-sym-crypt.sh softhsm
+$SOURCE_PATH/tests/test-pkcs11-tool-test-threads.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-test.sh softhsm
+$SOURCE_PATH/tests/test-pkcs11-tool-unwrap-wrap-test.sh softhsm
