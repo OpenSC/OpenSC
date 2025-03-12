0ea48eb0903aec21d74fd758b422be38b9f91b27
Veronika Hanulíková <vhanulik@redhat.com>
2025-03-12 09:49:01 +0100

    Run p11test also on Kryoptic

--- tests/test-p11test.sh
+++ tests/test-p11test.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 SOURCE_PATH=${SOURCE_PATH:-..}
 
-source $SOURCE_PATH/tests/common.sh softhsm
+TOKENTYPE=$1
+
+if [ "${TOKENTYPE}" == "softokn" ]; then
+	echo "p11test not supported"
+	exit 1
+elif [ "${TOKENTYPE}" == "" ]; then
+    TOKENTYPE=softhsm
+    echo "No tokentype provided, running with SoftHSM"
+fi
+
+source $SOURCE_PATH/tests/common.sh $TOKENTYPE
 
 echo "======================================================="
-echo "Setup SoftHSM"
+echo "Setup $TOKENTYPE"
 echo "======================================================="
 if [[ ! -f $P11LIB ]]; then
     echo "WARNING: The SoftHSM is not installed. Can not run this test"
@@ -17,10 +27,10 @@ assert $? "Failed to set up card"
 echo "======================================================="
 echo "Run p11test"
 echo "======================================================="
-$VALGRIND ./../src/tests/p11test/p11test -v -m $P11LIB -o softhsm.json -p $PIN
+$VALGRIND ./../src/tests/p11test/p11test -v -m $P11LIB -o $TOKENTYPE.json -p $PIN
 assert $? "Failed running tests"
 
-# Run the input shrough sed to skip the mechanism part:
+# Run the input through sed to skip the mechanism part:
 #  * broken because of uninitialized memory in softhsm
 #  * different for different softhsm versions
 # and interface tests
@@ -29,25 +39,25 @@ function filter_log() {
 	sed -n '/readonly_tests/,$p' $1
 }
 
-REF_FILE="$SOURCE_PATH/tests/softhsm_ref.json"
+REF_FILE="$SOURCE_PATH/tests/${TOKENTYPE}_ref.json"
 if [[ -f "/proc/sys/crypto/fips_enabled" && $(cat /proc/sys/crypto/fips_enabled) == "1" ]]; then
-	REF_FILE="$SOURCE_PATH/tests/softhsm_fips_ref.json"
+	REF_FILE="$SOURCE_PATH/tests/${TOKENTYPE}_fips_ref.json"
 fi
 
-diff -U3 <(filter_log $REF_FILE) <(filter_log softhsm.json)
+diff -U3 <(filter_log $REF_FILE) <(filter_log $TOKENTYPE.json)
 assert $? "Unexpected results"
 
 echo "======================================================="
 echo "Run p11test with PKCS11SPY"
 echo "======================================================="
 export PKCS11SPY="$P11LIB"
-$VALGRIND ./../src/tests/p11test/p11test -v -m ../src/pkcs11/.libs/pkcs11-spy.so -o softhsm.json -p $PIN
+$VALGRIND ./../src/tests/p11test/p11test -v -m ../src/pkcs11/.libs/pkcs11-spy.so -o $TOKENTYPE.json -p $PIN
 assert $? "Failed running tests"
 
-diff -U3 <(filter_log $REF_FILE) <(filter_log softhsm.json)
+diff -U3 <(filter_log $REF_FILE) <(filter_log $TOKENTYPE.json)
 assert $? "Unexpected results with PKCS11 spy"
 
-rm softhsm.json
+rm $TOKENTYPE.json
 
 echo "======================================================="
 echo "Cleanup"
