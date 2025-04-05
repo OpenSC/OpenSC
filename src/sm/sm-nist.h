30f8050125866f65cfc9b8feb95fc50f26d69485
Doug Engert <deengert@gmail.com>
2025-04-05 11:48:00 -0500

    sm/sm-nist.h sm/sm-nist.c -  define sm_nist_params_t
    sm_nist_params_t is used to pass parameters and shared data
    to sm_nist_start. This allows a card driver to provide
    certificates and flags outside of the strict PIV requirements
    so other card drivers can use the NIST SM as defined
    in NIST sp800-73-4 but store the certificates X509
    or CVC as PKCS15 files.
    
    sm_nist_params_cleanup can be used by a card driver to cleanup
    the sm_nist_params_t which can be part of a card driver's private data.
    
     On branch sm-nist
     Changes to be committed:
    	modified:   src/sm/sm-nist.c
    	modified:   src/sm/sm-nist.h

--- src/sm/sm-nist.h
+++ src/sm/sm-nist.h
@@ -33,46 +33,59 @@
 #ifdef ENABLE_ZLIB
 #include "libopensc/compression.h"
 #endif
-#include "libopensc/opensc.h"
 #include "libopensc/internal.h"
+#include "libopensc/opensc.h"
 #include "sm/sm-iso.h"
 
-#define PIV_PAIRING_CODE_LEN	8
+#define PIV_PAIRING_CODE_LEN 8
 
-/* for sm_flags  must match flag in card-piv.c*/
+/* sm_flags must match flag in card-piv.c*/
 
-#define PIV_SM_FLAGS_SM_CERT_SIGNER_VERIFIED	0x00000001lu
-#define PIV_SM_FLAGS_SM_CVC_VERIFIED		0x00000002lu
-#define PIV_SM_FLAGS_SM_IN_CVC_VERIFIED		0x00000004lu
-#define PIV_SM_FLAGS_SM_CERT_SIGNER_PRESENT	0x00000010lu
-#define PIV_SM_FLAGS_SM_CVC_PRESENT		0x00000020lu
-#define PIV_SM_FLAGS_SM_IN_CVC_PRESENT		0x00000040lu
-#define PIV_SM_FLAGS_SM_IS_ACTIVE		0x00000080lu	/* SM has been started */
-	/* if card supports SP800-73-4 SM: */
-#define PIV_SM_FLAGS_NEVER			0x00000100lu	/* Don't use SM even if card support it */
-								/* Default is use if card supports it */
-								/* will use VCI if card supports it for contactless */
-#define PIV_SM_FLAGS_ALWAYS			0x00000200lu	/* Use SM or quit, VCI requires SM */
-#define PIV_SM_FLAGS_DEFER_OPEN			0x00001000lu	/* call sm_open from reader_lock_obtained */
-#define PIV_SM_VCI_ACTIVE			0x00002000lu    /* VCI is active */
-#define PIV_SM_GET_DATA_IN_CLEAR		0x00004000lu	/* OK to do this GET DATA in the clear */
-#define PIV_SM_FLAGS_SM_CERT_SIGNER_COMPRESSED	0x00008000lu	/* compressed */
-#define PIV_SM_CONTACTLESS			0x00010000lu	/* contacless */
+#define PIV_SM_FLAGS_SM_CERT_SIGNER_VERIFIED 0x00000001lu
+#define PIV_SM_FLAGS_SM_CVC_VERIFIED	     0x00000002lu
+#define PIV_SM_FLAGS_SM_IN_CVC_VERIFIED	     0x00000004lu
+#define PIV_SM_FLAGS_SM_CERT_SIGNER_PRESENT  0x00000010lu
+#define PIV_SM_FLAGS_SM_CVC_PRESENT	     0x00000020lu
+#define PIV_SM_FLAGS_SM_IN_CVC_PRESENT	     0x00000040lu
+#define PIV_SM_FLAGS_SM_IS_ACTIVE	     0x00000080lu /* SM has been started */
+							  /* if card supports SP800-73-4 SM: */
+#define PIV_SM_FLAGS_NEVER 0x00000100lu			  /* Don't use SM even if card support it */
+/* Default is use if card supports it */
+/* will use VCI if card supports it for contactless */
+#define PIV_SM_FLAGS_ALWAYS		       0x00000200lu /* Use SM or quit, VCI requires SM */
+#define PIV_SM_FLAGS_DEFER_OPEN		       0x00001000lu /* call sm_open from reader_lock_obtained */
+#define PIV_SM_VCI_ACTIVE		       0x00002000lu /* VCI is active */
+#define PIV_SM_GET_DATA_IN_CLEAR	       0x00004000lu /* OK to do this GET DATA in the clear */
+#define PIV_SM_FLAGS_SM_CERT_SIGNER_COMPRESSED 0x00008000lu /* compressed */
+#define PIV_SM_CONTACTLESS		       0x00010000lu /* contacless */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int
-sm_nist_start(sc_card_t *card,
-		u8 *signer_cert, size_t signer_cert_len,
-		u8 *sm_in_cvc, size_t sm_in_len,
-		unsigned long *sm_flags, /* shared with caller */
-		unsigned long pin_policy,
-		u8 pairing_code[PIV_PAIRING_CODE_LEN],
-		u8 cipher_suite_id);
+/*
+ * Parameters shared between card driver and sm_nist
+ * Owned by card driver and cleared by sc_nist_parms_cleanup
+ */
 
-#ifdef  __cplusplus
+typedef struct sm_nist_params {
+	unsigned long flags; /* PIV_SM_* */
+	u8 *signer_cert_der;
+	size_t signer_cert_der_len;
+	u8 *sm_in_cvc_der;
+	size_t sm_in_cvc_der_len;
+	unsigned long pin_policy;
+	u8 pairing_code[PIV_PAIRING_CODE_LEN];
+	u8 csID; /* 0x27 or 0x2E */
+} sm_nist_params_t;
+
+int
+sm_nist_start(sc_card_t *card, sm_nist_params_t *params);
+
+int
+sm_nist_params_cleanup(sm_nist_params_t *params);
+
+#ifdef __cplusplus
 }
 #endif
 #endif
