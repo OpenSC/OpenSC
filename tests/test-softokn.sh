a9f77bb242e0cf30909f083389b87cd38e9a81b6
Veronika Hanulíková <vhanulik@redhat.com>
2025-04-16 14:41:21 +0200

    Rework wrap/unwrap tests and enable them for softokn

--- tests/test-softokn.sh
+++ tests/test-softokn.sh
@@ -5,3 +5,4 @@ echo "Running all supported tests for NSS Softokn token..."
 
 $SOURCE_PATH/tests/test-pkcs11-tool-sign-verify.sh softokn
 $SOURCE_PATH/tests/test-pkcs11-tool-import.sh softokn
+$SOURCE_PATH/tests/test-pkcs11-tool-unwrap-wrap-test.sh softokn
