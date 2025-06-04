ab568f0a420931d5372bc8657f4024ca6d0e3285
Jakub Jelen <jjelen@redhat.com>
2025-06-04 17:08:20 +0200

    iasecc: Properly set resplen when emulating fcp (1)
    Previously, this could lead to use uninitialized memory as
    the caller would handle the returned apdu buffer as longer
    than it actually is.
    
    Thanks Matteo Marini for the report!
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -862,7 +862,8 @@ iasecc_emulate_fcp(struct sc_context *ctx, struct sc_apdu *apdu)
 	memcpy(dummy_df_fcp + 16, apdu->data, apdu->datalen);
 	dummy_df_fcp[15] = apdu->datalen;
 	dummy_df_fcp[1] = apdu->datalen + 14;
-	memcpy(apdu->resp, dummy_df_fcp, apdu->datalen + 16);
+	apdu->resplen = apdu->datalen + 16;
+	memcpy(apdu->resp, dummy_df_fcp, apdu->resplen);
 
 	LOG_FUNC_RETURN(ctx, SC_SUCCESS);
 }
