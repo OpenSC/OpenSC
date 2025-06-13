1b6889ff3c6b9d44d30fab3aac48fea6ad975083
Raul Metsma <raul@metsma.ee>
2025-06-13 16:06:05 +0300

    Remove autotools dependency from NMake build
    Signed-off-by: Raul Metsma <raul@metsma.ee>

---
+++ VERSION.mk
@@ -0,0 +1,17 @@
+PRODUCT_NAME=OpenSC
+PRODUCT_TARNAME=opensc
+PRODUCT_BUGREPORT=https://github.com/OpenSC/OpenSC/issues
+PRODUCT_URL=https://github.com/OpenSC/OpenSC
+PACKAGE_VERSION_MAJOR=0
+PACKAGE_VERSION_MINOR=26
+PACKAGE_VERSION_FIX=1
+PACKAGE_VERSION_REVISION=0
+PACKAGE_SUFFIX=
+PACKAGE_SCM_REVISION=No Git revision info available
+
+VS_FF_LEGAL_COPYRIGHT=OpenSC Project
+VS_FF_LEGAL_COMPANY_NAME=OpenSC Project
+VS_FF_LEGAL_COMPANY_URL=https://github.com/OpenSC
+VS_FF_COMMENTS=Provided under the terms of the GNU Lesser General Public License (LGPLv2.1+).
+VS_FF_PRODUCT_NAME=OpenSC smartcard framework
+VS_FF_PRODUCT_UPDATES=https://github.com/OpenSC/OpenSC/releases
