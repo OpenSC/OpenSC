fc6b860edb2a5f0b8ffdca83b4713103c399d60a
Jakub Jelen <jjelen@redhat.com>
2025-11-13 14:10:06 +0100

    tests: Fix test name in script

--- tests/test-softhsm.sh
+++ tests/test-softhsm.sh
@@ -7,7 +7,7 @@ $SOURCE_PATH/tests/test-p11test.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-allowed-mechanisms.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-import.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-sign-verify.sh softhsm
-$SOURCE_PATH/tests/test-pkcs11-tool-sym-crypt.sh softhsm
+$SOURCE_PATH/tests/test-pkcs11-tool-sym-crypt-test.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-test-threads.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-test.sh softhsm
 $SOURCE_PATH/tests/test-pkcs11-tool-unwrap-wrap-test.sh softhsm
