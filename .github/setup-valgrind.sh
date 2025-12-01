530bcd3c02946de7346dc4a3026fc8a91e40540f
Jakub Jelen <jjelen@redhat.com>
2025-12-01 23:02:25 +0100

    ci: Try to use debuginfod instead of manual download of ddebs
    ... which is fragile given that ubuntu does not have predictable
    naming pattern for the debuginfo packages and the repositories
    are most of the time being updated which results in random
    failures in CI installing these packages.

--- .github/setup-valgrind.sh
+++ .github/setup-valgrind.sh
@@ -13,4 +13,7 @@ if [ "$1" == "valgrind" -o "$2" == "valgrind" ]; then
 	export VALGRIND="valgrind -q --error-exitcode=1 --leak-check=full --keep-debuginfo=yes --trace-children=yes --gen-suppressions=all --suppressions=$PWD/tests/opensc.supp"
 	# this should help us getting better traces as some of pcsclite and avoid false positives
 	export LD_PRELOAD=$(ldconfig -p | grep libpcsclite.so.1 |  tr ' ' '\n' | grep /)
+
+	# The valgrind should be able to find the debuginfo on the fly
+	export DEBUGINFOD_URLS="https://debuginfod.ubuntu.com"
 fi
