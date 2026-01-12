OpenSC/OpenSC

commit 767c340a489f17753bf137a12a9c579255c992ea

pkcs11: Use implicit type conversion to avoid build failure
Without this windows build fails silently like this:

```
framework-pkcs15.c
Error: framework-pkcs15.c(4697): error C2220: the following warning is treated as an error
Warning: framework-pkcs15.c(4697): warning C4319: '~': zero extending 'unsigned int' to 'size_t' of greater size
```

Signed-off-by: Jakub Jelen <jjelen@redhat.com>

src/pkcs11/framework-pkcs15.c
@@ -4694,7 +4694,7 @@ pkcs15_prkey_decrypt(struct sc_pkcs11_session *session, void *obj,
 
 	/* only padding error must be handled in constant-time way,
 	 * other error can be returned straight away */
-	if ((~constant_time_eq_i(rv, SC_ERROR_WRONG_PADDING) & constant_time_lt_s(sizeof(decrypted), (size_t)rv)))
+	if ((~(size_t)constant_time_eq_i(rv, SC_ERROR_WRONG_PADDING) & constant_time_lt_s(sizeof(decrypted), (size_t)rv)))
 		return sc_to_cryptoki_error(rv, "C_Decrypt");
 
 	/* check rv for padding error */

commit 64f7212ecfe4d4c45bd57b2947b631b9a2252594

pkcs11: Fix constant-time regression in PKCS#1 v1.5 decrypt path
A cast introduced in commit 767c340a489f17753bf137a12a9c579255c992ea causes
padding errors to return immediately on 64-bit platforms, breaking the
constant-time handling of PKCS#1 v1.5 decryption errors. On 64-bit systems,
casting the result of constant_time_eq_i() to size_t before applying bitwise
NOT produces 0xffffffff00000000 instead of zero when the values match, causing
the wrong branch to be taken. This timing discrepancy creates a padding oracle
that can be exploited for Bleichenbacher/Marvin-style attacks against PKCS#1
v1.5 decryption when software unpadding is used. The fix uses constant_time_eq_s
with size_t arguments to ensure proper full-width mask handling on 64-bit
platforms.

Reported-by: Pavel Kohout, Aisle Research, www.aisle.com

src/pkcs11/framework-pkcs15.c
@@ -4694,7 +4694,7 @@ pkcs15_prkey_decrypt(struct sc_pkcs11_session *session, void *obj,
 
 	/* only padding error must be handled in constant-time way,
 	 * other error can be returned straight away */
-	if ((~(size_t)constant_time_eq_i(rv, SC_ERROR_WRONG_PADDING) & constant_time_lt_s(sizeof(decrypted), (size_t)rv)))
+	if ((~constant_time_eq_s((size_t)rv, (size_t)SC_ERROR_WRONG_PADDING) & constant_time_lt_s(sizeof(decrypted), (size_t)rv)))
 		return sc_to_cryptoki_error(rv, "C_Decrypt");
 
 	/* check rv for padding error */