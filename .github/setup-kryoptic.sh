36851e3d347ea84c8625d0e6e0b9c00142dd4aa4
Jakub Jelen <jjelen@redhat.com>
2026-03-23 17:04:26 +0100

    ci: Simplify kryoptic tests by using packaged version

--- .github/setup-kryoptic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# build kryoptic
-if [ ! -d "kryoptic" ]; then
-	git clone https://github.com/latchset/kryoptic.git
-fi
-pushd kryoptic
-cargo build --features dynamic,standard,nssdb,pqc
-popd
