ed8575062d6093290087061cdf7d5b1e5b4e606a
Jakub Jelen <jjelen@redhat.com>
2026-04-13 15:30:02 +0200

    pkcs11: Improve logging message when registering mechanism

--- src/pkcs11/mechanism.c
+++ src/pkcs11/mechanism.c
@@ -1786,7 +1786,7 @@ sc_pkcs11_register_sign_and_hash_mechanism(struct sc_pkcs11_card *p11card,
 	CK_RV rv;
 
 	LOG_FUNC_CALLED(p11card->card->ctx);
-	sc_log(p11card->card->ctx, "mech = %lx, hash_mech = %lx", mech, hash_mech);
+	sc_log(p11card->card->ctx, "mech = 0x%lx, hash_mech = 0x%lx", mech, hash_mech);
 
 	if (!sign_type)
 		LOG_FUNC_RETURN(p11card->card->ctx, CKR_MECHANISM_INVALID);
