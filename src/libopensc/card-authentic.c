97c514e86d6da9e11a4b405f7e4d7f1babdca26f
Jakub Jelen <jjelen@redhat.com>
2026-04-13 22:14:34 +0200

    authentic: Avoid unsigned integer underflow
    Reported by Alex Kelchin (Medoedus)
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/libopensc/card-authentic.c
+++ src/libopensc/card-authentic.c
@@ -1039,6 +1039,9 @@ authentic_chv_verify(struct sc_card *card, struct sc_pin_cmd_data *pin_cmd,
 		pin_len = pin1->len;
 
 		if (pin1->pad_length && pin_cmd->flags & SC_PIN_CMD_NEED_PADDING)   {
+			if (pin1->len > pin1->pad_length || pin1->pad_length > sizeof(pin_buff)) {
+				LOG_FUNC_RETURN(ctx, SC_ERROR_INVALID_PIN_LENGTH);
+			}
 			memset(pin_buff + pin1->len, pin1->pad_char, pin1->pad_length - pin1->len);
 			pin_len = pin1->pad_length;
 		}
