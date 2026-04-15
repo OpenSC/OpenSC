b116f1b7ba6d844a5c4d85b41cb552d7d0f7346c
Frank Morgner <frankmorgner@gmail.com>
2026-04-15 14:19:58 +0200

    pkcs11: enable C_WaitForSlotEvent for all platforms

--- src/pkcs11/pkcs11-global.c
+++ src/pkcs11/pkcs11-global.c
@@ -805,11 +805,6 @@ CK_RV C_WaitForSlotEvent(CK_FLAGS flags,   /* blocking/nonblocking flag */
 		return  CKR_ARGUMENTS_BAD;
 
 	sc_log(context, "C_WaitForSlotEvent(block=%d)", !(flags & CKF_DONT_BLOCK));
-#ifndef PCSCLITE_GOOD
-	/* Not all pcsc-lite versions implement consistently used functions as they are */
-	if (!(flags & CKF_DONT_BLOCK))
-		return CKR_FUNCTION_NOT_SUPPORTED;
-#endif /* PCSCLITE_GOOD */
 	rv = sc_pkcs11_lock();
 	if (rv != CKR_OK)
 		return rv;
