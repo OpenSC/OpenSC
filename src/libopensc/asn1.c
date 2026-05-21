ead799c7c052bb0654cfce67fe01e83239329a7a
Jiami Lin <carmel0@me.com>
2026-05-21 16:04:22 +0800

    Update src/libopensc/asn1.c
    I looked at the rest of the logic, you're right.
    
    Co-authored-by: Frank Morgner <frankmorgner@gmail.com>

--- src/libopensc/asn1.c
+++ src/libopensc/asn1.c
@@ -1525,12 +1525,9 @@ static int asn1_decode_entry(sc_context_t *ctx,struct sc_asn1_entry *entry,
 	case SC_ASN1_ENUMERATED:
 		if (parm != NULL) {
 			r = sc_asn1_decode_integer(obj, objlen, (int *) entry->parm, 0);
-			if (r == 0) {
+			if (r == SC_SUCCESS) {
 				sc_debug(ctx, SC_LOG_DEBUG_ASN1, "%*.*sdecoding '%s' returned %d\n",
 					depth, depth, "", entry->name, *((int *) entry->parm));
-			} else {
-				sc_debug(ctx, SC_LOG_DEBUG_ASN1, "%*.*sdecoding '%s' failed: %d\n",
-					depth, depth, "", entry->name, r);
 			}
 		}
 		break;
