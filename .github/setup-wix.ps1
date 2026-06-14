136ad045353864ceebf3222983755f0e811a17d2
Raul Metsma <raul@metsma.ee>
2026-06-14 22:23:35 +0300

    Update windows build
    * Cleanup Github Actions build
    * WIX 7.0
    * Build libeac with vcpkg
    * Disable asserts all release builds
    * Zlib renamed dll zlib to z in 1.3.2 windows build
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- .github/setup-wix.ps1
+++ .github/setup-wix.ps1
@@ -1,10 +1,19 @@
 param(
-  [string]$version = '6.0.2'
+  [string]$version = '7.0.0'
 )
-& dotnet tool install -g --version $version wix
-& wix extension add -g WixToolset.UI.wixext/$version
-& wix extension add -g WixToolset.Util.wixext/$version
-& cd win32
-& dotnet new console --force --name CustomAction
-& dotnet add CustomAction package WixToolset.WcaUtil --version $version --package-directory packages
-& cd ..
+
+$ErrorActionPreference = 'Stop'
+
+function Exec([scriptblock]$cmd) {
+    & $cmd
+    if ($LASTEXITCODE -ne 0) { throw "Command failed (exit $LASTEXITCODE)" }
+}
+
+Exec { dotnet tool install -g --version $version wix }
+Exec { wix eula accept wix7 }
+Exec { wix extension add -g WixToolset.UI.wixext/$version }
+Exec { wix extension add -g WixToolset.Util.wixext/$version }
+Set-Location win32
+Exec { dotnet new console --force --name CustomAction }
+Exec { dotnet add CustomAction package WixToolset.WcaUtil --version $version --package-directory packages }
+Set-Location ..
