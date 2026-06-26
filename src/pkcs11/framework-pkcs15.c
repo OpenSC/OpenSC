edfc12b9fcdef2ebf589dbbcd33221249e07c36b
Frank Morgner <frankmorgner@gmail.com>
2026-06-26 21:09:03 +0200

    PKCS#11: propagate CKA_DERIVE when importing private key

--- src/pkcs11/framework-pkcs15.c
+++ src/pkcs11/framework-pkcs15.c
@@ -2453,9 +2453,9 @@ pkcs15_create_private_key(struct sc_pkcs11_slot *slot, struct sc_profile *profil
 		case CKA_DECRYPT:
 			args.usage |= pkcs15_check_bool_cka(attr, SC_PKCS15_PRKEY_USAGE_DECRYPT);
 			break;
-			/* FIXME
 		case CKA_DERIVE:
-		*/
+			args.usage |= pkcs15_check_bool_cka(attr, SC_PKCS15_PRKEY_USAGE_DERIVE);
+			break;
 		case CKA_UNWRAP:
 			args.usage |= pkcs15_check_bool_cka(attr, SC_PKCS15_PRKEY_USAGE_UNWRAP);
 			break;
@@ -2817,9 +2817,9 @@ pkcs15_create_public_key(struct sc_pkcs11_slot *slot, struct sc_profile *profile
 		case CKA_WRAP:
 			args.usage |= pkcs15_check_bool_cka(attr, SC_PKCS15_PRKEY_USAGE_WRAP);
 			break;
-			/* FIXME
 		case CKA_DERIVE:
-		*/
+			args.usage |= pkcs15_check_bool_cka(attr, SC_PKCS15_PRKEY_USAGE_DERIVE);
+			break;
 		case CKA_EC_POINT:
 			switch (key_type) {
 			case CKK_EC:
@@ -3243,6 +3243,7 @@ get_usage_pubk(CK_ATTRIBUTE_PTR pTempl, CK_ULONG ulCount, unsigned long *x509_us
 		case CKA_VERIFY_RECOVER:
 			/* FIXME x509_usage */
 			*usage      |= pkcs15_check_bool_cka(pTempl + i, SC_PKCS15_PRKEY_USAGE_VERIFYRECOVER);
+			break;
 		case CKA_WRAP:
 			*x509_usage |= pkcs15_check_bool_cka(pTempl + i, SC_PKCS15INIT_X509_KEY_ENCIPHERMENT);
 			*usage      |= pkcs15_check_bool_cka(pTempl + i, SC_PKCS15_PRKEY_USAGE_WRAP);
