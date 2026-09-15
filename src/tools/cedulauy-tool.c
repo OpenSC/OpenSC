36babaf5e2bd3cbd3f5e3dfcceea6552fac4a226
Nicolas Gutierrez <ngutierreztassano@gmail.com>
2026-09-15 15:39:27 -0300

    cedulauy: add contactless (PACE) support
    The contactless interface of the IAS Classic v5 cedula is protected with
    PACE using the MRZ as password. It is only matched once PACE with the MRZ
    and selecting the eID application succeed. The MRZ is stored with the new
    cedulauy-tool, either read over the contact interface or entered by hand,
    or given in the CEDULAUY_MRZ environment variable.
    
    Thanks to @macarenapineiro for testing the contactless ATR on her card,
    which made it possible to tighten the ATR mask.
    
    Co-authored-by: Frank Morgner <frankmorgner@gmail.com>

---
+++ src/tools/cedulauy-tool.c
@@ -0,0 +1,230 @@
+/*
+ * cedulauy-tool.c: Set up the Uruguayan eID card (cédula de identidad) for contactless use
+ *
+ * Copyright (C) 2026 Nicolás Gutiérrez <ngutierreztassano@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "libopensc/card-cedulauy-cache.h"
+#include "libopensc/cards.h"
+#include "libopensc/opensc.h"
+#include "util.h"
+
+static const char *app_name = "cedulauy-tool";
+
+static const struct option options[] = {
+		{"reader",   1, NULL, 'r'},
+		{"wait",	 0, NULL, 'w'},
+		{"read-mrz", 0, NULL, 'c'},
+		{"mrz",	1, NULL, 'm'},
+		{"delete",   0, NULL, 'd'},
+		{"verbose",  0, NULL, 'v'},
+		{"help",	 0, NULL, 'h'},
+		{NULL,       0, NULL, 0	 }
+};
+
+static const char *option_help[] = {
+		"Uses reader number <arg> [0]",
+		"Wait for a card to be inserted",
+		"Read the MRZ from the card in a contact reader",
+		"Use the MRZ <arg> (the three lines without separators)",
+		"Delete the stored MRZ",
+		"Verbose operation, may be used several times",
+		"Display tool options",
+};
+
+/* TD1 MRZ, readable over the contact interface without a PIN */
+#define CEDULAUY_MRZ_PATH "3F007000700B"
+
+static int
+read_mrz(sc_context_t *ctx, const char *reader, int wait, unsigned char *mrz)
+{
+	sc_card_t *card = NULL;
+	unsigned char buf[3 + CEDULAUY_MRZ_LEN];
+	sc_path_t path;
+	int r;
+
+	if (util_connect_card(ctx, &card, reader, wait)) {
+		fprintf(stderr, "Cannot connect with the card\n");
+		return SC_ERROR_CARD_NOT_PRESENT;
+	}
+
+	if (card->type != SC_CARD_TYPE_CEDULAUY) {
+		fprintf(stderr, "The card is not a cedulauy in a contact reader\n");
+		r = SC_ERROR_NOT_SUPPORTED;
+	} else {
+		sc_format_path(CEDULAUY_MRZ_PATH, &path);
+		r = sc_select_file(card, &path, NULL);
+		if (r >= 0)
+			r = sc_read_binary(card, 0, buf, sizeof buf, NULL);
+		if (r >= 0 && (r < (int)sizeof buf || buf[0] != 0x7F || buf[1] != 0x01 || buf[2] != CEDULAUY_MRZ_LEN))
+			r = SC_ERROR_INVALID_DATA;
+		if (r < 0) {
+			fprintf(stderr, "Cannot read the MRZ from the card: %s\n", sc_strerror(r));
+		} else {
+			memcpy(mrz, buf + 3, CEDULAUY_MRZ_LEN);
+			r = SC_SUCCESS;
+		}
+		sc_mem_clear(buf, sizeof buf);
+	}
+
+	sc_unlock(card);
+	sc_disconnect_card(card);
+	return r;
+}
+
+static int
+append_mrz(const char *in, unsigned char *mrz, size_t *len)
+{
+	for (; *in != '\0'; in++) {
+		if (isspace((unsigned char)*in))
+			continue;
+		if (*len == CEDULAUY_MRZ_LEN)
+			return SC_ERROR_WRONG_LENGTH;
+		mrz[(*len)++] = (unsigned char)toupper((unsigned char)*in);
+	}
+
+	return SC_SUCCESS;
+}
+
+static int
+prompt_mrz(unsigned char *mrz, size_t *len)
+{
+	char line[128];
+	int i, r = SC_SUCCESS;
+
+	printf("Enter the three lines of the MRZ printed on the back of the card.\n");
+	for (i = 1; i <= 3 && r == SC_SUCCESS; i++) {
+		printf("Line %d: ", i);
+		fflush(stdout);
+		if (fgets(line, sizeof line, stdin) == NULL) {
+			fprintf(stderr, "Cannot read the MRZ\n");
+			r = SC_ERROR_INTERNAL;
+			break;
+		}
+		r = append_mrz(line, mrz, len);
+	}
+	sc_mem_clear(line, sizeof line);
+
+	return r;
+}
+
+int
+main(int argc, char *argv[])
+{
+	const char *opt_reader = NULL;
+	const char *opt_mrz = NULL;
+	int opt_wait = 0, opt_read = 0, opt_delete = 0, opt_mrz_given = 0;
+	int verbose = 0;
+	sc_context_t *ctx = NULL;
+	sc_context_param_t ctx_param = {0};
+	unsigned char mrz[CEDULAUY_MRZ_LEN];
+	size_t mrz_len = 0;
+	int c, r;
+
+	while ((c = getopt_long(argc, argv, "r:wcm:dvh", options, NULL)) != -1) {
+		switch (c) {
+		case 'r':
+			opt_reader = optarg;
+			break;
+		case 'w':
+			opt_wait = 1;
+			break;
+		case 'c':
+			opt_read = 1;
+			break;
+		case 'm':
+			util_get_pin(optarg, &opt_mrz);
+			opt_mrz_given = 1;
+			break;
+		case 'd':
+			opt_delete = 1;
+			break;
+		case 'v':
+			verbose++;
+			break;
+		case 'h':
+		default:
+			util_print_usage_and_die(app_name, options, option_help, NULL);
+		}
+	}
+
+	if (optind < argc || opt_read + opt_delete + opt_mrz_given > 1)
+		util_print_usage_and_die(app_name, options, option_help, NULL);
+
+	if (opt_mrz_given && opt_mrz == NULL) {
+		fprintf(stderr, "No MRZ given\n");
+		return 1;
+	}
+
+	ctx_param.app_name = app_name;
+	ctx_param.debug = verbose;
+	if (verbose)
+		ctx_param.debug_file = stderr;
+
+	r = sc_context_create(&ctx, &ctx_param);
+	if (r) {
+		fprintf(stderr, "Failed to establish context: %s\n", sc_strerror(r));
+		return 1;
+	}
+
+	if (opt_delete) {
+		r = cedulauy_delete_mrz_cache(ctx);
+		if (r == SC_ERROR_FILE_NOT_FOUND) {
+			printf("No MRZ is stored.\n");
+			r = SC_SUCCESS;
+		} else if (r < 0) {
+			fprintf(stderr, "Cannot delete the stored MRZ: %s\n", sc_strerror(r));
+		} else {
+			printf("MRZ removed.\n");
+		}
+	} else {
+		if (opt_read) {
+			r = read_mrz(ctx, opt_reader, opt_wait, mrz);
+			if (r == SC_SUCCESS)
+				mrz_len = CEDULAUY_MRZ_LEN;
+		} else if (opt_mrz != NULL) {
+			r = append_mrz(opt_mrz, mrz, &mrz_len);
+		} else {
+			r = prompt_mrz(mrz, &mrz_len);
+		}
+
+		if ((r == SC_SUCCESS && mrz_len != CEDULAUY_MRZ_LEN) || r == SC_ERROR_WRONG_LENGTH) {
+			fprintf(stderr, "The MRZ has to be %d characters long\n", CEDULAUY_MRZ_LEN);
+			r = SC_ERROR_WRONG_LENGTH;
+		}
+
+		if (r == SC_SUCCESS) {
+			r = cedulauy_write_mrz_cache(ctx, mrz);
+			if (r < 0)
+				fprintf(stderr, "Cannot store the MRZ: %s\n", sc_strerror(r));
+			else
+				printf("MRZ stored, the card can now be used over the contactless interface.\n");
+		}
+	}
+
+	sc_mem_clear(mrz, sizeof mrz);
+	sc_release_context(ctx);
+	return r == SC_SUCCESS ? 0 : 1;
+}
